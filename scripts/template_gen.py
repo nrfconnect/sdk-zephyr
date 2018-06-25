9f73bdb8885526a86af53956243f0726115c83c5
Even Falch-Larsen <even.falch-larsen@nordicsemi.no>
2018-06-25 01:11:26 -0700

    Finished syscall header template prototype

--- scripts/template_gen.py
+++ scripts/template_gen.py
@@ -6,13 +6,13 @@ import json
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def render_template():
-    context = json.loads(open(THIS_DIR+'/templates/test_context.json', 'r').read())
+    context = json.loads(open(THIS_DIR+'/templates/gen_syscall_header_context.json', 'r').read())
     
     # Create the jinja2 environment.
     # Notice the use of trim_blocks, which greatly helps control whitespace.
     j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
                          trim_blocks=True)
-    print j2_env.get_template('templates/test.jinja2').render(context=context)
+    print j2_env.get_template('templates/gen_syscall_header.jinja2').render(context=context)
 
 if __name__ == '__main__':
     render_template()
