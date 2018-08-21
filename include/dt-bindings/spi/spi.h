7de3dfd2d5b8577e0d18190f8e2c601e9085bfcd
Filip Kubicz <filip.kubicz@nordicsemi.no>
2018-08-21 15:28:59 +0200

    dts: add SPI bindings for nRF5 family
    This change adds DTS definition of SPI device for nRF5 chips.
    
    This change also removes SPI pin configuration from Kconfig and moves
    it to chip DTS.
    
    Signed-off-by: Filip Kubicz <filip.kubicz@nordicsemi.no>

---
+++ include/dt-bindings/spi/spi.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: BSD-5-Clause-Nordic
+ */
+#ifndef __DT_BINDINGS_SPI_SPI_H
+#define __DT_BINDINGS_SPI_SPI_H
+
+#define SPI_BITRATE_STANDARD	1000000	/* 1 Mbit/s */
+
+#endif /* __DT_BINDINGS_SPI_SPI_H */
