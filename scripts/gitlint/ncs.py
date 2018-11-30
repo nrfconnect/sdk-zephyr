4a6d5fc57fe7589b2585ee5e44341f50821cc653
Carles Cufi <carles.cufi@nordicsemi.no>
2018-11-30 14:07:56 +0100

    [nrf noup] ci: NCS-specific CI tweaks
    Necessary changes for NCS CI.
    
    - Add a Jenkinsfile
    - compliance.yml: Disable check for merge commits, since we have upmerges
      downstream.
      Add an additional check for sauce tags (details in scripts/gitlint)
      Also, since in the code we refer to Kconfig symbols that are defined
      in the sdk-nrf repository, the Kconfig checks (Kconfig and
      KconfigBasic) will not pass so exclude them.
    - Extend the max commit line lengths for Gitlint to account for sauce
      tags
    - scripts/gitlint: Add support for sauce tags checks. In order to not
      interfere with CI in other repos (namely sdk-nrf) this is run as an
      independent gitlint invocation and not as part of check_compliance.py.
      This invocation enables the ncs-sauce-tags rule and, since it runs
      before rebasing and thus will run on merge commits, disables the
      title-starts-with-subsystem rule by setting a catch-all regex.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    Signed-off-by: Krishna T <krishna.t@nordicsemi.no>
    (cherry picked from commit d34b035bcc569431b7008c72edc55db1b29bccf3)

---
+++ scripts/gitlint/ncs.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2022 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+from gitlint.rules import BoolOption, CommitRule, RuleViolation
+import re
+
+class NCSSauceTags(CommitRule):
+    """This rule enforces the NCS sauce tag specification.
+       https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/nrf/dm_code_base.html#oss-repositories-downstream-project-history
+    """
+
+    # A rule MUST have a human friendly name
+    name = "ncs-sauce-tags"
+
+    # A rule MUST have a *unique* id, we recommend starting with UC (for User-defined Commit-rule).
+    id = "UC100"
+
+    options_spec = [BoolOption('enable', False, 'Enable the Sauce Tags Rule')]
+
+    def validate(self, commit):
+        self.log.debug(f'NCSSauceTags: enable:{self.options["enable"].value} ' \
+                       f'sha:{commit.sha}')
+
+        if not self.options['enable'].value:
+            self.log.debug(f'Disabled, skipping')
+            return
+
+        m = re.match(r'^(Revert\s+\")?\[nrf (mergeup|fromtree|fromlist|noup)\]\s+',
+                     commit.message.title)
+        if not m:
+            return [RuleViolation(self.id, 'Title does not contain a sauce tag',
+                                  line_nr=1)]
+
+        tag = m.group(1)
+        self.log.debug(f'Matched sauce tag {tag}')
+
+        if tag == 'mergeup':
+            if not commit.is_merge_commit:
+                return [RuleViolation(self.id,
+                                      'mergeup used in a non-merge commit')]
+            if not re.match(r'^\[nrf mergeup\] Merge upstream up to commit \b([a-f0-9]{40})\b',
+                    commit.message.title):
+                return [RuleViolation(self.id, 'Invalid mergeup commit title')]
+        elif tag == 'fromlist':
+            regex = r'^Upstream PR: https://github\.com/.*/pull/\d+'
+            matches = re.findall(regex, '\n'.join(commit.message.body), re.MULTILINE)
+            if len(matches) == 0:
+                return [RuleViolation(self.id,
+                        'Missing Upstream PR reference in fromlist commit')]
+        elif tag == 'fromtree':
+            regex = r'^\(cherry picked from commit \b([a-f0-9]{40})\b\)'
+            matches = re.findall(regex, '\n'.join(commit.message.body), re.MULTILINE)
+            if len(matches) == 0:
+                return [RuleViolation(self.id,
+                        'Missing cherry-pick reference in fromtree commit')]
