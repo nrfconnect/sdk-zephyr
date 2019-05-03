6bff2a99aba37ff5e74f48f26582927e1d49d60c
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2019-05-03 14:21:52 +0200

    [nrf noup] tree-wide: support NCS Partition Manager (PM) definitions
    Partition Manager (PM) is a component of the nRF Connect SDK (NCS)
    which uses yaml files to resolve flash partition placement with a
    holistic view of the entire device, including each firmware image
    present on the flash device, and various subsystems, such as settings
    and NFFS.
    
    When this NCS extension is used, various source files which would use
    partition information from devicetree in "vanilla" zephyr instead use
    defines generated by PM instead.
    
    This commit removes support for HEX_FILES_TO_MERGE, as it conflicts
    with PM.
    
    The settings subsystem pm.yml defines a partition 'settings_storage'.
    The nffs subsystem pm.yml defines 'nffs_storage'.
    
    Refer to the NCS documentation page for this feature for more details.
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>
    Signed-off-by: Thomas Stenersen <thomas.stenersen@nordicsemi.no>

--- subsys/settings/src/settings_init.c
+++ subsys/settings/src/settings_init.c
@@ -15,6 +15,17 @@
 #include "settings/settings_file.h"
 #include <zephyr.h>
 
+#if USE_PARTITION_MANAGER
+
+#include <pm_config.h>
+#define FLASH_AREA_STORAGE_ID       PM_SETTINGS_STORAGE_ID
+
+#else
+
+#include <generated_dts_board.h>
+#define FLASH_AREA_STORAGE_ID       DT_FLASH_AREA_STORAGE_ID
+
+#endif /* USE_PARTITION_MANAGER */
 
 bool settings_subsys_initialized;
 
@@ -78,7 +89,7 @@ int settings_backend_init(void)
 	int rc;
 	const struct flash_area *fap;
 
-	rc = flash_area_get_sectors(DT_FLASH_AREA_STORAGE_ID, &cnt,
+	rc = flash_area_get_sectors(FLASH_AREA_STORAGE_ID, &cnt,
 				    settings_fcb_area);
 	if (rc == -ENODEV) {
 		return rc;
@@ -91,7 +102,7 @@ int settings_backend_init(void)
 	rc = settings_fcb_src(&config_init_settings_fcb);
 
 	if (rc != 0) {
-		rc = flash_area_open(DT_FLASH_AREA_STORAGE_ID, &fap);
+		rc = flash_area_open(FLASH_AREA_STORAGE_ID, &fap);
 
 		if (rc == 0) {
 			rc = flash_area_erase(fap, 0, fap->fa_size);
@@ -136,12 +147,12 @@ int settings_backend_init(void)
 	struct flash_sector hw_flash_sector;
 	u32_t sector_cnt = 1;
 
-	rc = flash_area_open(DT_FLASH_AREA_STORAGE_ID, &fa);
+	rc = flash_area_open(FLASH_AREA_STORAGE_ID, &fa);
 	if (rc) {
 		return rc;
 	}
 
-	rc = flash_area_get_sectors(DT_FLASH_AREA_STORAGE_ID, &sector_cnt,
+	rc = flash_area_get_sectors(FLASH_AREA_STORAGE_ID, &sector_cnt,
 				    &hw_flash_sector);
 	if (rc == -ENODEV) {
 		return rc;
