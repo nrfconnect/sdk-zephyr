2ce2dd98198d59bb85de528d3dfa3f3a95549622
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2019-05-03 14:21:52 +0200

    [nrf noup] tree-wide: support NCS Partition Manager (PM) definitions
    Partition Manager (PM) is a component of the nRF Connect SDK (NCS)
    which uses yaml files to resolve flash partition placement with a
    holistic view of the entire device, including each firmware image
    present on the flash device, and various subsystems, such as settings
    and NFFS.
    
    When this NCS extension is used, various source files which would use
    partition information from devicetree in "vanilla" zephyr instead use
    defines generated by PM instead.
    
    This commit removes support for HEX_FILES_TO_MERGE, as it conflicts
    with PM.
    
    The settings subsystem pm.yml defines a partition 'settings_storage'.
    The nffs subsystem pm.yml defines 'nffs_storage'.
    
    Refer to the NCS documentation page for this feature for more details.
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>
    Signed-off-by: Thomas Stenersen <thomas.stenersen@nordicsemi.no>
    (cherry picked from commit 6bff2a99aba37ff5e74f48f26582927e1d49d60c)
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- subsys/storage/flash_map/flash_map_default.c
+++ subsys/storage/flash_map/flash_map_default.c
@@ -8,14 +8,39 @@
 #include <zephyr.h>
 #include <storage/flash_map.h>
 
-#define FLASH_AREA_FOO(i, _)				\
-	{.fa_id = i,					\
-	 .fa_off = DT_FLASH_AREA_##i##_OFFSET,		\
-	 .fa_dev_name = DT_FLASH_AREA_##i##_DEV,	\
-	 .fa_size = DT_FLASH_AREA_##i##_SIZE,},
+/**
+ * Have flash_map_default use Partition Manager information instead of
+ * DeviceTree information when we are using the Partition Manager.
+ */
+
+#if USE_PARTITION_MANAGER
+#include <pm_config.h>
+#include <sys/util.h>
+
+#define FLASH_MAP_OFFSET(i) UTIL_CAT(PM_, UTIL_CAT(PM_##i##_LABEL, _ADDRESS))
+#define FLASH_MAP_DEV(i)    UTIL_CAT(PM_, UTIL_CAT(PM_##i##_LABEL, _DEV_NAME))
+#define FLASH_MAP_SIZE(i)   UTIL_CAT(PM_, UTIL_CAT(PM_##i##_LABEL, _SIZE))
+#define FLASH_MAP_NUM       PM_NUM
+
+#else
+
+#define FLASH_MAP_OFFSET(i) DT_FLASH_AREA_##i##_OFFSET
+#define FLASH_MAP_DEV(i)    DT_FLASH_AREA_##i##_DEV
+#define FLASH_MAP_SIZE(i)   DT_FLASH_AREA_##i##_SIZE
+#define FLASH_MAP_NUM       DT_FLASH_AREA_NUM
+
+#endif /* USE_PARTITION_MANAGER */
+
+#define FLASH_AREA_FOO(i, _)                \
+	{                                       \
+		.fa_id       = i,                   \
+		.fa_off      = FLASH_MAP_OFFSET(i), \
+		.fa_dev_name = FLASH_MAP_DEV(i),    \
+		.fa_size     = FLASH_MAP_SIZE(i)    \
+	},
 
 const struct flash_area default_flash_map[] = {
-	UTIL_LISTIFY(DT_FLASH_AREA_NUM, FLASH_AREA_FOO, ~)
+	UTIL_LISTIFY(FLASH_MAP_NUM, FLASH_AREA_FOO, ~)
 };
 
 const int flash_map_entries = ARRAY_SIZE(default_flash_map);
