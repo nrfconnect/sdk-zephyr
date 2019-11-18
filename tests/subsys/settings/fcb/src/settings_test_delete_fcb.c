62bb0ce61fc8fc753c14a6ded406ed2475fffd2d
Dominik Ermel <dominik.ermel@nordicsemi.no>
2019-11-18 13:46:33 +0000

    Revert "[nrf fromtree] Test: Settings: Fix FCB delete test"
    This reverts commit d5e60422e368847bfb00e101cab49e2af8eba0a8.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- tests/subsys/settings/fcb/src/settings_test_delete_fcb.c
+++ tests/subsys/settings/fcb/src/settings_test_delete_fcb.c
@@ -37,10 +37,8 @@ void test_config_delete_fcb(void)
 	zassert_true(rc == 0, "fcb redout error");
 	zassert_true(val8 == 153U, "bad value read");
 
-	val8 = 0x55;
-
 	settings_delete("myfoo/mybar");
 	rc = settings_load();
 	zassert_true(rc == 0, "fcb redout error");
-	zassert_true(val8 == 0x55, "bad value read");
+	zassert_true(val8 == VAL8_DELETED, "bad value read");
 }
