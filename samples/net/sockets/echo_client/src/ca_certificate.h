2430996e499b4bb0ffda2b5a23a8cd8d6f1b6b97
Dominik Ermel <dominik.ermel@nordicsemi.no>
2019-11-18 13:47:35 +0000

    Revert "[nrf fromlist] samples: Add PSK support for echo client/server"
    This reverts commit c5eb13157c092002ac7b9592002da77620252e69.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- samples/net/sockets/echo_client/src/ca_certificate.h
+++ samples/net/sockets/echo_client/src/ca_certificate.h
@@ -8,7 +8,6 @@
 #define __CA_CERTIFICATE_H__
 
 #define CA_CERTIFICATE_TAG 1
-#define PSK_TAG 2
 
 #define TLS_PEER_HOSTNAME "localhost"
 
@@ -17,9 +16,5 @@
 static const unsigned char ca_certificate[] = {
 #include "echo-apps-cert.der.inc"
 };
-
-#if defined(CONFIG_MBEDTLS_KEY_EXCHANGE_PSK_ENABLED)
-#include CONFIG_NET_SAMPLE_PSK_HEADER_FILE
-#endif
 
 #endif /* __CA_CERTIFICATE_H__ */
--- samples/net/sockets/echo_server/src/certificate.h
+++ samples/net/sockets/echo_server/src/certificate.h
@@ -8,7 +8,6 @@
 #define __CERTIFICATE_H__
 
 #define SERVER_CERTIFICATE_TAG 1
-#define PSK_TAG 2
 
 static const unsigned char server_certificate[] = {
 #include "echo-apps-cert.der.inc"
@@ -18,9 +17,5 @@ static const unsigned char server_certificate[] = {
 static const unsigned char private_key[] = {
 #include "echo-apps-key.der.inc"
 };
-
-#if defined(CONFIG_MBEDTLS_KEY_EXCHANGE_PSK_ENABLED)
-#include CONFIG_NET_SAMPLE_PSK_HEADER_FILE
-#endif
 
 #endif /* __CERTIFICATE_H__ */
