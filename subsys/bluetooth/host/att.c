a5b1cfbc878fd422233b5375c0cb0544de2f2780
Robert Lubos <robert.lubos@nordicsemi.no>
2020-01-29 15:55:46 +0100

    Revert "[nrf fromlist] Bluetooth: ATT: Fix handling ATT response"
    This reverts commit b556ffa87d8850eaaaf0cd5a97a178900fc9dbcd.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/bluetooth/host/att.c
+++ subsys/bluetooth/host/att.c
@@ -344,7 +344,7 @@ static void att_process(struct bt_att *att)
 
 static u8_t att_handle_rsp(struct bt_att *att, void *pdu, u16_t len, u8_t err)
 {
-	bt_att_func_t func = NULL;
+	bt_att_func_t func;
 	void *params;
 
 	BT_DBG("err 0x%02x len %u: %s", err, len, bt_hex(pdu, len));
@@ -378,12 +378,11 @@ static u8_t att_handle_rsp(struct bt_att *att, void *pdu, u16_t len, u8_t err)
 	att_req_destroy(att->req);
 	att->req = NULL;
 
+	func(att->chan.chan.conn, err, pdu, len, params);
+
 process:
 	/* Process pending requests */
 	att_process(att);
-	if (func) {
-		func(att->chan.chan.conn, err, pdu, len, params);
-	}
 
 	return 0;
 }
