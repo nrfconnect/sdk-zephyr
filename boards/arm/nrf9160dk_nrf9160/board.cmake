4f31dcd32c27ad4c8bc4bbb24f359fdf87cb2ed5
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2020-05-11 12:59:41 +0200

    work
    work
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- boards/arm/nrf9160dk_nrf9160/board.cmake
+++ boards/arm/nrf9160dk_nrf9160/board.cmake
@@ -4,3 +4,5 @@ board_runner_args(nrfjprog "--nrf-family=NRF91")
 board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
+
