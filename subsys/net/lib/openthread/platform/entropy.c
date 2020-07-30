0e59e98d0473c0fed27e6ebdd883ddf78222c39c
Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>
2020-07-30 12:39:06 +0200

    net: lib: openthread: Add system source for entropy
    When there is no entropy driver use general system function.
    
    Signed-off-by: Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>

--- subsys/net/lib/openthread/platform/entropy.c
+++ subsys/net/lib/openthread/platform/entropy.c
@@ -7,6 +7,7 @@
 #include <kernel.h>
 #include <string.h>
 #include <drivers/entropy.h>
+#include <random/rand32.h>
 #include <logging/log.h>
 
 #include <openthread/platform/entropy.h>
@@ -15,12 +16,9 @@
 
 LOG_MODULE_REGISTER(net_otPlat_entropy, CONFIG_OPENTHREAD_L2_LOG_LEVEL);
 
-#if !defined(CONFIG_ENTROPY_HAS_DRIVER)
-#error OpenThread requires an entropy source for a TRNG
-#endif
-
 otError otPlatEntropyGet(uint8_t *aOutput, uint16_t aOutputLength)
 {
+#if defined(CONFIG_ENTROPY_HAS_DRIVER)
 	/* static to obtain it once in a first call */
 	static struct device *dev;
 	int err;
@@ -42,6 +40,20 @@ otError otPlatEntropyGet(uint8_t *aOutput, uint16_t aOutputLength)
 		LOG_ERR("Failed to obtain entropy, err %d", err);
 		return OT_ERROR_FAILED;
 	}
+#else
+	int err;
+
+	if ((aOutput == NULL) || (aOutputLength == 0)) {
+		return OT_ERROR_INVALID_ARGS;
+	}
+
+	err = sys_csrand_get(aOutput, aOutputLength);
+	if (err != 0) {
+		LOG_ERR("Failed to obtain entropy, err %d", err);
+		return OT_ERROR_FAILED;
+	}
+
+#endif
 
 	return OT_ERROR_NONE;
 }
