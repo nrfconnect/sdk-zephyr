d25812eea798fc3e2ba23d90d5a07c8f9675700f
Dominik Ermel <dominik.ermel@nordicsemi.no>
2020-12-17 11:03:41 +0000

    Revert "[nrf fromlist] drivers: ieee802154: nrf5: Add support for tx in the future"
    This reverts commit b38a27ce157502c67d365e7e7caa43cdfe47e578.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- drivers/ieee802154/ieee802154_nrf5.c
+++ drivers/ieee802154/ieee802154_nrf5.c
@@ -56,7 +56,6 @@ static struct nrf5_802154_data nrf5_data;
 #define ACK_REQUEST_BIT (1 << 5)
 #define FRAME_PENDING_BYTE 1
 #define FRAME_PENDING_BIT (1 << 4)
-#define TXTIME_OFFSET_US  (5 * USEC_PER_MSEC)
 
 #if defined(CONFIG_SOC_NRF5340_CPUAPP) || defined(CONFIG_SOC_NRF5340_CPUNET)
 #define EUI64_ADDR (NRF_FICR->INFO.DEVICEID)
@@ -402,8 +401,6 @@ static int nrf5_tx(const struct device *dev,
 	nrf5_radio->tx_psdu[0] = payload_len + NRF5_FCS_LENGTH;
 	memcpy(nrf5_radio->tx_psdu + 1, payload, payload_len);
 
-	k_sem_reset(&nrf5_radio->tx_started);
-
 	/* Reset semaphore in case ACK was received after timeout */
 	k_sem_reset(&nrf5_radio->tx_wait);
 
@@ -418,22 +415,7 @@ static int nrf5_tx(const struct device *dev,
 		nrf_802154_transmit_csma_ca_raw(nrf5_radio->tx_psdu);
 		break;
 	case IEEE802154_TX_MODE_TXTIME:
-	case IEEE802154_TX_MODE_TXTIME_CCA: {
-		bool cca = (mode == IEEE802154_TX_MODE_TXTIME_CCA);
-		uint32_t tx_at;
-
-		__ASSERT_NO_MSG(pkt);
-		__ASSERT(IS_ENABLED(CONFIG_NET_PKT_TIMESTAMP),
-			 "Timestamp is required");
-		tx_at = net_pkt_timestamp(pkt)->second * USEC_PER_SEC
-			+ net_pkt_timestamp(pkt)->nanosecond / NSEC_PER_USEC;
-		ret = nrf_802154_transmit_raw_at(nrf5_radio->tx_psdu,
-						 cca,
-						 tx_at - TXTIME_OFFSET_US,
-						 TXTIME_OFFSET_US,
-						 nrf_802154_channel_get());
-		break;
-	}
+	case IEEE802154_TX_MODE_TXTIME_CCA:
 	default:
 		NET_ERR("TX mode %d not supported", mode);
 		return -ENOTSUP;
@@ -444,7 +426,6 @@ static int nrf5_tx(const struct device *dev,
 		return -EIO;
 	}
 
-	k_sem_take(&nrf5_radio->tx_started, K_FOREVER);
 	nrf5_tx_started(dev, pkt, frag);
 
 	LOG_DBG("Sending frame (ch:%d, txpower:%d)",
@@ -534,7 +515,6 @@ static int nrf5_init(const struct device *dev)
 	struct nrf5_802154_data *nrf5_radio = NRF5_802154_DATA(dev);
 
 	k_fifo_init(&nrf5_radio->rx_fifo);
-	k_sem_init(&nrf5_radio->tx_started, 0, 1);
 	k_sem_init(&nrf5_radio->tx_wait, 0, 1);
 	k_sem_init(&nrf5_radio->cca_wait, 0, 1);
 
@@ -699,13 +679,6 @@ void nrf_802154_receive_failed(nrf_802154_rx_error_t error)
 					IEEE802154_EVENT_RX_FAILED,
 					(void *)&reason);
 	}
-}
-
-void nrf_802154_tx_started(const uint8_t *frame)
-{
-	ARG_UNUSED(frame);
-
-	k_sem_give(&nrf5_data.tx_started);
 }
 
 void nrf_802154_tx_ack_started(const uint8_t *data)
