2eec91eec60165ec77fa7cec8dad1be76f464348
Pawel Kwiek <pawel.kwiek@nordicsemi.no>
2021-01-14 16:35:07 +0100

    samples: ieee802154: Update serialization sample
    This commit introduces changes allowing to use other variants of nRF
    Service Layer with the serialization sample.
    
    Signed-off-by: Pawel Kwiek <pawel.kwiek@nordicsemi.no>

--- samples/boards/nrf/ieee802154/802154_rpmsg/src/main.c
+++ samples/boards/nrf/ieee802154/802154_rpmsg/src/main.c
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <inttypes.h>
+#include <kernel.h>
+#include <stdbool.h>
 #include <sys/__assert.h>
 #include <zephyr.h>
 
@@ -13,4 +16,20 @@ void nrf_802154_serialization_error(const nrf_802154_ser_err_data_t *err)
 {
 	(void)err;
 	__ASSERT(false, "802.15.4 serialization error");
+}
+
+void nrf_802154_sl_fault_handler(uint32_t module_id, int32_t line, const char *p_error)
+{
+	if (p_error == NULL) {
+		p_error = "error unknown";
+	}
+	
+	printk("nrf_802154_sl: ASSERTION FAILED: Module %" PRIu32 ":%" PRId32 " expr: '%s'",
+		module_id, line, p_error);
+
+	k_panic();
+	__disable_irq();
+
+	while (true) {
+	}
 }
