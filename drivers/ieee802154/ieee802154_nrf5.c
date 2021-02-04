zephyrproject-rtos/zephyr

commit 0ab7df74b22af2f06be7142741975add6a482b42

[nrf noup] drivers: ieee802154: Use nRF Secure Services to read EUI64
This commit allows the setting of ieee802154 EUI64 address
in the non-secure build by reading the FICR registers out
of the secure zone.

Signed-off-by: Czeslaw Makarski <Czeslaw.Makarski@nordicsemi.no>
Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>
(cherry picked from commit c1d979c4f9e5f1629f9c0d671391f547038172dd)

drivers/ieee802154/ieee802154_nrf5.c
@@ -39,6 +39,11 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
 #include <net/ieee802154_radio.h>
 
+#if defined(CONFIG_SOC_NRF5340_CPUAPP) && \
+	defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
+#include <secure_services.h>
+#endif
+
 #include "ieee802154_nrf5.h"
 #include "nrf_802154.h"
 
@@ -59,7 +64,11 @@ static struct nrf5_802154_data nrf5_data;
 #define TXTIME_OFFSET_US  (5 * USEC_PER_MSEC)
 
 #if defined(CONFIG_SOC_NRF5340_CPUAPP) || defined(CONFIG_SOC_NRF5340_CPUNET)
+#if defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
+#define EUI64_ADDR (NRF_FICR_S->INFO.DEVICEID)
+#else
 #define EUI64_ADDR (NRF_FICR->INFO.DEVICEID)
+#endif
 #else
 #define EUI64_ADDR (NRF_FICR->DEVICEID)
 #endif
@@ -89,7 +98,16 @@ static void nrf5_get_eui64(uint8_t *mac)
 
 #if defined(CONFIG_SOC_NRF5340_CPUAPP) && \
 	defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
-#error Accessing EUI64 on the non-secure mode is not supported at the moment
+	int ret = -EPERM;
+#if defined(CONFIG_SPM)
+	ret = spm_request_read(&factoryAddress, (uint32_t)EUI64_ADDR,
+				   sizeof(factoryAddress));
+#endif
+	if (ret != 0) {
+		LOG_ERR("Unable to read EUI64 from the secure zone.")
+		LOG_ERR("Setting EUI64 to 0");
+		factoryAddress = 0ULL;
+	}
 #else
 	/* Use device identifier assigned during the production. */
 	factoryAddress = (uint64_t)EUI64_ADDR[0] << 32;

nrfconnect/sdk-zephyr

commit dc7bdd1fd8c00a0dfd16faa0a4dd329a9ef06df6

[nrf noup] drivers: ieee802154: Fix build error on nRF5340
This commit adds a semicolon to fix a trivial build error for nRF5340.

It fixes a bug introduced in 0ab7df74b22af2f06be7142741975add6a482b42
and shall be squashed.

Signed-off-by: Pawel Kwiek <pawel.kwiek@nordicsemi.no>

drivers/ieee802154/ieee802154_nrf5.c
@@ -104,7 +104,7 @@ static void nrf5_get_eui64(uint8_t *mac)
 				   sizeof(factoryAddress));
 #endif
 	if (ret != 0) {
-		LOG_ERR("Unable to read EUI64 from the secure zone.")
+		LOG_ERR("Unable to read EUI64 from the secure zone.");
 		LOG_ERR("Setting EUI64 to 0");
 		factoryAddress = 0ULL;
 	}