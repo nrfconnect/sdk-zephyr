zephyrproject-rtos/zephyr

commit a8c18ff5f4ace13fd8a296112d6521cbd7b19a7c

tests: Bluetooth: df: Fix build error, missing antenna config in DTS
The DF tests are implemented to be executed with nrf52_bsim platform.
The nrf52_bsim platform does not include Direction Finding Extenstion.
Due to that, radio_df.c compilation failed with error about missing
antenna configuration in DTS.

To solve the problem, I've changed nrf.cmake to include radio_df.c
file when CONFIG_BT_CTLR_DF is defined and CONFIG_SOC_SERIES_BSIM_NRFXX
is not definded.

Thanks to that any other platform is not affected. The file will not
build if there is no appropriate configuration or there are missing
features in a hardwared.

Unit tests have provided stub imlpementation or radio functionality.
If nrf52_bsim has implemented Direction Finding Extension, the
DF unit tests code will stil work and will not require additional
changes. Also content of the file is not affected by contional
compilation entries.

Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nrf.cmake
@@ -55,7 +55,10 @@ if(CONFIG_BT_LL_SW_SPLIT)
   zephyr_library_sources_ifdef(
     CONFIG_BT_CTLR_DF
     ll_sw/nordic/lll/lll_df.c
-    ll_sw/nordic/hal/nrf5/radio/radio_df.c
+    )
+  zephyr_library_sources_ifdef(
+    (CONFIG_BT_CTLR_DF AND NOT CONFIG_SOC_SERIES_BSIM_NRFXX)
+    zephyr_library_sources(ll_sw/nordic/hal/nrf5/radio/radio_df.c)
     )
   zephyr_library_include_directories(
     ll_sw/nordic/lll

tests/bluetooth/df/CMakeLists.txt
@@ -5,7 +5,7 @@
 
 cmake_minimum_required(VERSION 3.13.1)
 find_package(Zephyr REQUIRED HINTS $ENV{ZEPHYR_BASE})
-project(df)
+project(bluetooth_df)
 
 FILE(GLOB app_sources src/*.c)
 target_sources(app PRIVATE ${app_sources})

tests/bluetooth/df/src/radio_df_stub.c
@@ -1,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <stdint.h>
+
+/* Stub functitons that does nothing. Just to avoid liker complains. */
+void radio_df_ant_configure(void)
+{
+
+}
+
+uint8_t radio_df_ant_num_get(void)
+{
+	return 0;
+}

nrfconnect/sdk-zephyr

commit 8f33303b3980afcf7e3170104ed8fbfaa3caedb1

[nrf fromtree] Bluetooth: controller: ll_sw: Fix wrong cmake code that ...
adds radio_df.c

Fix error introduced by pull request:
https://github.com/zephyrproject-rtos/zephyr/pull/31591

Zephyrs CMake extension function zephyr_library_sources_ifdef()
does not allow to use complex conditions to toggle if
sources should be included in build or not.
It allows to use only single variable as an input for
feature_toggle argument because it is double-expanded
in contition statement in the function.

In case there is a requirement that source file is included
when more complicated condition is true we have to fall-back
to reqular if() statement and zephyr_library_sources().

The fix is required because without it radio_df.c source
is never compiled. If DF functionality is enabled build
will fail because of missing functions implementation.

Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nrf.cmake
@@ -56,10 +56,9 @@ if(CONFIG_BT_LL_SW_SPLIT)
     CONFIG_BT_CTLR_DF
     ll_sw/nordic/lll/lll_df.c
     )
-  zephyr_library_sources_ifdef(
-    (CONFIG_BT_CTLR_DF AND NOT CONFIG_SOC_SERIES_BSIM_NRFXX)
+  if(CONFIG_BT_CTLR_DF AND NOT CONFIG_SOC_SERIES_BSIM_NRFXX)
     zephyr_library_sources(ll_sw/nordic/hal/nrf5/radio/radio_df.c)
-    )
+  endif()
   zephyr_library_include_directories(
     ll_sw/nordic/lll
     )