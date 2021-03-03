cc13ba54fb8dfe809b0b78ab7afacb79126e3123
Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>
2021-03-03 08:58:47 +0200

    [nrf noup] posix socket: MSG_TRUNC and MSG_WAITALL added
    Needed by: https://github.com/nrfconnect/sdk-nrf/pull/4074
    To be eventually squashed to: 38a90a7768511cbe769d04ce827e160d8fa554b8
    
    Signed-off-by: Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>

--- include/posix/sys/socket.h
+++ include/posix/sys/socket.h
@@ -29,6 +29,8 @@ static inline int socketpair(int family, int type, int proto, int sv[2])
 
 #define MSG_PEEK ZSOCK_MSG_PEEK
 #define MSG_DONTWAIT ZSOCK_MSG_DONTWAIT
+#define MSG_TRUNC ZSOCK_MSG_TRUNC
+#define MSG_WAITALL ZSOCK_MSG_WAITALL
 
 static inline int shutdown(int sock, int how)
 {
