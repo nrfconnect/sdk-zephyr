zephyrproject-rtos/zephyr

commit a77f809faa47ff9922692d2b4eab33062b8a2b4d

Bluetooth: Align error handling of send functions
This aligns the error handling of send function to never unref the
buffer in place so the caller retain the ownership of the buffer
whenever there is an error.

Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

subsys/bluetooth/host/l2cap.c
@@ -1748,6 +1748,13 @@ static int l2cap_chan_le_send(struct bt_l2cap_le_chan *ch,
 		BT_WARN("Unable to send seg %d", err);
 		atomic_inc(&ch->tx.credits);
 
+		/* If the segment is not the original buffer release it since it
+		 * won't be needed anymore.
+		 */
+		if (seg != buf) {
+			net_buf_unref(seg);
+		}
+
 		if (err == -ENOBUFS) {
 			/* Restore state since segment could not be sent */
 			net_buf_simple_restore(&buf->b, &state);

subsys/bluetooth/host/l2cap_br.c
@@ -1321,9 +1321,7 @@ int bt_l2cap_br_chan_send(struct bt_l2cap_chan *chan, struct net_buf *buf)
 		return -EMSGSIZE;
 	}
 
-	bt_l2cap_send(ch->chan.conn, ch->tx.cid, buf);
-
-	return buf->len;
+	return bt_l2cap_send_cb(ch->chan.conn, ch->tx.cid, buf, NULL, NULL);
 }
 
 static int l2cap_br_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)

subsys/bluetooth/host/l2cap_internal.h
@@ -300,8 +300,7 @@ struct net_buf *bt_l2cap_create_rsp(struct net_buf *buf, size_t reserve);
 
 /* Send L2CAP PDU over a connection
  *
- * Buffer ownership is transferred to stack so either in case of success
- * or error the buffer will be unref internally.
+ * Buffer ownership is transferred to stack in case of success
  *
  * Calling this from RX thread is assumed to never fail so the return can be
  * ignored.

subsys/bluetooth/host/att.c
@@ -196,23 +196,12 @@ static int chan_send(struct bt_att_chan *chan, struct net_buf *buf,
 
 	chan->sent = cb ? cb : chan_cb(buf);
 
-	/* bt_l2cap_send_cb takes onwership of the buffer so take another
-	 * reference to restore the state in case an error is returned.
-	 */
-	net_buf_ref(buf);
-
 	err = bt_l2cap_send_cb(chan->att->conn, BT_L2CAP_CID_ATT,
 			       buf, att_cb(chan->sent),
 			       &chan->chan.chan);
 	if (err) {
-		/* In case of an error has occurred restore the buffer state as
-		 * the extra reference shall have prevented the buffer to be
-		 * freed.
-		 */
+		/* In case of an error has occurred restore the buffer state */
 		net_buf_simple_restore(&buf->b, &state);
-	} else {
-		/* In case of success unref the extra reference taken */
-		net_buf_unref(buf);
 	}
 
 	return err;

subsys/bluetooth/host/avdtp.c
@@ -66,6 +66,7 @@ static int avdtp_send(struct bt_avdtp *session,
 	result = bt_l2cap_chan_send(&session->br_chan.chan, buf);
 	if (result < 0) {
 		BT_ERR("Error:L2CAP send fail - result = %d", result);
+		net_buf_unref(buf);
 		return result;
 	}
 

subsys/bluetooth/host/conn.c
@@ -1022,7 +1022,6 @@ int bt_conn_send_cb(struct bt_conn *conn, struct net_buf *buf,
 
 	if (conn->state != BT_CONN_CONNECTED) {
 		BT_ERR("not connected!");
-		net_buf_unref(buf);
 		return -ENOTCONN;
 	}
 
@@ -1030,14 +1029,12 @@ int bt_conn_send_cb(struct bt_conn *conn, struct net_buf *buf,
 		tx = conn_tx_alloc();
 		if (!tx) {
 			BT_ERR("Unable to allocate TX context");
-			net_buf_unref(buf);
 			return -ENOBUFS;
 		}
 
 		/* Verify that we're still connected after blocking */
 		if (conn->state != BT_CONN_CONNECTED) {
 			BT_WARN("Disconnected while allocating context");
-			net_buf_unref(buf);
 			tx_free(tx);
 			return -ENOTCONN;
 		}

subsys/bluetooth/host/conn_internal.h
@@ -218,7 +218,13 @@ void bt_conn_reset_rx_state(struct bt_conn *conn);
 /* Process incoming data for a connection */
 void bt_conn_recv(struct bt_conn *conn, struct net_buf *buf, uint8_t flags);
 
-/* Send data over a connection */
+/* Send data over a connection
+ *
+ * Buffer ownership is transferred to stack in case of success.
+ *
+ * Calling this from RX thread is assumed to never fail so the return can be
+ * ignored.
+ */
 int bt_conn_send_cb(struct bt_conn *conn, struct net_buf *buf,
 		    bt_conn_tx_cb_t cb, void *user_data);
 

subsys/bluetooth/host/rfcomm.c
@@ -317,6 +317,18 @@ struct net_buf *bt_rfcomm_create_pdu(struct net_buf_pool *pool)
 				  sizeof(struct bt_rfcomm_hdr) + 1);
 }
 
+static int rfcomm_send(struct bt_rfcomm_session *session, struct net_buf *buf)
+{
+	int err;
+
+	err = bt_l2cap_chan_send(&session->br_chan.chan, buf);
+	if (err < 0) {
+		net_buf_unref(buf);
+	}
+
+	return err;
+}
+
 static int rfcomm_send_sabm(struct bt_rfcomm_session *session, uint8_t dlci)
 {
 	struct bt_rfcomm_hdr *hdr;
@@ -334,7 +346,7 @@ static int rfcomm_send_sabm(struct bt_rfcomm_session *session, uint8_t dlci)
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_NON_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&session->br_chan.chan, buf);
+	return rfcomm_send(session, buf);
 }
 
 static int rfcomm_send_disc(struct bt_rfcomm_session *session, uint8_t dlci)
@@ -355,7 +367,7 @@ static int rfcomm_send_disc(struct bt_rfcomm_session *session, uint8_t dlci)
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_NON_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&session->br_chan.chan, buf);
+	return rfcomm_send(session, buf);
 }
 
 static void rfcomm_session_disconnect(struct bt_rfcomm_session *session)
@@ -502,7 +514,7 @@ static int rfcomm_send_dm(struct bt_rfcomm_session *session, uint8_t dlci)
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_NON_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&session->br_chan.chan, buf);
+	return rfcomm_send(session, buf);
 }
 
 static void rfcomm_check_fc(struct bt_rfcomm_dlc *dlc)
@@ -558,10 +570,9 @@ static void rfcomm_dlc_tx_thread(void *p1, void *p2, void *p3)
 			break;
 		}
 
-		if (bt_l2cap_chan_send(&dlc->session->br_chan.chan, buf) < 0) {
+		if (rfcomm_send(dlc->session, buf) < 0) {
 			/* This fails only if channel is disconnected */
 			dlc->state = BT_RFCOMM_STATE_DISCONNECTED;
-			net_buf_unref(buf);
 			break;
 		}
 
@@ -608,7 +619,7 @@ static int rfcomm_send_ua(struct bt_rfcomm_session *session, uint8_t dlci)
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_NON_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&session->br_chan.chan, buf);
+	return rfcomm_send(session, buf);
 }
 
 static int rfcomm_send_msc(struct bt_rfcomm_dlc *dlc, uint8_t cr,
@@ -629,7 +640,7 @@ static int rfcomm_send_msc(struct bt_rfcomm_dlc *dlc, uint8_t cr,
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&dlc->session->br_chan.chan, buf);
+	return rfcomm_send(dlc->session, buf);
 }
 
 static int rfcomm_send_rls(struct bt_rfcomm_dlc *dlc, uint8_t cr,
@@ -650,7 +661,7 @@ static int rfcomm_send_rls(struct bt_rfcomm_dlc *dlc, uint8_t cr,
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&dlc->session->br_chan.chan, buf);
+	return rfcomm_send(dlc->session, buf);
 }
 
 static int rfcomm_send_rpn(struct bt_rfcomm_session *session, uint8_t cr,
@@ -666,7 +677,7 @@ static int rfcomm_send_rpn(struct bt_rfcomm_session *session, uint8_t cr,
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&session->br_chan.chan, buf);
+	return rfcomm_send(session, buf);
 }
 
 static int rfcomm_send_test(struct bt_rfcomm_session *session, uint8_t cr,
@@ -682,7 +693,7 @@ static int rfcomm_send_test(struct bt_rfcomm_session *session, uint8_t cr,
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&session->br_chan.chan, buf);
+	return rfcomm_send(session, buf);
 }
 
 static int rfcomm_send_nsc(struct bt_rfcomm_session *session, uint8_t cmd_type)
@@ -698,7 +709,7 @@ static int rfcomm_send_nsc(struct bt_rfcomm_session *session, uint8_t cmd_type)
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&session->br_chan.chan, buf);
+	return rfcomm_send(session, buf);
 }
 
 static int rfcomm_send_fcon(struct bt_rfcomm_session *session, uint8_t cr)
@@ -711,7 +722,7 @@ static int rfcomm_send_fcon(struct bt_rfcomm_session *session, uint8_t cr)
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&session->br_chan.chan, buf);
+	return rfcomm_send(session, buf);
 }
 
 static int rfcomm_send_fcoff(struct bt_rfcomm_session *session, uint8_t cr)
@@ -724,7 +735,7 @@ static int rfcomm_send_fcoff(struct bt_rfcomm_session *session, uint8_t cr)
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&session->br_chan.chan, buf);
+	return rfcomm_send(session, buf);
 }
 
 static void rfcomm_dlc_connected(struct bt_rfcomm_dlc *dlc)
@@ -926,7 +937,7 @@ static int rfcomm_send_pn(struct bt_rfcomm_dlc *dlc, uint8_t cr)
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&dlc->session->br_chan.chan, buf);
+	return rfcomm_send(dlc->session, buf);
 }
 
 static int rfcomm_send_credit(struct bt_rfcomm_dlc *dlc, uint8_t credits)
@@ -949,7 +960,7 @@ static int rfcomm_send_credit(struct bt_rfcomm_dlc *dlc, uint8_t credits)
 	fcs = rfcomm_calc_fcs(BT_RFCOMM_FCS_LEN_UIH, buf->data);
 	net_buf_add_u8(buf, fcs);
 
-	return bt_l2cap_chan_send(&dlc->session->br_chan.chan, buf);
+	return rfcomm_send(dlc->session, buf);
 }
 
 static int rfcomm_dlc_start(struct bt_rfcomm_dlc *dlc)

nrfconnect/sdk-zephyr

commit 90e64e4723a19d3d42012e514f42398eb4a8acd8

[nrf fromlist] Bluetooth: host: Fix buffer leak trying to send when
...disconnected

Upstream PR: https://github.com/zephyrproject-rtos/zephyr/pull/34710

Fix buffer leak in L2CAP and SMP when attempting to send over a
a connection that has been disconnected.
Both L2CAP and SMP now have ownership of the buffer in case of failure
to send it and need to unref it in case of send failure.

Change the return type of bt_l2cap_send since the user of this function
has to handle releasing of the buffer when this fails.

Regression from: a77f809faa47ff9922692d2b4eab33062b8a2b4d

Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>
(cherry picked from commit 86799072118f8929c0f51dacc6b4dd92209d4d30)
Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

subsys/bluetooth/host/l2cap.c
@@ -427,10 +427,27 @@ static struct net_buf *l2cap_create_le_sig_pdu(struct net_buf *buf,
 	return buf;
 }
 
+/* Send the buffer and release it in case of failure.
+ * Any other cleanup in failure to send should be handled by the disconnected
+ * handler.
+ */
+static inline void l2cap_send(struct bt_conn *conn, uint16_t cid,
+			      struct net_buf *buf)
+{
+	if (bt_l2cap_send(conn, cid, buf)) {
+		net_buf_unref(buf);
+	}
+}
+
 #if defined(CONFIG_BT_L2CAP_DYNAMIC_CHANNEL)
 static void l2cap_chan_send_req(struct bt_l2cap_chan *chan,
 				struct net_buf *buf, k_timeout_t timeout)
 {
+	if (bt_l2cap_send(chan->conn, BT_L2CAP_CID_LE_SIG, buf)) {
+		net_buf_unref(buf);
+		return;
+	}
+
 	/* BLUETOOTH SPECIFICATION Version 4.2 [Vol 3, Part A] page 126:
 	 *
 	 * The value of this timer is implementation-dependent but the minimum
@@ -441,8 +458,6 @@ static void l2cap_chan_send_req(struct bt_l2cap_chan *chan,
 	 * link is lost.
 	 */
 	k_delayed_work_submit(&chan->rtx_work, timeout);
-
-	bt_l2cap_send(chan->conn, BT_L2CAP_CID_LE_SIG, buf);
 }
 
 static int l2cap_le_conn_req(struct bt_l2cap_le_chan *ch)
@@ -619,7 +634,7 @@ static void l2cap_send_reject(struct bt_conn *conn, uint8_t ident,
 		net_buf_add_mem(buf, data, data_len);
 	}
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
+	l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
 }
 
 static void le_conn_param_rsp(struct bt_l2cap *l2cap, struct net_buf *buf)
@@ -678,7 +693,7 @@ static void le_conn_param_update_req(struct bt_l2cap *l2cap, uint8_t ident,
 		rsp->result = sys_cpu_to_le16(BT_L2CAP_CONN_PARAM_REJECTED);
 	}
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
+	l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
 
 	if (accepted) {
 		bt_conn_le_conn_update(conn, &param);
@@ -1083,7 +1098,7 @@ static void le_conn_req(struct bt_l2cap *l2cap, uint8_t ident,
 	rsp->result = BT_L2CAP_LE_SUCCESS;
 
 rsp:
-	bt_l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
+	l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
 }
 
 #if defined(CONFIG_BT_L2CAP_ECRED)
@@ -1187,7 +1202,7 @@ response:
 
 	rsp->result = sys_cpu_to_le16(result);
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
+	l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
 }
 
 static void le_ecred_reconf_req(struct bt_l2cap *l2cap, uint8_t ident,
@@ -1262,7 +1277,7 @@ response:
 	rsp = net_buf_add(buf, sizeof(*rsp));
 	rsp->result = sys_cpu_to_le16(result);
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
+	l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
 }
 #endif /* defined(CONFIG_BT_L2CAP_ECRED) */
 
@@ -1331,7 +1346,7 @@ static void le_disconn_req(struct bt_l2cap *l2cap, uint8_t ident,
 
 	bt_l2cap_chan_del(&chan->chan);
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
+	l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
 }
 
 static int l2cap_change_security(struct bt_l2cap_le_chan *chan, uint16_t err)
@@ -2038,7 +2053,7 @@ static void l2cap_chan_send_credits(struct bt_l2cap_le_chan *chan,
 	ev->cid = sys_cpu_to_le16(chan->rx.cid);
 	ev->credits = sys_cpu_to_le16(credits);
 
-	bt_l2cap_send(chan->chan.conn, BT_L2CAP_CID_LE_SIG, buf);
+	l2cap_send(chan->chan.conn, BT_L2CAP_CID_LE_SIG, buf);
 
 	BT_DBG("chan %p credits %u", chan, atomic_get(&chan->rx.credits));
 }
@@ -2319,6 +2334,7 @@ int bt_l2cap_update_conn_param(struct bt_conn *conn,
 {
 	struct bt_l2cap_conn_param_req *req;
 	struct net_buf *buf;
+	int err;
 
 	buf = l2cap_create_le_sig_pdu(NULL, BT_L2CAP_CONN_PARAM_REQ,
 				      get_ident(), sizeof(*req));
@@ -2332,7 +2348,11 @@ int bt_l2cap_update_conn_param(struct bt_conn *conn,
 	req->latency = sys_cpu_to_le16(param->latency);
 	req->timeout = sys_cpu_to_le16(param->timeout);
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
+	err = bt_l2cap_send(conn, BT_L2CAP_CID_LE_SIG, buf);
+	if (err) {
+		net_buf_unref(buf);
+		return err;
+	}
 
 	return 0;
 }

subsys/bluetooth/host/l2cap_br.c
@@ -220,9 +220,27 @@ static uint8_t l2cap_br_get_ident(void)
 	return ident;
 }
 
+/* Send the buffer and release it in case of failure.
+ * Any other cleanup in failure to send should be handled by the disconnected
+ * handler.
+ */
+static inline void l2cap_send(struct bt_conn *conn, uint16_t cid,
+			      struct net_buf *buf)
+{
+	if (bt_l2cap_send(conn, cid, buf)) {
+		net_buf_unref(buf);
+	}
+}
+
 static void l2cap_br_chan_send_req(struct bt_l2cap_br_chan *chan,
 				   struct net_buf *buf, k_timeout_t timeout)
 {
+
+	if (bt_l2cap_send(chan->chan.conn, BT_L2CAP_CID_BR_SIG, buf)) {
+		net_buf_unref(buf);
+		return;
+	}
+
 	/* BLUETOOTH SPECIFICATION Version 4.2 [Vol 3, Part A] page 126:
 	 *
 	 * The value of this timer is implementation-dependent but the minimum
@@ -233,8 +251,6 @@ static void l2cap_br_chan_send_req(struct bt_l2cap_br_chan *chan,
 	 * link is lost.
 	 */
 	k_delayed_work_submit(&chan->chan.rtx_work, timeout);
-
-	bt_l2cap_send(chan->chan.conn, BT_L2CAP_CID_BR_SIG, buf);
 }
 
 static void l2cap_br_get_info(struct bt_l2cap_br *l2cap, uint16_t info_type)
@@ -432,7 +448,7 @@ static int l2cap_br_info_req(struct bt_l2cap_br *l2cap, uint8_t ident,
 		break;
 	}
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_BR_SIG, rsp_buf);
+	l2cap_send(conn, BT_L2CAP_CID_BR_SIG, rsp_buf);
 
 	return 0;
 }
@@ -640,7 +656,7 @@ static void l2cap_br_send_conn_rsp(struct bt_conn *conn, uint16_t scid,
 		rsp->status = sys_cpu_to_le16(BT_L2CAP_CS_NO_INFO);
 	}
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_BR_SIG, buf);
+	l2cap_send(conn, BT_L2CAP_CID_BR_SIG, buf);
 }
 
 static int l2cap_br_conn_req_reply(struct bt_l2cap_chan *chan, uint16_t result)
@@ -879,7 +895,7 @@ static void l2cap_br_send_reject(struct bt_conn *conn, uint8_t ident,
 		net_buf_add_mem(buf, data, data_len);
 	}
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_BR_SIG, buf);
+	l2cap_send(conn, BT_L2CAP_CID_BR_SIG, buf);
 }
 
 static uint16_t l2cap_br_conf_opt_mtu(struct bt_l2cap_chan *chan,
@@ -1008,7 +1024,7 @@ send_rsp:
 
 	hdr->len = sys_cpu_to_le16(buf->len - sizeof(*hdr));
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_BR_SIG, buf);
+	l2cap_send(conn, BT_L2CAP_CID_BR_SIG, buf);
 
 	if (result != BT_L2CAP_CONF_SUCCESS) {
 		return;
@@ -1096,7 +1112,7 @@ static void l2cap_br_disconn_req(struct bt_l2cap_br *l2cap, uint8_t ident,
 
 	bt_l2cap_chan_del(&chan->chan);
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_BR_SIG, buf);
+	l2cap_send(conn, BT_L2CAP_CID_BR_SIG, buf);
 }
 
 static void l2cap_br_connected(struct bt_l2cap_chan *chan)

subsys/bluetooth/host/l2cap_internal.h
@@ -305,10 +305,10 @@ struct net_buf *bt_l2cap_create_rsp(struct net_buf *buf, size_t reserve);
 int bt_l2cap_send_cb(struct bt_conn *conn, uint16_t cid, struct net_buf *buf,
 		     bt_conn_tx_cb_t cb, void *user_data);
 
-static inline void bt_l2cap_send(struct bt_conn *conn, uint16_t cid,
-				 struct net_buf *buf)
+static inline int bt_l2cap_send(struct bt_conn *conn, uint16_t cid,
+				struct net_buf *buf)
 {
-	bt_l2cap_send_cb(conn, cid, buf, NULL, NULL);
+	return bt_l2cap_send_cb(conn, cid, buf, NULL, NULL);
 }
 
 /* Receive a new L2CAP PDU from a connection */

subsys/bluetooth/host/smp.c
@@ -1017,8 +1017,11 @@ static void smp_br_timeout(struct k_work *work)
 static void smp_br_send(struct bt_smp_br *smp, struct net_buf *buf,
 			bt_conn_tx_cb_t cb)
 {
-	bt_l2cap_send_cb(smp->chan.chan.conn, BT_L2CAP_CID_BR_SMP, buf, cb,
-			 NULL);
+	if (bt_l2cap_send_cb(smp->chan.chan.conn, BT_L2CAP_CID_BR_SMP, buf, cb, NULL)) {
+		net_buf_unref(buf);
+		return;
+	}
+
 	k_delayed_work_submit(&smp->work, SMP_TIMEOUT);
 }
 
@@ -1575,7 +1578,9 @@ static int smp_br_error(struct bt_smp_br *smp, uint8_t reason)
 	 * SMP timer is not restarted for PairingFailed so don't use
 	 * smp_br_send
 	 */
-	bt_l2cap_send(smp->chan.chan.conn, BT_L2CAP_CID_SMP, buf);
+	if (bt_l2cap_send(smp->chan.chan.conn, BT_L2CAP_CID_SMP, buf)) {
+		net_buf_unref(buf);
+	}
 
 	return 0;
 }
@@ -1893,7 +1898,11 @@ static void smp_timeout(struct k_work *work)
 static void smp_send(struct bt_smp *smp, struct net_buf *buf,
 		     bt_conn_tx_cb_t cb, void *user_data)
 {
-	bt_l2cap_send_cb(smp->chan.chan.conn, BT_L2CAP_CID_SMP, buf, cb, NULL);
+	if (bt_l2cap_send_cb(smp->chan.chan.conn, BT_L2CAP_CID_SMP, buf, cb, NULL)) {
+		net_buf_unref(buf);
+		return;
+	}
+
 	k_delayed_work_submit(&smp->work, SMP_TIMEOUT);
 }
 
@@ -1918,7 +1927,9 @@ static int smp_error(struct bt_smp *smp, uint8_t reason)
 	rsp->reason = reason;
 
 	/* SMP timer is not restarted for PairingFailed so don't use smp_send */
-	bt_l2cap_send(smp->chan.chan.conn, BT_L2CAP_CID_SMP, buf);
+	if (bt_l2cap_send(smp->chan.chan.conn, BT_L2CAP_CID_SMP, buf)) {
+		net_buf_unref(buf);
+	}
 
 	return 0;
 }
@@ -2875,6 +2886,7 @@ static int smp_send_security_req(struct bt_conn *conn)
 	struct bt_smp *smp;
 	struct bt_smp_security_request *req;
 	struct net_buf *req_buf;
+	int err;
 
 	BT_DBG("");
 	smp = smp_chan_get(conn);
@@ -2922,7 +2934,11 @@ static int smp_send_security_req(struct bt_conn *conn)
 	req->auth_req = get_auth(conn, BT_SMP_AUTH_DEFAULT);
 
 	/* SMP timer is not restarted for SecRequest so don't use smp_send */
-	bt_l2cap_send(conn, BT_L2CAP_CID_SMP, req_buf);
+	err = bt_l2cap_send(conn, BT_L2CAP_CID_SMP, req_buf);
+	if (err) {
+		net_buf_unref(req_buf);
+		return err;
+	}
 
 	atomic_set_bit(smp->flags, SMP_FLAG_SEC_REQ);
 	atomic_set_bit(smp->allowed_cmds, BT_SMP_CMD_PAIRING_REQ);

subsys/bluetooth/host/smp_null.c
@@ -39,11 +39,14 @@ int bt_smp_sign(struct bt_conn *conn, struct net_buf *buf)
 	return -ENOTSUP;
 }
 
-static int bt_smp_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
+static int bt_smp_recv(struct bt_l2cap_chan *chan, struct net_buf *req_buf)
 {
 	struct bt_conn *conn = chan->conn;
 	struct bt_smp_pairing_fail *rsp;
 	struct bt_smp_hdr *hdr;
+	struct net_buf *buf;
+
+	ARG_UNUSED(req_buf);
 
 	/* If a device does not support pairing then it shall respond with
 	 * a Pairing Failed command with the reason set to "Pairing Not
@@ -60,7 +63,9 @@ static int bt_smp_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
 	rsp = net_buf_add(buf, sizeof(*rsp));
 	rsp->reason = BT_SMP_ERR_PAIRING_NOTSUPP;
 
-	bt_l2cap_send(conn, BT_L2CAP_CID_SMP, buf);
+	if (bt_l2cap_send(conn, BT_L2CAP_CID_SMP, buf)) {
+		net_buf_unref(buf);
+	}
 
 	return 0;
 }