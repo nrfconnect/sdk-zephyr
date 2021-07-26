478f697276c9297e3b29a0cc329517f10b264070
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-07-26 13:08:38 -0700

    Revert "[nrf fromtree] drivers: ieee802154: include timestamp in acks"
    This reverts commit 0b0c867e74b2e93e77381ce630fd9c2774bd5292.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- drivers/ieee802154/ieee802154_nrf5.c
+++ drivers/ieee802154/ieee802154_nrf5.c
@@ -431,15 +431,6 @@ static int handle_ack(struct nrf5_802154_data *nrf5_radio)
 	net_pkt_set_ieee802154_lqi(ack_pkt, nrf5_radio->ack_frame.lqi);
 	net_pkt_set_ieee802154_rssi(ack_pkt, nrf5_radio->ack_frame.rssi);
 
-#if defined(CONFIG_NET_PKT_TIMESTAMP)
-	struct net_ptp_time timestamp = {
-		.second = nrf5_radio->ack_frame.time / USEC_PER_SEC,
-		.nanosecond = (nrf5_radio->ack_frame.time % USEC_PER_SEC) * NSEC_PER_USEC
-	};
-
-	net_pkt_set_timestamp(ack_pkt, &timestamp);
-#endif
-
 	net_pkt_cursor_init(ack_pkt);
 
 	if (ieee802154_radio_handle_ack(nrf5_radio->iface, ack_pkt) != NET_OK) {
@@ -892,10 +883,12 @@ void nrf_802154_tx_ack_started(const uint8_t *data)
 				data[FRAME_PENDING_BYTE] & FRAME_PENDING_BIT;
 }
 
-#if defined(CONFIG_NRF_802154_SER_HOST)
-void nrf_802154_transmitted_raw(const uint8_t *frame, uint8_t *ack, int8_t power, uint8_t lqi)
+void nrf_802154_transmitted_raw(const uint8_t *frame, uint8_t *ack,
+				int8_t power, uint8_t lqi)
 {
 	ARG_UNUSED(frame);
+	ARG_UNUSED(power);
+	ARG_UNUSED(lqi);
 
 	nrf5_data.tx_result = NRF_802154_TX_ERROR_NONE;
 	nrf5_data.ack_frame.psdu = ack;
@@ -904,26 +897,6 @@ void nrf_802154_transmitted_raw(const uint8_t *frame, uint8_t *ack, int8_t power
 
 	k_sem_give(&nrf5_data.tx_wait);
 }
-#else
-void nrf_802154_transmitted_timestamp_raw(const uint8_t *frame, uint8_t *ack, int8_t power,
-					  uint8_t lqi, uint32_t ack_time)
-{
-	ARG_UNUSED(frame);
-	ARG_UNUSED(ack_time);
-
-	nrf5_data.tx_result = NRF_802154_TX_ERROR_NONE;
-	nrf5_data.ack_frame.psdu = ack;
-	nrf5_data.ack_frame.rssi = power;
-	nrf5_data.ack_frame.lqi = lqi;
-
-#if defined(CONFIG_NET_PKT_TIMESTAMP)
-	nrf5_data.ack_frame.time =
-		nrf_802154_first_symbol_timestamp_get(ack_time, nrf5_data.ack_frame.psdu[0]);
-#endif
-
-	k_sem_give(&nrf5_data.tx_wait);
-}
-#endif
 
 void nrf_802154_transmit_failed(const uint8_t *frame,
 				nrf_802154_tx_error_t error)
