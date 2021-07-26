32fec5fc00242fce1fe6f5f960fd3fb13114b758
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-07-26 13:58:18 -0700

    Revert "[nrf noup] net: sockets: Add new DTLS and RAI socket options"
    This reverts commit 5b2f44fa727e02361d984784bcecf5090be009da.
    
    The NCS socket extensions will be reverted and reapplied in a single
    commit after the upmerge.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- include/net/socket_ncs.h
+++ include/net/socket_ncs.h
@@ -44,30 +44,11 @@ enum net_local_protocol {
  *  - 0 - Disabled.
  *  - 1 - Enabled.
  */
-#define TLS_SESSION_CACHE 10
-/** Socket option to purge session cache immediately.
- *  This option accepts any value.
- */
-#define TLS_SESSION_CACHE_PURGE 11
-/** Socket option to set DTLS handshake timeout, specifically for nRF sockets.
- *  The option accepts an integer, indicating the total handshake timeout,
- *  including retransmissions, in seconds.
- *  Accepted values for the option are: 1, 3, 7, 15, 31, 63, 123.
- */
-#define TLS_DTLS_HANDSHAKE_TIMEO 12
+#define TLS_SESSION_CACHE 8
 
 /* Valid values for TLS_SESSION_CACHE option */
 #define TLS_SESSION_CACHE_DISABLED 0 /**< Disable TLS session caching. */
 #define TLS_SESSION_CACHE_ENABLED 1 /**< Enable TLS session caching. */
-
-/* Valid values for TLS_DTLS_HANDSHAKE_TIMEO option */
-#define TLS_DTLS_HANDSHAKE_TIMEO_1S 1 /**< 1 second */
-#define TLS_DTLS_HANDSHAKE_TIMEO_3S 3 /**< 1s + 2s */
-#define TLS_DTLS_HANDSHAKE_TIMEO_7S 7 /**< 1s + 2s + 4s */
-#define TLS_DTLS_HANDSHAKE_TIMEO_15S 15 /**< 1s + 2s + 4s + 8s */
-#define TLS_DTLS_HANDSHAKE_TIMEO_31S 31 /**< 1s + 2s + 4s + 8s + 16s */
-#define TLS_DTLS_HANDSHAKE_TIMEO_63S 63 /**< 1s + 2s + 4s + 8s + 16s + 32s */
-#define TLS_DTLS_HANDSHAKE_TIMEO_123S 123 /**< 1s + 2s + 4s + 8s + 16s + 32s + 60s */
 
 /* NCS specific socket options */
 
@@ -77,29 +58,6 @@ enum net_local_protocol {
 #define SO_IP_ECHO_REPLY 31
 /** sockopt: disable IPv6 ICMP replies */
 #define SO_IPV6_ECHO_REPLY 32
-/** sockopt: Release Assistance Indication feature: This will indicate that the
- *  next call to send/sendto will be the last one for some time.
- */
-#define SO_RAI_LAST 50
-/** sockopt: Release Assistance Indication feature: This will indicate that the
- *  application will not send any more data.
- */
-#define SO_RAI_NO_DATA 51
-/** sockopt: Release Assistance Indication feature: This will indicate that
- *  after the next call to send/sendto, the application is expecting to receive
- *  one more data packet before this socket will not be used again for some time.
- */
-#define SO_RAI_ONE_RESP 52
-/** sockopt: Release Assistance Indication feature: If a client application
- *  expects to use the socket more it can indicate that by setting this socket
- *  option before the next send call which will keep the network up longer.
- */
-#define SO_RAI_ONGOING 53
-/** sockopt: Release Assistance Indication feature: If a server application
- *  expects to use the socket more it can indicate that by setting this socket
- *  option before the next send call.
- */
-#define SO_RAI_WAIT_MORE 54
 
 /* NCS specific PDN options */
 
