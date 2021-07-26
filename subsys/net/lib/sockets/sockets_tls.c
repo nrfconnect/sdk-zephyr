aed2d3967195c54e72448f7f6e9d3f4e466ad104
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-07-26 14:10:36 -0700

    Revert "[nrf fromlist] net: sockets: tls: Use secure random generator from Zephyr"
    This reverts commit 970ea065d03d7eabf9c92b00955adadb869f8908.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- subsys/net/lib/sockets/sockets_tls.c
+++ subsys/net/lib/sockets/sockets_tls.c
@@ -12,6 +12,7 @@
 LOG_MODULE_REGISTER(net_sock_tls, CONFIG_NET_SOCKETS_LOG_LEVEL);
 
 #include <init.h>
+#include <drivers/entropy.h>
 #include <sys/util.h>
 #include <net/socket.h>
 #include <random/rand32.h>
@@ -25,6 +26,7 @@ LOG_MODULE_REGISTER(net_sock_tls, CONFIG_NET_SOCKETS_LOG_LEVEL);
 #include CONFIG_MBEDTLS_CFG_FILE
 #endif /* CONFIG_MBEDTLS_CFG_FILE */
 
+#include <mbedtls/ctr_drbg.h>
 #include <mbedtls/net_sockets.h>
 #include <mbedtls/x509.h>
 #include <mbedtls/x509_crt.h>
@@ -161,6 +163,12 @@ __net_socket struct tls_context {
 #endif /* CONFIG_MBEDTLS */
 };
 
+#if defined(CONFIG_ENTROPY_HAS_DRIVER)
+static const struct device *entropy_dev;
+#endif
+
+static mbedtls_ctr_drbg_context tls_ctr_drbg;
+
 /* A global pool of TLS contexts. */
 static struct tls_context tls_contexts[CONFIG_NET_SOCKETS_TLS_MAX_CONTEXTS];
 
@@ -196,18 +204,37 @@ static void tls_debug(void *ctx, int level, const char *file,
 }
 #endif /* defined(MBEDTLS_DEBUG_C) && (CONFIG_NET_SOCKETS_LOG_LEVEL >= LOG_LEVEL_DBG) */
 
-static int tls_ctr_drbg_random(void *ctx, unsigned char *buf, size_t len)
+#if defined(CONFIG_ENTROPY_HAS_DRIVER)
+static int tls_entropy_func(void *ctx, unsigned char *buf, size_t len)
 {
 	ARG_UNUSED(ctx);
 
-#if defined(CONFIG_ENTROPY_HAS_DRIVER)
-	return sys_csrand_get(buf, len);
+	return entropy_get_entropy(entropy_dev, buf, len);
+}
 #else
-	sys_rand_get(buf, len);
+static int tls_entropy_func(void *ctx, unsigned char *buf, size_t len)
+{
+	ARG_UNUSED(ctx);
+
+	size_t i = len / 4;
+	uint32_t val;
+
+	while (i--) {
+		val = sys_rand32_get();
+		UNALIGNED_PUT(val, (uint32_t *)buf);
+		buf += 4;
+	}
+
+	i = len & 0x3;
+	val = sys_rand32_get();
+	while (i--) {
+		*buf++ = val;
+		val >>= 8;
+	}
 
 	return 0;
-#endif
 }
+#endif /* defined(CONFIG_ENTROPY_HAS_DRIVER) */
 
 #if defined(CONFIG_NET_SOCKETS_ENABLE_DTLS)
 /* mbedTLS-defined function for setting timer. */
@@ -260,14 +287,33 @@ static int tls_init(const struct device *unused)
 {
 	ARG_UNUSED(unused);
 
-#if !defined(CONFIG_ENTROPY_HAS_DRIVER)
+	int ret;
+	static const unsigned char drbg_seed[] = "zephyr";
+
+#if defined(CONFIG_ENTROPY_HAS_DRIVER)
+	entropy_dev = device_get_binding(DT_CHOSEN_ZEPHYR_ENTROPY_LABEL);
+	if (!entropy_dev) {
+		NET_ERR("Failed to obtain entropy device");
+		return -ENODEV;
+	}
+#else
 	NET_WARN("No entropy device on the system, "
 		 "TLS communication may be insecure!");
-#endif
+#endif /* defined(CONFIG_ENTROPY_HAS_DRIVER) */
 
 	(void)memset(tls_contexts, 0, sizeof(tls_contexts));
 
 	k_mutex_init(&context_lock);
+
+	mbedtls_ctr_drbg_init(&tls_ctr_drbg);
+
+	ret = mbedtls_ctr_drbg_seed(&tls_ctr_drbg, tls_entropy_func, NULL,
+				    drbg_seed, sizeof(drbg_seed));
+	if (ret != 0) {
+		mbedtls_ctr_drbg_free(&tls_ctr_drbg);
+		NET_ERR("TLS entropy source initialization failed");
+		return -EFAULT;
+	}
 
 #if defined(MBEDTLS_DEBUG_C) && (CONFIG_NET_SOCKETS_LOG_LEVEL >= LOG_LEVEL_DBG)
 	mbedtls_debug_set_threshold(CONFIG_MBEDTLS_DEBUG_LEVEL);
@@ -901,8 +947,8 @@ static int tls_mbedtls_init(struct tls_context *context, bool is_server)
 		/* Configure cookie for DTLS server */
 		if (role == MBEDTLS_SSL_IS_SERVER) {
 			ret = mbedtls_ssl_cookie_setup(&context->cookie,
-						       tls_ctr_drbg_random,
-						       NULL);
+						       mbedtls_ctr_drbg_random,
+						       &tls_ctr_drbg);
 			if (ret != 0) {
 				return -ENOMEM;
 			}
@@ -938,8 +984,8 @@ static int tls_mbedtls_init(struct tls_context *context, bool is_server)
 	}
 
 	mbedtls_ssl_conf_rng(&context->config,
-			     tls_ctr_drbg_random,
-			     NULL);
+			     mbedtls_ctr_drbg_random,
+			     &tls_ctr_drbg);
 
 	ret = tls_mbedtls_set_credentials(context);
 	if (ret != 0) {
