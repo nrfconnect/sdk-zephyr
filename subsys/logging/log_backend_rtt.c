f3f3bb7558736ebfd0d94001969cd0a50fd584f6
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-07-26 14:00:09 -0700

    Revert "[nrf fromtree] logging: backend_rtt: Align to changes in RTT_LOCK/UNLOCK"
    This reverts commit 46fdb8c9252d0e863200470b88cd33eeac9511f2.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- subsys/logging/log_backend_rtt.c
+++ subsys/logging/log_backend_rtt.c
@@ -154,11 +154,9 @@ static int line_out_drop_mode(void)
 		}
 	}
 
-	int ret;
-
 	RTT_LOCK();
-	ret = SEGGER_RTT_WriteSkipNoLock(CONFIG_LOG_BACKEND_RTT_BUFFER,
-					 line_buf, line_pos - line_buf);
+	int ret = SEGGER_RTT_WriteSkipNoLock(CONFIG_LOG_BACKEND_RTT_BUFFER,
+					     line_buf, line_pos - line_buf);
 	RTT_UNLOCK();
 
 	if (ret == 0) {
@@ -211,12 +209,12 @@ static int data_out_block_mode(uint8_t *data, size_t length, void *ctx)
 	do {
 		if (!is_sync_mode()) {
 			RTT_LOCK();
-			ret = SEGGER_RTT_WriteSkipNoLock(CONFIG_LOG_BACKEND_RTT_BUFFER,
-							 data, length);
+		}
+
+		ret = SEGGER_RTT_WriteSkipNoLock(CONFIG_LOG_BACKEND_RTT_BUFFER,
+						 data, length);
+		if (!is_sync_mode()) {
 			RTT_UNLOCK();
-		} else {
-			ret = SEGGER_RTT_WriteSkipNoLock(CONFIG_LOG_BACKEND_RTT_BUFFER,
-							 data, length);
 		}
 
 		if (ret) {
