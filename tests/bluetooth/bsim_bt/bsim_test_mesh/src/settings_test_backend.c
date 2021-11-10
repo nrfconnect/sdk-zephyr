7addc8186c11a939e2b4b5620a94345197aaa831
Robert Lubos <robert.lubos@nordicsemi.no>
2021-11-10 11:53:12 +0100

    Revert "[nrf fromtree] Bluetooth: Mesh: Fix overwriting existing entries in test"
    This reverts commit 8e3582918e31811db924f6edf762bda3a848df6f.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_mesh/src/settings_test_backend.c
+++ tests/bluetooth/bsim_bt/bsim_test_mesh/src/settings_test_backend.c
@@ -21,7 +21,6 @@
 LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
 #define SETTINGS_FILE "settings_data.log"
-#define SETTINGS_FILE_TMP "~settings_data.log"
 
 #define ENTRY_LEN_SIZE (4)
 #define ENTRY_NAME_MAX_LEN (SETTINGS_MAX_NAME_LEN + SETTINGS_EXTRA_LEN)
@@ -33,17 +32,18 @@ struct line_read_ctx {
 	const uint8_t *val;
 };
 
-static int entry_check_and_copy(FILE *fin, FILE *fout, const char *name)
+static int entry_check_and_seek(FILE *fp, const char *name)
 {
+	if (fseek(fp, 0, SEEK_SET) < 0) {
+		return -1;
+	}
+
 	char line[READ_LEN_MAX + 1];
 
-	while (fgets(line, sizeof(line), fin) == line) {
+	while (fgets(line, sizeof(line), fp) == line) {
 		if (strstr(line, name) != NULL) {
+			fseek(fp, -(strlen(line)), SEEK_CUR);
 			return 0;
-		}
-
-		if (fputs(line, fout) < 0) {
-			return -1;
 		}
 	};
 
@@ -54,13 +54,11 @@ static ssize_t settings_line_read_cb(void *cb_arg, void *data, size_t len)
 {
 	struct line_read_ctx *valctx = (struct line_read_ctx *)cb_arg;
 
-	if ((valctx->len / 2) > len) {
-		return -ENOBUFS;
+	if ((valctx->len / 2) != len) {
+		return 0;
 	}
 
 	uint8_t *n = (uint8_t *) data;
-
-	len = valctx->len / 2;
 
 	for (int i = 0; i < len; i++, n++) {
 		if (sscanf(&valctx->val[i * 2], "%2hhx", n) != 1) {
@@ -124,20 +122,13 @@ static int settings_custom_load(struct settings_store *cs, const struct settings
 static int settings_custom_save(struct settings_store *cs, const char *name,
 				const char *value, size_t val_len)
 {
-	FILE *fcur = fopen(SETTINGS_FILE, "r+");
-	FILE *fnew = NULL;
+	FILE *fp = fopen(SETTINGS_FILE, "r+");
 
-	if (fcur == NULL) {
-		fcur = fopen(SETTINGS_FILE, "w");
-	} else {
-		fnew = fopen(SETTINGS_FILE_TMP, "w");
-		if (fnew == NULL) {
-			LOG_ERR("Failed to create temporary file %s", SETTINGS_FILE_TMP);
-			return -1;
-		}
+	if (fp == NULL) {
+		fp = fopen(SETTINGS_FILE, "w+");
 	}
 
-	if (fcur == NULL) {
+	if (fp == NULL) {
 		LOG_ERR("Failed to create settings file: %s", SETTINGS_FILE);
 		return -1;
 	}
@@ -146,20 +137,18 @@ static int settings_custom_save(struct settings_store *cs, const char *name,
 		return -1;
 	}
 
-	if (fnew != NULL) {
-		if (entry_check_and_copy(fcur, fnew, name) < 0) {
-			return -1;
-		}
+	if (entry_check_and_seek(fp, name) != 0) {
+		return -1;
 	}
 
 	char bufvname[ENTRY_NAME_MAX_LEN + ENTRY_LEN_SIZE + 3];
 
 	snprintk(bufvname, sizeof(bufvname), "%s=", name);
-	if (fputs(bufvname, fnew != NULL ? fnew : fcur) < 0) {
+	if (fputs(bufvname, fp) < 0) {
 		return -1;
 	}
 
-	char bufval[ENTRY_VAL_MAX_LEN + 2] = {};
+	char bufval[ENTRY_VAL_MAX_LEN + 2];
 	size_t valcnt = 0;
 
 	while (valcnt < (val_len * 2)) {
@@ -171,24 +160,11 @@ static int settings_custom_save(struct settings_store *cs, const char *name,
 	bufval[valcnt++] = '\n';
 	bufval[valcnt] = 0;
 
-	if (fputs(bufval, fnew != NULL ? fnew : fcur) < 0) {
+	if (fputs(bufval, fp) < 0) {
 		return -1;
 	}
 
-	if (fnew != NULL) {
-		entry_check_and_copy(fcur, fnew, name);
-	}
-
-	fclose(fcur);
-
-	if (fnew != NULL) {
-		fclose(fnew);
-
-		remove(SETTINGS_FILE);
-		rename(SETTINGS_FILE_TMP, SETTINGS_FILE);
-	}
-
-	return 0;
+	return fclose(fp);
 }
 
 /* custom backend interface */
