2234eba0bd3f98af5b6ec058cff9aa330b22a3ff
Robert Lubos <robert.lubos@nordicsemi.no>
2021-11-10 11:53:11 +0100

    Revert "[nrf fromtree] test: Bluetooth: Mesh: test replay attack on ble mesh rpc"
    This reverts commit 553537456b4324b5018b4474e6d250c414ea94cd.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_mesh/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_mesh/src/main.c
@@ -9,7 +9,6 @@
 
 #if defined(CONFIG_SETTINGS)
 extern struct bst_test_list *test_persistence_install(struct bst_test_list *tests);
-extern struct bst_test_list *test_rpc_install(struct bst_test_list *tests);
 #else
 extern struct bst_test_list *test_transport_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_friendship_install(struct bst_test_list *tests);
@@ -21,7 +20,6 @@ extern struct bst_test_list *test_scanner_install(struct bst_test_list *test);
 bst_test_install_t test_installers[] = {
 #if defined(CONFIG_SETTINGS)
 	test_persistence_install,
-	test_rpc_install,
 #else
 	test_transport_install,
 	test_friendship_install,
