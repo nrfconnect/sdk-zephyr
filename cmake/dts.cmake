90d29c7e8f4fc5a5787d970e1aca4003359e0f30
Robert Lubos <robert.lubos@nordicsemi.no>
2021-11-10 11:53:11 +0100

    Revert "[nrf fromtree] cmake: dts: import devicetree symbols into CMake"
    This reverts commit 86cc40585e19bdbd736dbebdcd458274c923b9be.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- cmake/dts.cmake
+++ cmake/dts.cmake
@@ -24,10 +24,6 @@ set(DEVICETREE_UNFIXED_H        ${PROJECT_BINARY_DIR}/include/generated/devicetr
 set(DEVICE_EXTERN_H             ${PROJECT_BINARY_DIR}/include/generated/device_extern.h)
 set(DTS_POST_CPP                ${PROJECT_BINARY_DIR}/${BOARD}.dts.pre.tmp)
 
-# Devicetree in CMake.
-set(DTS_CMAKE_SCRIPT            ${ZEPHYR_BASE}/scripts/dts/gen_dts_cmake.py)
-set(DTS_CMAKE                   ${PROJECT_BINARY_DIR}/dts.cmake)
-
 set_ifndef(DTS_SOURCE ${BOARD_DIR}/${BOARD}.dts)
 
 zephyr_file(APPLICATION_ROOT DTS_ROOT)
@@ -173,7 +169,6 @@ if(SUPPORTS_DTS)
     CMAKE_CONFIGURE_DEPENDS
     ${include_files}
     ${GEN_DEFINES_SCRIPT}
-    ${DTS_CMAKE_SCRIPT}
     )
 
   #
@@ -247,20 +242,6 @@ if(SUPPORTS_DTS)
     message(STATUS "Generated zephyr.dts: ${ZEPHYR_DTS}")
     message(STATUS "Generated devicetree_unfixed.h: ${DEVICETREE_UNFIXED_H}")
     message(STATUS "Generated device_extern.h: ${DEVICE_EXTERN_H}")
-  endif()
-
-  execute_process(
-    COMMAND ${PYTHON_EXECUTABLE} ${DTS_CMAKE_SCRIPT}
-    --edt-pickle ${EDT_PICKLE}
-    --cmake-out ${DTS_CMAKE}
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-    RESULT_VARIABLE ret
-    )
-  if(NOT "${ret}" STREQUAL "0")
-    message(FATAL_ERROR "gen_dts_cmake.py failed with return code: ${ret}")
-  else()
-    message(STATUS "Including generated dts.cmake file: ${DTS_CMAKE}")
-    include(${DTS_CMAKE})
   endif()
 
 else()
