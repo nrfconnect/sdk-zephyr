f9e35247c42bfec91a448eb979f9007071aa8360
Robert Lubos <robert.lubos@nordicsemi.no>
2021-11-10 11:53:13 +0100

    Revert "[nrf fromtree] Bluetooth: Mesh: Fix wrong use `log_strdup`"
    This reverts commit ad6767ccef9e2af42e29f29a0050cc1ab07c7516.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/bluetooth/mesh/cfg_cli.c
+++ subsys/bluetooth/mesh/cfg_cli.c
@@ -1967,7 +1967,7 @@ static int mod_sub_va(uint32_t op, uint16_t net_idx, uint16_t addr, uint16_t ele
 	}
 
 	BT_DBG("net_idx 0x%04x addr 0x%04x elem_addr 0x%04x label %s",
-	       net_idx, addr, elem_addr, bt_hex(label, 16));
+	       net_idx, addr, elem_addr, log_strdup(label));
 	BT_DBG("mod_id 0x%04x cid 0x%04x", mod_id, cid);
 
 	bt_mesh_model_msg_init(&msg, op);
