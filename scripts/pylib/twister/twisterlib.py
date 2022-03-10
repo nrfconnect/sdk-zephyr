9dced082deaceed9e6ccbf76b83e139043b0a022
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2022-03-10 11:06:55 +0100

    [nrf temphack] Revert "twister: add -Werror to CMAKE_AFLAGS"
    This reverts commit 8a603da6cde63a3800c162793e6c15f764bcd3f3.

--- scripts/pylib/twister/twisterlib.py
+++ scripts/pylib/twister/twisterlib.py
@@ -2163,7 +2163,7 @@ class CMake():
         if self.warnings_as_errors:
             ldflags = "-Wl,--fatal-warnings"
             cflags = "-Werror"
-            aflags = "-Werror -Wa,--fatal-warnings"
+            aflags = "-Wa,--fatal-warnings"
             gen_defines_args = "--edtlib-Werror"
         else:
             ldflags = cflags = aflags = ""
