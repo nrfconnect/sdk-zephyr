2a4fe50b3510225b2fdd8de26fd3b0527fb8a25e
Dominik Ermel <dominik.ermel@nordicsemi.no>
2022-04-21 12:57:55 +0000

    Revert "[nrf fromtree] mgmt/mcumgr/lib: Fix taskstat thread name processing"
    This reverts commit 6281a784dad28fe109169e3a19edd5bbd9a3e571.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- subsys/mgmt/mcumgr/lib/cmd/os_mgmt/src/os_mgmt.c
+++ subsys/mgmt/mcumgr/lib/cmd/os_mgmt/src/os_mgmt.c
@@ -65,40 +65,50 @@ static inline CborError
 os_mgmt_taskstat_encode_thread_name(struct CborEncoder *encoder, int idx,
 				    const struct k_thread *thread)
 {
-	size_t name_len = strlen(thread->name);
+	int err = 0;
 
 	ARG_UNUSED(idx);
 
-	if (name_len > CONFIG_OS_MGMT_TASKSTAT_THREAD_NAME_LEN) {
-		name_len = CONFIG_OS_MGMT_TASKSTAT_THREAD_NAME_LEN;
-	}
-
-	return cbor_encode_text_string(encoder, thread->name, name_len);
+	err = cbor_encode_text_string(encoder, thread->name, strnlen(thread->name,
+				      CONFIG_OS_MGMT_TASKSTAT_THREAD_NAME_LEN - 1));
+	return err;
 }
 
-#else
+#elif defined(CONFIG_OS_MGMT_TASKSTAT_USE_THREAD_PRIO_FOR_NAME)
 static inline CborError
 os_mgmt_taskstat_encode_thread_name(struct CborEncoder *encoder, int idx,
 				    const struct k_thread *thread)
 {
 	CborError err = 0;
-	char thread_name[CONFIG_OS_MGMT_TASKSTAT_THREAD_NAME_LEN + 1];
+	char thread_name[CONFIG_OS_MGMT_TASKSTAT_THREAD_NAME_LEN];
 
-#if defined(CONFIG_OS_MGMT_TASKSTAT_USE_THREAD_PRIO_FOR_NAME)
-	idx = (int)thread->base.prio;
-#elif defined(CONFIG_OS_MGMT_TASKSTAT_USE_THREAD_IDX_FOR_NAME)
-	ARG_UNUSED(thread);
-#else
-#error Unsupported option for taskstat thread name
-#endif
+	ARG_UNUSED(idx);
 
-	ll_to_s(idx, sizeof(thread_name) - 1, thread_name);
 	thread_name[sizeof(thread_name) - 1] = 0;
+	ll_to_s((int)thread->base.prio, sizeof(thread_name) - 1, thread_name);
 
 	err = cbor_encode_text_stringz(encoder, thread_name);
 	return err;
 }
 
+#elif defined(CONFIG_OS_MGMT_TASKSTAT_USE_THREAD_IDX_FOR_NAME)
+static inline CborError
+os_mgmt_taskstat_encode_thread_name(struct CborEncoder *encoder, int idx,
+				    const struct k_thread *thread)
+{
+	CborError err = 0;
+	char thread_name[CONFIG_OS_MGMT_TASKSTAT_THREAD_NAME_LEN];
+
+	ARG_UNUSED(thread);
+
+	thread_name[sizeof(thread_name) - 1] = 0;
+	ll_to_s(idx, sizeof(thread_name) - 1, thread_name);
+
+	err = cbor_encode_text_stringz(encoder, thread_name);
+	return err;
+}
+#else
+#error Unsupported option for taskstat thread name
 #endif
 
 static inline int
