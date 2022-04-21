cbdceceb02b32692bfc36e7679dc3781af11d522
Dominik Ermel <dominik.ermel@nordicsemi.no>
2022-04-21 12:57:54 +0000

    Revert "[nrf fromtree] net: lwm2m: obj inst processing ..."
    This reverts commit de3d00e727d4bc7df88bdbe5e5ffdf2b0cd64254.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- subsys/net/lib/lwm2m/lwm2m_engine.c
+++ subsys/net/lib/lwm2m/lwm2m_engine.c
@@ -3286,9 +3286,12 @@ static int lwm2m_perform_read_object_instance(struct lwm2m_message *msg,
 		/* update the obj_inst_id as we move through the instances */
 		msg->path.obj_inst_id = obj_inst->obj_inst_id;
 
-		ret = engine_put_begin_oi(&msg->out, &msg->path);
-		if (ret < 0) {
-			return ret;
+		if (msg->path.level <= LWM2M_PATH_LEVEL_OBJECT) {
+			/* start instance formatting */
+			ret = engine_put_begin_oi(&msg->out, &msg->path);
+			if (ret < 0) {
+				return ret;
+			}
 		}
 
 		for (int index = 0; index < obj_inst->resource_count; index++) {
@@ -3346,9 +3349,12 @@ static int lwm2m_perform_read_object_instance(struct lwm2m_message *msg,
 		}
 
 move_forward:
-		ret = engine_put_end_oi(&msg->out, &msg->path);
-		if (ret < 0) {
-			return ret;
+		if (msg->path.level <= LWM2M_PATH_LEVEL_OBJECT) {
+			/* end instance formatting */
+			ret = engine_put_end_oi(&msg->out, &msg->path);
+			if (ret < 0) {
+				return ret;
+			}
 		}
 
 		if (msg->path.level <= LWM2M_PATH_LEVEL_OBJECT) {
