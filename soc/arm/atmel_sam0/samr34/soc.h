argentum-systems/zephyr

soc: atmel_sam0: Add support for SAMR34 and SAMR35 parts
Implement support for SAMR34 and SAMR35 parts, tested on a SAMR34
Xplained Pro dev board. Confirmed operational peripherals: UART, GPIO,
PWM, ADC.

NOTE: The SAMR34 datasheet lists NVIC line 20 for the ADC's interrupt,
but the SAML21 datasheet gives line 22. It appears that the SAMR34 sheet
is incorrect (the ADC doesn't function with line 20), and as such, I have
also omitted the adjustments for TC4 and AC...

Signed-off-by: Attie Grande <attie.grande@argentum-systems.co.uk>

soc/arm/atmel_sam0/samr34/soc.h
@@ -1,0 +1,58 @@
+/* Copyright (c) 2021 Argentum Systems Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _ATMEL_SAMR_SOC_H_
+#define _ATMEL_SAMR_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#define DONT_USE_CMSIS_INIT
+
+#include <zephyr/types.h>
+
+#if defined(CONFIG_SOC_PART_NUMBER_SAMR34J16B)
+#include <samr34j16b.h>
+#elif defined(CONFIG_SOC_PART_NUMBER_SAMR34J17B)
+#include <samr34j17b.h>
+#elif defined(CONFIG_SOC_PART_NUMBER_SAMR34J18B)
+#include <samr34j18b.h>
+#else
+#error Library does not support the specified device.
+#endif
+
+#endif /* _ASMLANGUAGE */
+
+#include "adc_fixup_saml21.h"
+#include "../common/soc_port.h"
+#include "../common/atmel_sam0_dt.h"
+
+/** Processor Clock (HCLK) Frequency */
+#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ ATMEL_SAM0_DT_CPU_CLK_FREQ_HZ
+
+/** Master Clock (MCK) Frequency */
+#define SOC_ATMEL_SAM0_MCK_FREQ_HZ SOC_ATMEL_SAM0_HCLK_FREQ_HZ
+#define SOC_ATMEL_SAM0_OSC32K_FREQ_HZ 32768
+#define SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ 32768
+#define SOC_ATMEL_SAM0_OSC16M_FREQ_HZ 16000000
+#define SOC_ATMEL_SAM0_GCLK0_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_GCLK3_FREQ_HZ 24000000
+
+#if defined(CONFIG_SOC_ATMEL_SAML_OPENLOOP_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ 0
+#elif defined(CONFIG_SOC_ATMEL_SAML_OSC32K_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_OSC32K_FREQ_HZ
+#elif defined(CONFIG_SOC_ATMEL_SAML_XOSC32K_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ
+#elif defined(CONFIG_SOC_ATMEL_SAML_OSC16M_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_OSC16M_FREQ_HZ
+#else
+#error Unsupported GCLK1 clock source.
+#endif
+
+#define SOC_ATMEL_SAM0_APBA_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_APBB_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_APBC_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+
+#endif /* _ATMEL_SAMR_SOC_H_ */

soc/arm/atmel_sam0/samr35/soc.h
@@ -1,0 +1,58 @@
+/* Copyright (c) 2021 Argentum Systems Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _ATMEL_SAMR_SOC_H_
+#define _ATMEL_SAMR_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#define DONT_USE_CMSIS_INIT
+
+#include <zephyr/types.h>
+
+#if defined(CONFIG_SOC_PART_NUMBER_SAMR35J16B)
+#include <samr35j16b.h>
+#elif defined(CONFIG_SOC_PART_NUMBER_SAMR35J17B)
+#include <samr35j17b.h>
+#elif defined(CONFIG_SOC_PART_NUMBER_SAMR35J18B)
+#include <samr35j18b.h>
+#else
+#error Library does not support the specified device.
+#endif
+
+#endif /* _ASMLANGUAGE */
+
+#include "adc_fixup_saml21.h"
+#include "../common/soc_port.h"
+#include "../common/atmel_sam0_dt.h"
+
+/** Processor Clock (HCLK) Frequency */
+#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ ATMEL_SAM0_DT_CPU_CLK_FREQ_HZ
+
+/** Master Clock (MCK) Frequency */
+#define SOC_ATMEL_SAM0_MCK_FREQ_HZ SOC_ATMEL_SAM0_HCLK_FREQ_HZ
+#define SOC_ATMEL_SAM0_OSC32K_FREQ_HZ 32768
+#define SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ 32768
+#define SOC_ATMEL_SAM0_OSC16M_FREQ_HZ 16000000
+#define SOC_ATMEL_SAM0_GCLK0_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_GCLK3_FREQ_HZ 24000000
+
+#if defined(CONFIG_SOC_ATMEL_SAML_OPENLOOP_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ 0
+#elif defined(CONFIG_SOC_ATMEL_SAML_OSC32K_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_OSC32K_FREQ_HZ
+#elif defined(CONFIG_SOC_ATMEL_SAML_XOSC32K_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ
+#elif defined(CONFIG_SOC_ATMEL_SAML_OSC16M_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_OSC16M_FREQ_HZ
+#else
+#error Unsupported GCLK1 clock source.
+#endif
+
+#define SOC_ATMEL_SAM0_APBA_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_APBB_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_APBC_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+
+#endif /* _ATMEL_SAMR_SOC_H_ */

nrfconnect/sdk-zephyr

soc/arm/atmel_sam0/samr34/soc.h
@@ -1,0 +1,58 @@
+/* Copyright (c) 2021 Argentum Systems Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _ATMEL_SAMR_SOC_H_
+#define _ATMEL_SAMR_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#define DONT_USE_CMSIS_INIT
+
+#include <zephyr/types.h>
+
+#if defined(CONFIG_SOC_PART_NUMBER_SAMR34J16B)
+#include <samr34j16b.h>
+#elif defined(CONFIG_SOC_PART_NUMBER_SAMR34J17B)
+#include <samr34j17b.h>
+#elif defined(CONFIG_SOC_PART_NUMBER_SAMR34J18B)
+#include <samr34j18b.h>
+#else
+#error Library does not support the specified device.
+#endif
+
+#endif /* _ASMLANGUAGE */
+
+#include "adc_fixup_sam0.h"
+#include "../common/soc_port.h"
+#include "../common/atmel_sam0_dt.h"
+
+/** Processor Clock (HCLK) Frequency */
+#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ ATMEL_SAM0_DT_CPU_CLK_FREQ_HZ
+
+/** Master Clock (MCK) Frequency */
+#define SOC_ATMEL_SAM0_MCK_FREQ_HZ SOC_ATMEL_SAM0_HCLK_FREQ_HZ
+#define SOC_ATMEL_SAM0_OSC32K_FREQ_HZ 32768
+#define SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ 32768
+#define SOC_ATMEL_SAM0_OSC16M_FREQ_HZ 16000000
+#define SOC_ATMEL_SAM0_GCLK0_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_GCLK3_FREQ_HZ 24000000
+
+#if defined(CONFIG_SOC_ATMEL_SAML_OPENLOOP_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ 0
+#elif defined(CONFIG_SOC_ATMEL_SAML_OSC32K_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_OSC32K_FREQ_HZ
+#elif defined(CONFIG_SOC_ATMEL_SAML_XOSC32K_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ
+#elif defined(CONFIG_SOC_ATMEL_SAML_OSC16M_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_OSC16M_FREQ_HZ
+#else
+#error Unsupported GCLK1 clock source.
+#endif
+
+#define SOC_ATMEL_SAM0_APBA_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_APBB_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_APBC_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+
+#endif /* _ATMEL_SAMR_SOC_H_ */

soc/arm/atmel_sam0/samr35/soc.h
@@ -1,0 +1,58 @@
+/* Copyright (c) 2021 Argentum Systems Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _ATMEL_SAMR_SOC_H_
+#define _ATMEL_SAMR_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#define DONT_USE_CMSIS_INIT
+
+#include <zephyr/types.h>
+
+#if defined(CONFIG_SOC_PART_NUMBER_SAMR35J16B)
+#include <samr35j16b.h>
+#elif defined(CONFIG_SOC_PART_NUMBER_SAMR35J17B)
+#include <samr35j17b.h>
+#elif defined(CONFIG_SOC_PART_NUMBER_SAMR35J18B)
+#include <samr35j18b.h>
+#else
+#error Library does not support the specified device.
+#endif
+
+#endif /* _ASMLANGUAGE */
+
+#include "adc_fixup_sam0.h"
+#include "../common/soc_port.h"
+#include "../common/atmel_sam0_dt.h"
+
+/** Processor Clock (HCLK) Frequency */
+#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ ATMEL_SAM0_DT_CPU_CLK_FREQ_HZ
+
+/** Master Clock (MCK) Frequency */
+#define SOC_ATMEL_SAM0_MCK_FREQ_HZ SOC_ATMEL_SAM0_HCLK_FREQ_HZ
+#define SOC_ATMEL_SAM0_OSC32K_FREQ_HZ 32768
+#define SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ 32768
+#define SOC_ATMEL_SAM0_OSC16M_FREQ_HZ 16000000
+#define SOC_ATMEL_SAM0_GCLK0_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_GCLK3_FREQ_HZ 24000000
+
+#if defined(CONFIG_SOC_ATMEL_SAML_OPENLOOP_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ 0
+#elif defined(CONFIG_SOC_ATMEL_SAML_OSC32K_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_OSC32K_FREQ_HZ
+#elif defined(CONFIG_SOC_ATMEL_SAML_XOSC32K_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ
+#elif defined(CONFIG_SOC_ATMEL_SAML_OSC16M_AS_MAIN)
+#define SOC_ATMEL_SAM0_GCLK1_FREQ_HZ SOC_ATMEL_SAM0_OSC16M_FREQ_HZ
+#else
+#error Unsupported GCLK1 clock source.
+#endif
+
+#define SOC_ATMEL_SAM0_APBA_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_APBB_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+#define SOC_ATMEL_SAM0_APBC_FREQ_HZ SOC_ATMEL_SAM0_MCK_FREQ_HZ
+
+#endif /* _ATMEL_SAMR_SOC_H_ */