747cedbd314e5a2c430171e9159c7752aeb23429
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2022-10-12 12:29:09 +0200

    Revert "[nrf fromtree] wifi_mgmt: Add support for MFP"
    This reverts commit cdd5fdd89561f3b94578a5438b5ba9129f8041b1.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- include/zephyr/net/wifi_mgmt.h
+++ include/zephyr/net/wifi_mgmt.h
@@ -90,7 +90,6 @@ struct wifi_scan_result {
 
 	uint8_t channel;
 	enum wifi_security_type security;
-	enum wifi_mfp_options mfp;
 	int8_t rssi;
 
 	uint8_t mac[WIFI_MAC_ADDR_LEN];
@@ -109,7 +108,6 @@ struct wifi_connect_req_params {
 
 	uint8_t channel;
 	enum wifi_security_type security;
-	enum wifi_mfp_options mfp;
 };
 
 struct wifi_status {
