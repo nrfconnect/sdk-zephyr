2e16a9998e23bbce9d72b28d14b92e7bc32fa0f6
Mariusz Poslinski <mariusz.poslinski@nordicsemi.no>
2023-02-17 09:06:40 +0100

    update

--- modules/hal_nordic/nrf_802154/serialization/platform/nrf_802154_spinel_response_notifier.c
+++ modules/hal_nordic/nrf_802154/serialization/platform/nrf_802154_spinel_response_notifier.c
@@ -43,9 +43,10 @@ static spinel_prop_key_t awaited_property = AWAITED_PROPERTY_NONE;
 
 void nrf_802154_spinel_response_notifier_init(void)
 {
-	LOG_INF("awaited_property: %d",awaited_property);
+	printf("awaited_property: %d",awaited_property);
 	notify_buff.free = true;
 	k_mutex_init(&await_mutex);
+	awaited_property = AWAITED_PROPERTY_NONE;
 
 }
 
