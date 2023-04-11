a9aab574abb6aa0d398821d6dfdccdd97f0fabdb
Johann Fischer <johann.fischer@nordicsemi.no>
2023-04-11 15:07:08 +0200

    Revert "[nrf fromtree] net: wifi: Add support for regulatory domain configuration"
    This reverts commit d2066d42034c9bdabbc65aec0e501fbfbe236703.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- include/zephyr/net/wifi.h
+++ include/zephyr/net/wifi.h
@@ -12,8 +12,6 @@
 #ifndef ZEPHYR_INCLUDE_NET_WIFI_H_
 #define ZEPHYR_INCLUDE_NET_WIFI_H_
 
-#define WIFI_COUNTRY_CODE_LEN 2
-
 /* Not having support for legacy types is deliberate to enforce
  * higher security.
  */
