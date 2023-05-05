76ad781c59d359eb4e100440c9ce558f188822ca
Marek Pieta <Marek.Pieta@nordicsemi.no>
2023-05-05 11:12:26 +0200

    [nrf noup] mcumgr: img_mgmt: Fix check for chosen code partition
    The "zephyr-code-partition" chosen DTS node cannot be used when build
    uses Partition Manager. In that case, mcumgr must rely on the
    definitions provided by the Partition Manager.
    
    Jira: NCSDK-21381
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>
    (cherry picked from commit 394faafd215eaa2405f58f6aa7793afcc62b6e99)
    (cherry picked from commit 36961d6b72f53b9ff0eef1e398f08da84dae5032)
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- subsys/mgmt/mcumgr/grp/img_mgmt/src/img_mgmt.c
+++ subsys/mgmt/mcumgr/grp/img_mgmt/src/img_mgmt.c
@@ -34,6 +34,23 @@
 #include <zephyr/mgmt/mcumgr/mgmt/callbacks.h>
 #endif
 
+#if USE_PARTITION_MANAGER
+#include <flash_map_pm.h>
+
+#ifdef PM_MCUBOOT_SECONDARY_PAD_SIZE
+BUILD_ASSERT(PM_MCUBOOT_PAD_SIZE == PM_MCUBOOT_SECONDARY_PAD_SIZE);
+#endif
+
+#if CONFIG_BUILD_WITH_TFM
+  #define PM_ADDRESS_OFFSET (PM_MCUBOOT_PAD_SIZE + PM_TFM_SIZE)
+#else
+  #define PM_ADDRESS_OFFSET (PM_MCUBOOT_PAD_SIZE)
+#endif
+
+#define FIXED_PARTITION_IS_RUNNING_APP_PARTITION(label)	\
+	(FIXED_PARTITION_OFFSET(label) == (PM_ADDRESS - PM_ADDRESS_OFFSET))
+
+#else /* ! USE_PARTITION_MANAGER */
 #ifndef CONFIG_FLASH_LOAD_OFFSET
 #error MCUmgr requires application to be built with CONFIG_FLASH_LOAD_OFFSET set \
 	to be able to figure out application running slot.
@@ -41,12 +58,13 @@
 
 #define FIXED_PARTITION_IS_RUNNING_APP_PARTITION(label)	\
 	(FIXED_PARTITION_OFFSET(label) == CONFIG_FLASH_LOAD_OFFSET)
+#endif /* USE_PARTITION_MANAGER */
 
 #if !(FIXED_PARTITION_IS_RUNNING_APP_PARTITION(slot0_partition) ||	\
 	FIXED_PARTITION_IS_RUNNING_APP_PARTITION(slot0_ns_partition) ||	\
 	FIXED_PARTITION_IS_RUNNING_APP_PARTITION(slot1_partition) ||	\
 	FIXED_PARTITION_IS_RUNNING_APP_PARTITION(slot2_partition))
-#error "Unsupported chosen zephyr,code-partition for boot application."
+#error "Unsupported chosen code partition for boot application."
 #endif
 
 LOG_MODULE_REGISTER(mcumgr_img_grp, CONFIG_MCUMGR_GRP_IMG_LOG_LEVEL);
