920fb9e5246f38b6d2d8b6a4c448ae165c27e67a
Dominik Ermel <dominik.ermel@nordicsemi.no>
2023-06-15 12:47:49 +0000

    Revert "[nrf noup] tree-wide: support NCS Partition Manager (PM) definitions"
    This reverts commit 68110ee5a75167e8e2c65746211571315618a3e1.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- include/zephyr/storage/flash_map.h
+++ include/zephyr/storage/flash_map.h
@@ -255,10 +255,6 @@ const struct device *flash_area_get_device(const struct flash_area *fa);
  */
 uint8_t flash_area_erased_val(const struct flash_area *fa);
 
-#if USE_PARTITION_MANAGER
-#include <flash_map_pm.h>
-#else
-
 #define FLASH_AREA_LABEL_EXISTS(label) __DEPRECATED_MACRO \
 	DT_HAS_FIXED_PARTITION_LABEL(label)
 
@@ -330,8 +326,6 @@ uint8_t flash_area_erased_val(const struct flash_area *fa);
  */
 #define FIXED_PARTITION_DEVICE(label) \
 	DEVICE_DT_GET(DT_MTD_FROM_FIXED_PARTITION(DT_NODELABEL(label)))
-
-#endif /* USE_PARTITION_MANAGER */
 
 #ifdef __cplusplus
 }
