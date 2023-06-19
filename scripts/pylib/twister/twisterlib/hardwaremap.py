e0ca6d5b865822e5a984c103453146bf115920a1
Robert Lubos <robert.lubos@nordicsemi.no>
2023-06-19 12:19:05 +0200

    [nrf noup] Revert "twister: Use natural sort when generating hardware map"
    This reverts commit c37deeb0c4602dc3eeb700a7dc3ed317283fff17.
    
    This is only a temporary change, until we align our CI. To be removed
    once natsort is avaialble in the NCS CI.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    (cherry picked from commit 374374bd72782f93412ea12d1713d543d4080ffb)

--- scripts/pylib/twister/twisterlib/hardwaremap.py
+++ scripts/pylib/twister/twisterlib/hardwaremap.py
@@ -384,7 +384,7 @@ class HardwareMap:
 
     def save(self, hwm_file):
         # use existing map
-        self.detected = natsorted(self.detected, key=lambda x: x.serial or '')
+        self.detected.sort(key=lambda x: x.serial or '')
         if os.path.exists(hwm_file):
             with open(hwm_file) as yaml_file:
                 hwm = yaml.load(yaml_file, Loader=SafeLoader)
