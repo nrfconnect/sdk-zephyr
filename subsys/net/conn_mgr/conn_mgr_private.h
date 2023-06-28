5622265e68bc86191a0d51b4e3a0c12967fc951f
Robert Lubos <robert.lubos@nordicsemi.no>
2023-06-28 10:32:29 +0200

    Revert "[nrf fromlist] net: conn_mgr: refactor event logic"
    This reverts commit 160fd21baacd4a0b17a829f9b86ce890f99b14b7.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/conn_mgr/conn_mgr_private.h
+++ subsys/net/conn_mgr/conn_mgr_private.h
@@ -51,6 +51,11 @@
 extern struct k_sem conn_mgr_event_signal;
 extern struct k_mutex conn_mgr_lock;
 
+enum conn_mgr_state {
+	CONN_MGR_STATE_DISCONNECTED	= 0,
+	CONN_MGR_STATE_CONNECTED	= 1,
+};
+
 void conn_mgr_init_events_handler(void);
 
 /**
