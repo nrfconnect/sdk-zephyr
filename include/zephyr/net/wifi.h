d95cbec12f485e6331e4700dc84acc15b65c4cf4
Robert Lubos <robert.lubos@nordicsemi.no>
2023-06-28 10:32:16 +0200

    Revert "[nrf fromtree] net: wifi: Add an error code for TWT duplicate flow request"
    This reverts commit 917d571c71f56d165f6330a6a276d3bc4eb09fae.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- include/zephyr/net/wifi.h
+++ include/zephyr/net/wifi.h
@@ -352,7 +352,6 @@ enum wifi_twt_fail_reason {
 	WIFI_TWT_FAIL_OPERATION_IN_PROGRESS,
 	WIFI_TWT_FAIL_INVALID_FLOW_ID,
 	WIFI_TWT_FAIL_IP_NOT_ASSIGNED,
-	WIFI_TWT_FAIL_FLOW_ALREADY_EXISTS,
 };
 
 static const char * const twt_err_code_tbl[] = {
@@ -372,8 +371,6 @@ static const char * const twt_err_code_tbl[] = {
 		"Invalid negotiated flow id",
 	[WIFI_TWT_FAIL_IP_NOT_ASSIGNED] =
 		"IP address not assigned",
-	[WIFI_TWT_FAIL_FLOW_ALREADY_EXISTS] =
-		"Flow already exists",
 };
 
 static inline const char *get_twt_err_code_str(int16_t err_no)
