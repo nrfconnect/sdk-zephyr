eb27640538bd4d5882fa0ccb96bd601220464efe
Dominik Ermel <dominik.ermel@nordicsemi.no>
2023-09-29 13:35:28 +0000

    Revert "[nrf fromtree] west: build: also parse common section in yaml file"
    This reverts commit 3d36559670975b38714db83f40a0b23fbe30cdf8.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- scripts/west_commands/build.py
+++ scripts/west_commands/build.py
@@ -270,7 +270,6 @@ class Build(Forceable):
                     y = yaml.safe_load(stream)
                 except yaml.YAMLError as exc:
                     log.die(exc)
-            common = y.get('common')
             tests = y.get('tests')
             if not tests:
                 log.die(f"No tests found in {yf}")
@@ -278,29 +277,23 @@ class Build(Forceable):
             if not item:
                 log.die(f"Test item {test_item} not found in {yf}")
 
-            sysbuild = False
-            for section in [common, item]:
-                if not section:
+            for data in ['extra_args', 'extra_configs']:
+                extra = item.get(data)
+                if not extra:
                     continue
-                sysbuild = section.get('sysbuild', sysbuild)
-                for data in ['extra_args', 'extra_configs']:
-                    extra = section.get(data)
-                    if not extra:
-                        continue
-                    if isinstance(extra, str):
-                        arg_list = extra.split(" ")
-                    else:
-                        arg_list = extra
-                    if data == 'extra_configs':
-                        args = ["-D{}".format(arg.replace('"', '\"')) for arg in arg_list]
-                    elif data == 'extra_args':
-                        args = ["-D{}".format(arg.replace('"', '')) for arg in arg_list]
-                    if self.args.cmake_opts:
-                        self.args.cmake_opts.extend(args)
-                    else:
-                        self.args.cmake_opts = args
-
-            self.args.sysbuild = sysbuild
+                if isinstance(extra, str):
+                    arg_list = extra.split(" ")
+                else:
+                    arg_list = extra
+                if data == 'extra_configs':
+                    args = ["-D{}".format(arg.replace('"', '\"')) for arg in arg_list]
+                elif data == 'extra_args':
+                    args = ["-D{}".format(arg.replace('"', '')) for arg in arg_list]
+                if self.args.cmake_opts:
+                    self.args.cmake_opts.extend(args)
+                else:
+                    self.args.cmake_opts = args
+            self.args.sysbuild = item.get('sysbuild')
         return found_test_metadata
 
     def _sanity_precheck(self):
