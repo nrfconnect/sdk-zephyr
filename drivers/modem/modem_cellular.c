1635e873f08500a7890d66bef14b3afa97f0c39c
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2024-02-14 10:27:55 +0200

    drivers: modem_cellular: add nRF91 series (w/ SLM)
    The nRF91 series devices, when running the Serial LTE Modem (SLM)
    application starting with nRF Connect SDK 2.6.0, can now be used as
    standalone modems via the generic modem_cellular driver.
    
    A configuration to run the cellular_modem sample on the nRF9160 DK
    (plugged in to another nRF91 series device running SLM) is provided.
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- drivers/modem/modem_cellular.c
+++ drivers/modem/modem_cellular.c
@@ -2651,6 +2651,34 @@ MODEM_CHAT_SCRIPT_DEFINE(sqn_gm02s_periodic_chat_script,
 			      &MODEM_CELLULAR_INST_NAME(config, inst), POST_KERNEL, 99,            \
 			      &modem_cellular_api);
 
+#define MODEM_CELLULAR_DEVICE_NORDIC_NRF91_SLM(inst)						   \
+	MODEM_PPP_DEFINE(MODEM_CELLULAR_INST_NAME(ppp, inst), NULL, 98, 1500, 1500);               \
+                                                                                                   \
+	static struct modem_cellular_data MODEM_CELLULAR_INST_NAME(data, inst) = {                 \
+		.chat_delimiter = "\r\n",                                                          \
+		.ppp = &MODEM_CELLULAR_INST_NAME(ppp, inst),                                       \
+	};                                                                                         \
+                                                                                                   \
+	static struct modem_cellular_config MODEM_CELLULAR_INST_NAME(config, inst) = {             \
+		.uart = DEVICE_DT_GET(DT_INST_BUS(inst)),                                          \
+		.power_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, mdm_power_gpios, {}),                 \
+		.reset_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, mdm_reset_gpios, {}),                 \
+		.power_pulse_duration_ms = 100,                                                    \
+		.reset_pulse_duration_ms = 100,                                                    \
+		.startup_time_ms = 2000,                                                           \
+		.shutdown_time_ms = 3000,                                                          \
+		.init_chat_script = &nordic_nrf91_slm_init_chat_script,                            \
+		.dial_chat_script = &nordic_nrf91_slm_dial_chat_script,                            \
+		.periodic_chat_script = &nordic_nrf91_slm_periodic_chat_script,                    \
+	};                                                                                         \
+                                                                                                   \
+	PM_DEVICE_DT_INST_DEFINE(inst, modem_cellular_pm_action);                                  \
+                                                                                                   \
+	DEVICE_DT_INST_DEFINE(inst, modem_cellular_init, PM_DEVICE_DT_INST_GET(inst),              \
+			      &MODEM_CELLULAR_INST_NAME(data, inst),                               \
+			      &MODEM_CELLULAR_INST_NAME(config, inst), POST_KERNEL, 99,            \
+			      &modem_cellular_api);
+
 #define DT_DRV_COMPAT quectel_bg95
 DT_INST_FOREACH_STATUS_OKAY(MODEM_CELLULAR_DEVICE_QUECTEL_BG95)
 #undef DT_DRV_COMPAT
