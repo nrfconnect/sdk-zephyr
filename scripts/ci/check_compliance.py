ad3a56e02c5a94499cbbca4e8fe99af4eeb6a3a5
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2024-05-08 13:10:48 +0200

    [nrf fromlist] cmake: scripts: support board extension
    Upstream PR: https://github.com/zephyrproject-rtos/zephyr/pull/72857
    
    Fixes: #69548
    
    Support extending an existing board with new board variants.
    
    This commit introduces the following changes to allow a board to be
    extended out-of-tree.
    
    The board yaml schema is extended to support an extend field which
    will be used to identify the board to be extended.
    
    A board 'plank' can be extended like this:
    > board:
    >   extend: plank
    >   variants:
    >     - name: ext
    >       qualifier: soc1
    
    For the rest of the build system this means that there is no longer a
    single board directory.
    The existing CMake variable BOARD_DIR is kept and reference the
    directory which defines the board.
    A new CMake variable BOARD_DIRECTORIES provides a list of all
    directories which defines board targets for the board.
    This means the directory which defines the board as well as all
    directories that extends the board.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
    (cherry picked from commit ad16407aca8ccde1aaa57d4d1dcb15bb4d0bfc25)
    (cherry picked from commit a276e94de17390ccfaad180c0323d501844e1a92)

--- scripts/ci/check_compliance.py
+++ scripts/ci/check_compliance.py
@@ -507,12 +507,14 @@ class KconfigCheck(ComplianceTest):
         kconfig_defconfig_file = os.path.join(kconfig_dir, 'boards', 'Kconfig.defconfig')
 
         root_args = argparse.Namespace(**{'board_roots': [Path(ZEPHYR_BASE)],
-                                          'soc_roots': [Path(ZEPHYR_BASE)], 'board': None})
-        v2_boards = list_boards.find_v2_boards(root_args)
+                                          'soc_roots': [Path(ZEPHYR_BASE)], 'board': None,
+                                          'board_dir': []})
+        v2_boards = list_boards.find_v2_boards(root_args).values()
 
         with open(kconfig_defconfig_file, 'w') as fp:
             for board in v2_boards:
-                fp.write('osource "' + (Path(board.dir) / 'Kconfig.defconfig').as_posix() + '"\n')
+                fp.write('osource "' +
+                         (board.directories[0] / 'Kconfig.defconfig').as_posix() + '"\n')
 
         with open(kconfig_boards_file, 'w') as fp:
             for board in v2_boards:
@@ -525,7 +527,8 @@ class KconfigCheck(ComplianceTest):
                     fp.write('config  ' + board_str + '\n')
                     fp.write('\t bool\n')
                 fp.write(
-                    'source "' + (Path(board.dir) / ('Kconfig.' + board.name)).as_posix() + '"\n\n'
+                    'source "' +
+                    (board.directories[0] / ('Kconfig.' + board.name)).as_posix() + '"\n\n'
                 )
 
         with open(kconfig_file, 'w') as fp:
@@ -533,7 +536,7 @@ class KconfigCheck(ComplianceTest):
                 'osource "' + (Path(kconfig_dir) / 'boards' / 'Kconfig.syms.v1').as_posix() + '"\n'
             )
             for board in v2_boards:
-                fp.write('osource "' + (Path(board.dir) / 'Kconfig').as_posix() + '"\n')
+                fp.write('osource "' + (Path(board.directories[0]) / 'Kconfig').as_posix() + '"\n')
 
         kconfig_defconfig_file = os.path.join(kconfig_dir, 'soc', 'Kconfig.defconfig')
         kconfig_soc_file = os.path.join(kconfig_dir, 'soc', 'Kconfig.soc')
@@ -542,7 +545,7 @@ class KconfigCheck(ComplianceTest):
         root_args = argparse.Namespace(**{'soc_roots': soc_roots})
         v2_systems = list_hardware.find_v2_systems(root_args)
 
-        soc_folders = {soc.folder for soc in v2_systems.get_socs()}
+        soc_folders = {soc.folder[0] for soc in v2_systems.get_socs()}
         with open(kconfig_defconfig_file, 'w') as fp:
             for folder in soc_folders:
                 fp.write('osource "' + (Path(folder) / 'Kconfig.defconfig').as_posix() + '"\n')
@@ -612,7 +615,7 @@ class KconfigCheck(ComplianceTest):
         os.makedirs(os.path.join(kconfiglib_dir, 'soc'), exist_ok=True)
         os.makedirs(os.path.join(kconfiglib_dir, 'arch'), exist_ok=True)
 
-        os.environ["BOARD_DIR"] = kconfiglib_boards_dir
+        os.environ["KCONFIG_BOARD_DIR"] = kconfiglib_boards_dir
         self.get_v2_model(kconfiglib_dir)
 
         # Tells Kconfiglib to generate warnings for all references to undefined
@@ -922,6 +925,9 @@ flagged.
         "BOOT_RAM_LOAD", # Used in sysbuild for MCUboot configuration
         "BOOT_SWAP_USING_MOVE", # Used in sysbuild for MCUboot configuration
         "BOOT_SWAP_USING_SCRATCH", # Used in sysbuild for MCUboot configuration
+        "BOARD_MPS2_AN521_CPUTEST", # Used for board and SoC extension feature tests
+        "BOARD_NATIVE_SIM_NATIVE_64_TWO", # Used for board and SoC extension feature tests
+        "BOARD_NATIVE_SIM_NATIVE_ONE", # Used for board and SoC extension feature tests
         "BOOT_ENCRYPTION_KEY_FILE", # Used in sysbuild
         "BOOT_ENCRYPT_IMAGE", # Used in sysbuild
         "BOOT_MAX_IMG_SECTORS_AUTO", # Used in sysbuild
