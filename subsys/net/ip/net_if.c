b8e53e740510bbaca596d87b0ac4f00b879d524c
Robert Lubos <robert.lubos@nordicsemi.no>
2024-06-05 13:53:11 +0200

    [nrf fromtree] net: ipv4: Implement IPv4 address conflict detection
    Add support for IPv4 conflict detection, as specified in RFC 5227.
    The new feature is optional and disabled by default.
    
    Address conflict detection was implemented as a part of the IPv4
    autoconf feature can be generalized to be available for all address
    types.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    (cherry picked from commit 0a95423421dad9b487bf59aa438484e915c65b2a)

--- subsys/net/ip/net_if.c
+++ subsys/net/ip/net_if.c
@@ -4154,6 +4154,118 @@ static struct net_if_addr *ipv4_addr_find(struct net_if *iface,
 	return NULL;
 }
 
+#if defined(CONFIG_NET_IPV4_ACD)
+void net_if_ipv4_acd_succeeded(struct net_if *iface, struct net_if_addr *ifaddr)
+{
+	net_if_lock(iface);
+
+	NET_DBG("ACD succeeded for %s at interface %d",
+		net_sprint_ipv4_addr(&ifaddr->address.in_addr),
+		ifaddr->ifindex);
+
+	ifaddr->addr_state = NET_ADDR_PREFERRED;
+
+	net_mgmt_event_notify_with_info(NET_EVENT_IPV4_ACD_SUCCEED, iface,
+					&ifaddr->address.in_addr,
+					sizeof(struct in_addr));
+
+	net_if_unlock(iface);
+}
+
+void net_if_ipv4_acd_failed(struct net_if *iface, struct net_if_addr *ifaddr)
+{
+	net_if_lock(iface);
+
+	NET_DBG("ACD failed for %s at interface %d",
+		net_sprint_ipv4_addr(&ifaddr->address.in_addr),
+		ifaddr->ifindex);
+
+	net_mgmt_event_notify_with_info(NET_EVENT_IPV4_ACD_FAILED, iface,
+					&ifaddr->address.in_addr,
+					sizeof(struct in_addr));
+
+	net_if_ipv4_addr_rm(iface, &ifaddr->address.in_addr);
+
+	net_if_unlock(iface);
+}
+
+void net_if_ipv4_start_acd(struct net_if *iface, struct net_if_addr *ifaddr)
+{
+	ifaddr->addr_state = NET_ADDR_TENTATIVE;
+
+	if (net_if_is_up(iface)) {
+		NET_DBG("Interface %p ll addr %s tentative IPv4 addr %s",
+			iface,
+			net_sprint_ll_addr(net_if_get_link_addr(iface)->addr,
+					   net_if_get_link_addr(iface)->len),
+			net_sprint_ipv4_addr(&ifaddr->address.in_addr));
+
+		if (net_ipv4_acd_start(iface, ifaddr) != 0) {
+			NET_DBG("Failed to start ACD for %s on iface %p.",
+				net_sprint_ipv4_addr(&ifaddr->address.in_addr),
+				iface);
+
+			/* Just act as if no conflict was detected. */
+			net_if_ipv4_acd_succeeded(iface, ifaddr);
+		}
+	} else {
+		NET_DBG("Interface %p is down, starting ACD for %s later.",
+			iface, net_sprint_ipv4_addr(&ifaddr->address.in_addr));
+	}
+}
+
+void net_if_start_acd(struct net_if *iface)
+{
+	struct net_if_ipv4 *ipv4;
+	int ret;
+
+	net_if_lock(iface);
+
+	NET_DBG("Starting ACD for iface %p", iface);
+
+	ret = net_if_config_ipv4_get(iface, &ipv4);
+	if (ret < 0) {
+		if (ret != -ENOTSUP) {
+			NET_WARN("Cannot do ACD IPv4 config is not valid.");
+		}
+
+		goto out;
+	}
+
+	if (!ipv4) {
+		goto out;
+	}
+
+	ipv4->conflict_cnt = 0;
+
+	/* Start ACD for all the addresses that were added earlier when
+	 * the interface was down.
+	 */
+	for (int i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
+		if (!ipv4->unicast[i].is_used ||
+		    ipv4->unicast[i].address.family != AF_INET ||
+		    net_ipv4_is_addr_loopback(
+			    &ipv4->unicast[i].address.in_addr)) {
+			continue;
+		}
+
+		net_if_ipv4_start_acd(iface, &ipv4->unicast[i]);
+	}
+
+out:
+	net_if_unlock(iface);
+}
+#else
+void net_if_ipv4_start_acd(struct net_if *iface, struct net_if_addr *ifaddr)
+{
+	ARG_UNUSED(iface);
+
+	ifaddr->addr_state = NET_ADDR_PREFERRED;
+}
+
+#define net_if_start_acd(...)
+#endif /* CONFIG_NET_IPV4_ACD */
+
 struct net_if_addr *net_if_ipv4_addr_add(struct net_if *iface,
 					 struct in_addr *addr,
 					 enum net_addr_type addr_type,
@@ -4211,12 +4323,18 @@ struct net_if_addr *net_if_ipv4_addr_add(struct net_if *iface,
 		 *  TODO: Handle properly PREFERRED/DEPRECATED state when
 		 *  address in use, expired and renewal state.
 		 */
-		ifaddr->addr_state = NET_ADDR_PREFERRED;
 
 		NET_DBG("[%d] interface %d (%p) address %s type %s added",
 			idx, net_if_get_by_iface(iface), iface,
 			net_sprint_ipv4_addr(addr),
 			net_addr_type2str(addr_type));
+
+		if (!(l2_flags_get(iface) & NET_L2_POINT_TO_POINT) &&
+		    !net_ipv4_is_addr_loopback(addr)) {
+			net_if_ipv4_start_acd(iface, ifaddr);
+		} else {
+			ifaddr->addr_state = NET_ADDR_PREFERRED;
+		}
 
 		net_mgmt_event_notify_with_info(NET_EVENT_IPV4_ADDR_ADD, iface,
 						&ifaddr->address.in_addr,
@@ -4234,6 +4352,7 @@ bool net_if_ipv4_addr_rm(struct net_if *iface, const struct in_addr *addr)
 {
 	struct net_if_ipv4 *ipv4;
 	int ret;
+	int i;
 
 	NET_ASSERT(addr);
 
@@ -4251,6 +4370,22 @@ bool net_if_ipv4_addr_rm(struct net_if *iface, const struct in_addr *addr)
 	} else if (ret < 0) {
 		NET_DBG("Address %s not found (%d)",
 			net_sprint_ipv4_addr(addr), ret);
+	}
+
+	for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
+		if (!net_ipv4_addr_cmp(&ipv4->unicast[i].ipv4.address.in_addr,
+				       addr)) {
+			continue;
+		}
+
+		ipv4->unicast[i].ipv4.is_used = false;
+
+		NET_DBG("[%d] interface %p address %s removed",
+			i, iface, net_sprint_ipv4_addr(addr));
+
+#if defined(CONFIG_NET_IPV4_ACD)
+		net_ipv4_acd_cancel(iface, &ipv4->unicast[i]);
+#endif
 	}
 
 	return true;
@@ -4568,9 +4703,22 @@ static void leave_ipv4_mcast_all(struct net_if *iface)
 	}
 }
 
-#else
+static void iface_ipv4_start(struct net_if *iface)
+{
+	if (!net_if_flag_is_set(iface, NET_IF_IPV4)) {
+		return;
+	}
+
+	if (IS_ENABLED(CONFIG_NET_IPV4_ACD)) {
+		net_if_start_acd(iface);
+	}
+}
+
+
+#else /* CONFIG_NET_NATIVE_IPV4 */
 #define leave_ipv4_mcast_all(...)
 #define iface_ipv4_init(...)
+#define iface_ipv4_start(...)
 
 struct net_if_mcast_addr *net_if_ipv4_maddr_lookup(const struct in_addr *addr,
 						   struct net_if **iface)
@@ -5069,6 +5217,7 @@ static void notify_iface_up(struct net_if *iface)
 		 */
 		rejoin_multicast_groups(iface);
 		iface_ipv6_start(iface);
+		iface_ipv4_start(iface);
 		net_ipv4_autoconf_start(iface);
 	}
 }
