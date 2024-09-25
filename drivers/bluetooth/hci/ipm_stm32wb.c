e95e30e9516f127b31daed00c3192ee356cc1d91
Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>
2024-09-25 15:58:10 +0200

    Revert "[nrf fromtree] Bluetooth: drivers: Convert STM32 IPM driver to new API"
    This reverts commit 62c988c1bf01bcf79839eb118b16db06953541f0.

--- drivers/bluetooth/hci/ipm_stm32wb.c
+++ drivers/bluetooth/hci/ipm_stm32wb.c
@@ -11,7 +11,7 @@
 #include <zephyr/init.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/bluetooth/hci.h>
-#include <zephyr/drivers/bluetooth.h>
+#include <zephyr/drivers/bluetooth/hci_driver.h>
 #include <zephyr/bluetooth/addr.h>
 #include <zephyr/drivers/clock_control/stm32_clock_control.h>
 #include <zephyr/irq.h>
@@ -21,11 +21,7 @@
 #include "shci.h"
 #include "shci_tl.h"
 
-struct hci_data {
-	bt_hci_recv_t recv;
-};
-
-static const struct stm32_pclken clk_cfg[] = STM32_DT_CLOCKS(DT_DRV_INST(0));
+static const struct stm32_pclken clk_cfg[] = STM32_DT_CLOCKS(DT_NODELABEL(ble_rf));
 
 #define POOL_SIZE (CFG_TLBLE_EVT_QUEUE_LENGTH * 4 * \
 		DIVC((sizeof(TL_PacketHeader_t) + TL_BLE_EVENT_FRAME_SIZE), 4))
@@ -160,9 +156,7 @@ void TM_EvtReceivedCb(TL_EvtPacket_t *hcievt)
 
 static void bt_ipm_rx_thread(void *p1, void *p2, void *p3)
 {
-	const struct device *dev = p1;
-	struct hci_data *hci = dev->data;
-
+	ARG_UNUSED(p1);
 	ARG_UNUSED(p2);
 	ARG_UNUSED(p3);
 
@@ -251,7 +245,7 @@ static void bt_ipm_rx_thread(void *p1, void *p2, void *p3)
 
 		TL_MM_EvtDone(hcievt);
 
-		hci->recv(dev, buf);
+		bt_recv(buf);
 end_loop:
 		k_sem_give(&ipm_busy);
 	}
@@ -353,11 +347,9 @@ void transport_init(void)
 	TL_Enable();
 }
 
-static int bt_ipm_send(const struct device *dev, struct net_buf *buf)
+static int bt_ipm_send(struct net_buf *buf)
 {
 	TL_CmdPacket_t *ble_cmd_buff = &BleCmdBuffer;
-
-	ARG_UNUSED(dev);
 
 	k_sem_take(&ipm_busy, K_FOREVER);
 
@@ -542,9 +534,8 @@ static int c2_reset(void)
 	return 0;
 }
 
-static int bt_ipm_open(const struct device *dev, bt_hci_recv_t recv)
+static int bt_ipm_open(void)
 {
-	struct hci_data *hci = dev->data;
 	int err;
 
 	if (!c2_started_flag) {
@@ -562,7 +553,7 @@ static int bt_ipm_open(const struct device *dev, bt_hci_recv_t recv)
 	/* Start RX thread */
 	k_thread_create(&ipm_rx_thread_data, ipm_rx_stack,
 			K_KERNEL_STACK_SIZEOF(ipm_rx_stack),
-			bt_ipm_rx_thread, (void *)dev, NULL, NULL,
+			bt_ipm_rx_thread, NULL, NULL, NULL,
 			K_PRIO_COOP(CONFIG_BT_DRIVER_RX_HIGH_PRIO),
 			0, K_NO_WAIT);
 
@@ -573,17 +564,14 @@ static int bt_ipm_open(const struct device *dev, bt_hci_recv_t recv)
 	}
 #endif /* CONFIG_BT_HCI_HOST */
 
-	hci->recv = recv;
-
 	LOG_DBG("IPM Channel Open Completed");
 
 	return 0;
 }
 
 #ifdef CONFIG_BT_HCI_HOST
-static int bt_ipm_close(const struct device *dev)
+static int bt_ipm_close(void)
 {
-	struct hci_data *hci = dev->data;
 	int err;
 	struct net_buf *rsp;
 
@@ -602,15 +590,15 @@ static int bt_ipm_close(const struct device *dev)
 
 	k_thread_abort(&ipm_rx_thread_data);
 
-	hci->recv = NULL;
-
 	LOG_DBG("IPM Channel Close Completed");
 
 	return err;
 }
 #endif /* CONFIG_BT_HCI_HOST */
 
-static const struct bt_hci_driver_api drv = {
+static const struct bt_hci_driver drv = {
+	.name           = "BT IPM",
+	.bus            = BT_HCI_DRIVER_BUS_IPM,
 	.open           = bt_ipm_open,
 #ifdef CONFIG_BT_HCI_HOST
 	.close          = bt_ipm_close,
@@ -618,11 +606,12 @@ static const struct bt_hci_driver_api drv = {
 	.send           = bt_ipm_send,
 };
 
-static int _bt_ipm_init(const struct device *dev)
+static int _bt_ipm_init(void)
 {
 	int err;
 
-	ARG_UNUSED(dev);
+
+	bt_hci_driver_register(&drv);
 
 	err = c2_reset();
 	if (err) {
@@ -632,11 +621,4 @@ static int _bt_ipm_init(const struct device *dev)
 	return 0;
 }
 
-#define HCI_DEVICE_INIT(inst) \
-	static struct hci_data hci_data_##inst = { \
-	}; \
-	DEVICE_DT_INST_DEFINE(inst, _bt_ipm_init, NULL, &hci_data_##inst, NULL, \
-			      POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE, &drv)
-
-/* Only one instance supported right now */
-HCI_DEVICE_INIT(0)
+SYS_INIT(_bt_ipm_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
