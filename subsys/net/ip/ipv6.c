2d10763ed52ea8206241b8f454a85a220d59cf60
Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>
2024-10-04 10:48:49 +0200

    Revert "[nrf fromtree] net: ipv6: routing: do not add route to original iface without ND"
    This reverts commit ff1c3ba93ce8b7cf996fab13fb8f2977f82a115b.

--- subsys/net/ip/ipv6.c
+++ subsys/net/ip/ipv6.c
@@ -346,8 +346,7 @@ static enum net_verdict ipv6_route_packet(struct net_pkt *pkt,
 		}
 
 		if (IS_ENABLED(CONFIG_NET_ROUTING) &&
-		    net_pkt_orig_iface(pkt) != net_pkt_iface(pkt) &&
-		    !net_if_flag_is_set(net_pkt_orig_iface(pkt), NET_IF_IPV6_NO_ND)) {
+		    net_pkt_orig_iface(pkt) != net_pkt_iface(pkt)) {
 			/* If the route interface to destination is
 			 * different than the original route, then add
 			 * route to original source.
