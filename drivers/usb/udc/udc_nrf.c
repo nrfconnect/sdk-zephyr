16bcaed54230e3d86aeaf2b2f1d47393967254fd
Johann Fischer <johann.fischer@nordicsemi.no>
2024-10-23 11:23:23 +0200

    [nrf fromtree] drivers: udc_nrf: fix enqueue of control IN transfer with length 0
    If the direction of the last setup packet is not to the device but to
    the host, then the transfer is not a status stage and should be queued.
    This is not checked and prevents a zero length control IN transfer to
    the host, e.g. used by the DFU class to indicate the end of the upload
    process.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>
    (cherry picked from commit 973f914b90a2e204165e9e32cdf4a653acc00cd9)

--- drivers/usb/udc/udc_nrf.c
+++ drivers/usb/udc/udc_nrf.c
@@ -561,7 +561,7 @@ static void udc_nrf_power_handler(nrfx_power_usb_evt_t pwr_evt)
 	}
 }
 
-static void udc_nrf_fake_status_in(const struct device *dev)
+static bool udc_nrf_fake_status_in(const struct device *dev)
 {
 	struct udc_nrf_evt evt = {
 		.type = UDC_NRF_EVT_STATUS_IN,
@@ -572,7 +572,10 @@ static void udc_nrf_fake_status_in(const struct device *dev)
 	    udc_nrf_fake_setup) {
 		/* Let controller perform status IN stage */
 		k_msgq_put(&drv_msgq, &evt, K_NO_WAIT);
+		return true;
 	}
+
+	return false;
 }
 
 static int udc_nrf_ep_enqueue(const struct device *dev,
@@ -587,8 +590,9 @@ static int udc_nrf_ep_enqueue(const struct device *dev,
 	udc_buf_put(cfg, buf);
 
 	if (cfg->addr == USB_CONTROL_EP_IN && buf->len == 0) {
-		udc_nrf_fake_status_in(dev);
-		return 0;
+		if (udc_nrf_fake_status_in(dev)) {
+			return 0;
+		}
 	}
 
 	k_msgq_put(&drv_msgq, &evt, K_NO_WAIT);
