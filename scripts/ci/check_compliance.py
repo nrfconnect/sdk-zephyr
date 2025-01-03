45c2a745ede50078dd6640eba608b56baeeb57de
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2025-01-03 17:19:24 +0100

    Revert "[nrf fromlist] cmake: scripts: support board extension"
    This reverts commit ac11ba595ea48e5f5328ec630b124988d296f668.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- scripts/ci/check_compliance.py
+++ scripts/ci/check_compliance.py
@@ -507,14 +507,12 @@ class KconfigCheck(ComplianceTest):
         kconfig_defconfig_file = os.path.join(kconfig_dir, 'boards', 'Kconfig.defconfig')
 
         root_args = argparse.Namespace(**{'board_roots': [Path(ZEPHYR_BASE)],
-                                          'soc_roots': [Path(ZEPHYR_BASE)], 'board': None,
-                                          'board_dir': []})
-        v2_boards = list_boards.find_v2_boards(root_args).values()
+                                          'soc_roots': [Path(ZEPHYR_BASE)], 'board': None})
+        v2_boards = list_boards.find_v2_boards(root_args)
 
         with open(kconfig_defconfig_file, 'w') as fp:
             for board in v2_boards:
-                fp.write('osource "' +
-                         (board.directories[0] / 'Kconfig.defconfig').as_posix() + '"\n')
+                fp.write('osource "' + (Path(board.dir) / 'Kconfig.defconfig').as_posix() + '"\n')
 
         with open(kconfig_boards_file, 'w') as fp:
             for board in v2_boards:
@@ -527,8 +525,7 @@ class KconfigCheck(ComplianceTest):
                     fp.write('config  ' + board_str + '\n')
                     fp.write('\t bool\n')
                 fp.write(
-                    'source "' +
-                    (board.directories[0] / ('Kconfig.' + board.name)).as_posix() + '"\n\n'
+                    'source "' + (Path(board.dir) / ('Kconfig.' + board.name)).as_posix() + '"\n\n'
                 )
 
         with open(kconfig_file, 'w') as fp:
@@ -536,7 +533,7 @@ class KconfigCheck(ComplianceTest):
                 'osource "' + (Path(kconfig_dir) / 'boards' / 'Kconfig.syms.v1').as_posix() + '"\n'
             )
             for board in v2_boards:
-                fp.write('osource "' + (Path(board.directories[0]) / 'Kconfig').as_posix() + '"\n')
+                fp.write('osource "' + (Path(board.dir) / 'Kconfig').as_posix() + '"\n')
 
         kconfig_defconfig_file = os.path.join(kconfig_dir, 'soc', 'Kconfig.defconfig')
         kconfig_soc_file = os.path.join(kconfig_dir, 'soc', 'Kconfig.soc')
@@ -545,7 +542,7 @@ class KconfigCheck(ComplianceTest):
         root_args = argparse.Namespace(**{'soc_roots': soc_roots})
         v2_systems = list_hardware.find_v2_systems(root_args)
 
-        soc_folders = {soc.folder[0] for soc in v2_systems.get_socs()}
+        soc_folders = {soc.folder for soc in v2_systems.get_socs()}
         with open(kconfig_defconfig_file, 'w') as fp:
             for folder in soc_folders:
                 fp.write('osource "' + (Path(folder) / 'Kconfig.defconfig').as_posix() + '"\n')
@@ -615,7 +612,7 @@ class KconfigCheck(ComplianceTest):
         os.makedirs(os.path.join(kconfiglib_dir, 'soc'), exist_ok=True)
         os.makedirs(os.path.join(kconfiglib_dir, 'arch'), exist_ok=True)
 
-        os.environ["KCONFIG_BOARD_DIR"] = kconfiglib_boards_dir
+        os.environ["BOARD_DIR"] = kconfiglib_boards_dir
         self.get_v2_model(kconfiglib_dir)
 
         # Tells Kconfiglib to generate warnings for all references to undefined
@@ -925,9 +922,6 @@ flagged.
         "BOOT_RAM_LOAD", # Used in sysbuild for MCUboot configuration
         "BOOT_SWAP_USING_MOVE", # Used in sysbuild for MCUboot configuration
         "BOOT_SWAP_USING_SCRATCH", # Used in sysbuild for MCUboot configuration
-        "BOARD_MPS2_AN521_CPUTEST", # Used for board and SoC extension feature tests
-        "BOARD_NATIVE_SIM_NATIVE_64_TWO", # Used for board and SoC extension feature tests
-        "BOARD_NATIVE_SIM_NATIVE_ONE", # Used for board and SoC extension feature tests
         "BOOT_ENCRYPTION_KEY_FILE", # Used in sysbuild
         "BOOT_ENCRYPT_IMAGE", # Used in sysbuild
         "BOOT_MAX_IMG_SECTORS_AUTO", # Used in sysbuild
