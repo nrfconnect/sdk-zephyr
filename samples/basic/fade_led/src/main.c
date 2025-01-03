2959c11053a744573fe123ae09339ba7fbd160cd
Piotr Krzyzanowski <piotr.krzyzanowski@nordicsemi.no>
2025-01-03 22:09:16 +0100

    [DNM] samples: basic: fade_led: pwm120_test
    pwm120 100% duty cycle bug showcase
    
    Signed-off-by: Piotr Krzyzanowski <piotr.krzyzanowski@nordicsemi.no>

--- samples/basic/fade_led/src/main.c
+++ samples/basic/fade_led/src/main.c
@@ -16,15 +16,11 @@
 
 static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
 
-#define NUM_STEPS	50U
-#define SLEEP_MSEC	25U
 
 int main(void)
 {
-	uint32_t pulse_width = 0U;
-	uint32_t step = pwm_led0.period / NUM_STEPS;
-	uint8_t dir = 1U;
 	int ret;
+	uint32_t pulse_width;
 
 	printk("PWM-based LED fade\n");
 
@@ -35,6 +31,7 @@ int main(void)
 	}
 
 	while (1) {
+		pulse_width = 0;
 		ret = pwm_set_pulse_dt(&pwm_led0, pulse_width);
 		if (ret) {
 			printk("Error %d: failed to set pulse width\n", ret);
@@ -42,22 +39,27 @@ int main(void)
 		}
 		printk("Using pulse width %d%%\n", 100 * pulse_width / pwm_led0.period);
 
-		if (dir) {
-			pulse_width += step;
-			if (pulse_width >= pwm_led0.period) {
-				pulse_width = pwm_led0.period - step;
-				dir = 0U;
-			}
-		} else {
-			if (pulse_width >= step) {
-				pulse_width -= step;
-			} else {
-				pulse_width = step;
-				dir = 1U;
-			}
-		}
+		k_msleep(1000);
 
-		k_sleep(K_MSEC(SLEEP_MSEC));
+		pulse_width = pwm_led0.period / 2;
+		ret = pwm_set_pulse_dt(&pwm_led0, pulse_width);
+		if (ret) {
+			printk("Error %d: failed to set pulse width\n", ret);
+			return 0;
+		}
+		printk("Using pulse width %d%%\n", 100 * pulse_width / pwm_led0.period);
+
+		k_msleep(1000);
+
+		pulse_width = pwm_led0.period;
+		ret = pwm_set_pulse_dt(&pwm_led0, pulse_width);
+		if (ret) {
+			printk("Error %d: failed to set pulse width\n", ret);
+			return 0;
+		}
+		printk("Using pulse width %d%%\n", 100 * pulse_width / pwm_led0.period);
+
+		k_msleep(1000);
 	}
 	return 0;
 }
