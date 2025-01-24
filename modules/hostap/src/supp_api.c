23d63b8a9e5e78e5054b515310722a4b8c90cd3e
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2025-01-24 09:35:47 +0100

    Revert "[nrf fromtree] hostap: Fix hostapd AP compilation error"
    This reverts commit ec45c1a524769036157613693d0fb2a5c9d9b72e.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- modules/hostap/src/supp_api.c
+++ modules/hostap/src/supp_api.c
@@ -1847,6 +1847,7 @@ out:
 
 	return ret;
 }
+#endif
 
 int supplicant_ap_status(const struct device *dev, struct wifi_iface_status *status)
 {
@@ -1921,7 +1922,6 @@ out:
 	k_mutex_unlock(&wpa_supplicant_mutex);
 	return ret;
 }
-#endif /* CONFIG_WIFI_NM_HOSTAPD_AP */
 
 int supplicant_ap_enable(const struct device *dev,
 			 struct wifi_connect_req_params *params)
