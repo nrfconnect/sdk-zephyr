bf52523e9a783e1234fdd33dc6032df5cf76d52a
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2025-01-29 10:31:06 +0100

    Revert "[nrf fromtree] net: wifi: shell: add enterprise support for station"
    This reverts commit 6d75db5444ac20d079a781281ed680912a42c99b.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- modules/hostap/src/supp_main.h
+++ modules/hostap/src/supp_main.h
@@ -15,7 +15,7 @@
 	!defined(CONFIG_EAP_PSK) && !defined(CONFIG_EAP_PAX) && \
 	!defined(CONFIG_EAP_SAKE) && !defined(CONFIG_EAP_GPSK) && \
 	!defined(CONFIG_EAP_PWD) && !defined(CONFIG_EAP_EKE) && \
-	!defined(CONFIG_EAP_IKEV2 && !defined(CONFIG_EAP_GTC)
+	!defined(CONFIG_EAP_IKEV2)
 #error "At least one of the following EAP methods need to be defined    \
 	CONFIG_EAP_TLS    \
 	CONFIG_EAP_TTLS   \
@@ -32,7 +32,6 @@
 	CONFIG_EAP_IKEV2   \
 	CONFIG_EAP_SIM   \
 	CONFIG_EAP_AKA   \
-	CONFIG_EAP_GTC   \
 	CONFIG_EAP_ALL "
 #endif /* EAP METHODS */
 #endif /* CONFIG_WIFI_NM_WPA_SUPPLICANT_CRYPTO_ENTERPRISE */
