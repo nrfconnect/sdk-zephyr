8ee8e962a3deb32b56ff99de384e9ac18056b632
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2025-01-29 10:31:30 +0100

    Revert "[nrf fromlist] net: wifi: Fix DPP disabled build"
    This reverts commit 29b9b3e2052e7022ce8a5c197612e08d75796535.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- modules/hostap/src/supp_api.h
+++ modules/hostap/src/supp_api.h
@@ -268,7 +268,6 @@ int supplicant_ap_sta_disconnect(const struct device *dev,
  * @return 0 for OK; -1 for ERROR
  */
 int supplicant_dpp_dispatch(const struct device *dev, struct wifi_dpp_params *params);
-#endif /* CONFIG_WIFI_NM_WPA_SUPPLICANT_DPP */
 
 #ifdef CONFIG_WIFI_NM_HOSTAPD_AP
 /**
