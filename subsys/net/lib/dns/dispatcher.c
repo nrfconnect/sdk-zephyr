5ab3d929bb93eac215d1f403f2cf0f56ca476ac6
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2025-02-05 16:18:48 +0100

    Revert "[nrf fromtree] net: dns: dispatcher: Don't use uninitialized entries"
    This reverts commit b5c4e13237a99901ee1767bb405b3b6cf2c8f6d2.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- subsys/net/lib/dns/dispatcher.c
+++ subsys/net/lib/dns/dispatcher.c
@@ -240,10 +240,6 @@ int dns_dispatcher_register(struct dns_socket_dispatcher *ctx)
 				goto out;
 			}
 
-			if (ctx->fds[i].fd < 0) {
-				continue;
-			}
-
 			if (dispatch_table[ctx->fds[i].fd].ctx == NULL) {
 				dispatch_table[ctx->fds[i].fd].ctx = ctx;
 			}
@@ -279,10 +275,6 @@ int dns_dispatcher_register(struct dns_socket_dispatcher *ctx)
 		if ((int)ctx->fds[i].fd >= (int)ARRAY_SIZE(dispatch_table)) {
 			ret = -ERANGE;
 			goto out;
-		}
-
-		if (ctx->fds[i].fd < 0) {
-			continue;
 		}
 
 		if (dispatch_table[ctx->fds[i].fd].ctx == NULL) {
