4cd2d4ab4a81947c8453e1bf9bb476daf0460879
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2025-02-12 11:29:07 +0100

    Revert "[nrf fromtree] samples: boards: nordic: coresight_stm: Add flpr and ppr support"
    This reverts commit ab41cd0ecb692509b607f4775fe41e98ef21c3d1.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- samples/boards/nordic/coresight_stm/sysbuild.cmake
+++ samples/boards/nordic/coresight_stm/sysbuild.cmake
@@ -1,28 +1,16 @@
 # Copyright (c) 2024 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
-set(REMOTE_APP remote)
+if("${SB_CONFIG_RAD_CORE_BOARD}" STREQUAL "")
+	message(FATAL_ERROR
+	"Target ${BOARD} not supported for this sample. "
+	"There is no remote board selected in Kconfig.sysbuild")
+endif()
+
+set(RAD_APP remote)
 
 ExternalZephyrProject_Add(
-	APPLICATION ${REMOTE_APP}_rad
-	SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
-	BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpurad
+	APPLICATION ${RAD_APP}
+	SOURCE_DIR  ${APP_DIR}/${RAD_APP}
+	BOARD       ${SB_CONFIG_RAD_CORE_BOARD}
 )
-
-# There are sample configurations which do not use PPR.
-if(SB_CONFIG_APP_CPUPPR_RUN)
-	ExternalZephyrProject_Add(
-		APPLICATION ${REMOTE_APP}_ppr
-		SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
-		BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpuppr
-	)
-endif()
-
-# There are sample configurations which do not use FLPR.
-if(SB_CONFIG_APP_CPUFLPR_RUN)
-	ExternalZephyrProject_Add(
-		APPLICATION ${REMOTE_APP}_flpr
-		SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
-		BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpuflpr
-	)
-endif()
