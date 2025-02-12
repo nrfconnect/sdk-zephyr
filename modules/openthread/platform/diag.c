61c91a0108e8f0c562f6810b3aa9165a45a5c1f4
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2025-02-12 11:28:08 +0100

    Revert "[nrf fromtree] net: openthread: cleanup diag commands"
    This reverts commit 52a1ceee359c93ecf0cfeee835b60e7b4c8c78a5.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- modules/openthread/platform/diag.c
+++ modules/openthread/platform/diag.c
@@ -125,8 +125,8 @@ void otPlatDiagRadioReceived(otInstance *aInstance,
 #if defined(CONFIG_IEEE802154_CARRIER_FUNCTIONS)
 otError otPlatDiagRadioTransmitCarrier(otInstance *aInstance, bool aEnable)
 {
-	if (sTransmitMode != DIAG_TRANSMIT_MODE_IDLE &&
-	    sTransmitMode != DIAG_TRANSMIT_MODE_CARRIER) {
+	if (!otPlatDiagModeGet() || (sTransmitMode != DIAG_TRANSMIT_MODE_IDLE &&
+				     sTransmitMode != DIAG_TRANSMIT_MODE_CARRIER)) {
 		return OT_ERROR_INVALID_STATE;
 	}
 
@@ -174,6 +174,10 @@ static otError gpio_get_spec(uint32_t gpio_idx, const struct gpio_dt_spec **spec
 	}
 
 	*spec = &gpio_spec[gpio_idx];
+
+	if (!otPlatDiagModeGet()) {
+		return OT_ERROR_INVALID_STATE;
+	}
 
 	if (!gpio_is_ready_dt(*spec)) {
 		return OT_ERROR_INVALID_ARGS;
@@ -322,8 +326,8 @@ static otError startModCarrier(otInstance *aInstance, uint8_t aArgsLength, char 
 		return OT_ERROR_INVALID_ARGS;
 	}
 
-	if (sTransmitMode != DIAG_TRANSMIT_MODE_IDLE &&
-	    sTransmitMode != DIAG_TRANSMIT_MODE_MODCARRIER) {
+	if (!otPlatDiagModeGet() || (sTransmitMode != DIAG_TRANSMIT_MODE_IDLE &&
+				     sTransmitMode != DIAG_TRANSMIT_MODE_MODCARRIER)) {
 		return OT_ERROR_INVALID_STATE;
 	}
 
@@ -391,6 +395,10 @@ static otError processTransmit(otInstance *aInstance, uint8_t aArgsLength, char 
 	otError error = OT_ERROR_NONE;
 	long value;
 	uint32_t now;
+
+	if (!otPlatDiagModeGet()) {
+		return OT_ERROR_INVALID_STATE;
+	}
 
 	if (aArgsLength == 0) {
 		diag_output("transmit will send %" PRId32 " diagnostic messages with %" PRIu32
