1b9601d6c26b2ec95c6e9d8ae805608b541e392d
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2025-02-12 11:28:28 +0100

    Revert "[nrf fromtree] boards: nxp: Add FRDM-MCXW71"
    This reverts commit ac4d00d3420c7e88e85f001da1261434e29c7bea.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- boards/nxp/frdm_mcxw71/board.cmake
+++
@@ -1,6 +0,0 @@
-# Copyright 2024 NXP
-# SPDX-License-Identifier: Apache-2.0
-
-board_runner_args(jlink "--device=mcxw716" "--reset-after-load")
-
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
