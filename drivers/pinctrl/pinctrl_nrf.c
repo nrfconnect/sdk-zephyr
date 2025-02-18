698fc1618e87126e01d3d2ed077598d900190a3a
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2025-02-18 13:07:37 +0100

    [nrf fromlist]  drivers: pinctrl: nrf: Add support for TDM peripheral
    Add support for configuring pins of the nRF TDM peripheral.
    
    Signed-off-by: Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>
    
    Upstream PR #: 82144
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>

--- drivers/pinctrl/pinctrl_nrf.c
+++ drivers/pinctrl/pinctrl_nrf.c
@@ -99,6 +99,10 @@ static const nrf_gpio_pin_drive_t drive_modes[NRF_DRIVE_COUNT] = {
 #define NRF_PSEL_TWIS(reg, line) ((NRF_TWIS_Type *)reg)->PSEL.line
 #endif
 
+#if DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_tdm)
+#define NRF_PSEL_TDM(reg, line) ((NRF_TDM_Type *)reg)->PSEL.line
+#endif
+
 #if DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_grtc) || defined(CONFIG_NRFX_GRTC)
 #if DT_NODE_HAS_PROP(DT_NODELABEL(grtc), clkout_fast_frequency)
 #define NRF_GRTC_CLKOUT_FAST 1
@@ -361,6 +365,47 @@ int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt,
 			input = NRF_GPIO_PIN_INPUT_DISCONNECT;
 			break;
 #endif /* defined(NRF_PSEL_QSPI) */
+#if defined(NRF_PSEL_TDM)
+		case NRF_FUN_TDM_SCK_M:
+			NRF_PSEL_TDM(reg, SCK) = psel;
+			write = 0U;
+			dir = NRF_GPIO_PIN_DIR_OUTPUT;
+			input = NRF_GPIO_PIN_INPUT_DISCONNECT;
+			break;
+		case NRF_FUN_TDM_SCK_S:
+			NRF_PSEL_TDM(reg, SCK) = psel;
+			dir = NRF_GPIO_PIN_DIR_INPUT;
+			input = NRF_GPIO_PIN_INPUT_CONNECT;
+			break;
+		case NRF_FUN_TDM_FSYNC_M:
+			NRF_PSEL_TDM(reg, FSYNC) = psel;
+			write = 0U;
+			dir = NRF_GPIO_PIN_DIR_OUTPUT;
+			input = NRF_GPIO_PIN_INPUT_DISCONNECT;
+			break;
+		case NRF_FUN_TDM_FSYNC_S:
+			NRF_PSEL_TDM(reg, FSYNC) = psel;
+			dir = NRF_GPIO_PIN_DIR_INPUT;
+			input = NRF_GPIO_PIN_INPUT_CONNECT;
+			break;
+		case NRF_FUN_TDM_SDIN:
+			NRF_PSEL_TDM(reg, SDIN) = psel;
+			dir = NRF_GPIO_PIN_DIR_INPUT;
+			input = NRF_GPIO_PIN_INPUT_CONNECT;
+			break;
+		case NRF_FUN_TDM_SDOUT:
+			NRF_PSEL_TDM(reg, SDOUT) = psel;
+			write = 0U;
+			dir = NRF_GPIO_PIN_DIR_OUTPUT;
+			input = NRF_GPIO_PIN_INPUT_DISCONNECT;
+			break;
+		case NRF_FUN_TDM_MCK:
+			NRF_PSEL_TDM(reg, MCK) = psel;
+			write = 0U;
+			dir = NRF_GPIO_PIN_DIR_OUTPUT;
+			input = NRF_GPIO_PIN_INPUT_DISCONNECT;
+			break;
+#endif /* defined(NRF_PSEL_TDM) */
 #if defined(NRF_GRTC_CLKOUT_FAST)
 		case NRF_FUN_GRTC_CLKOUT_FAST:
 #if NRF_GPIO_HAS_SEL && defined(GPIO_PIN_CNF_CTRLSEL_GRTC)
