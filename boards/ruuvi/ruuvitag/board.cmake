a94033d63f7e5be1a89644362f3040b0877afc83
Tomasz Moń <tomasz.mon@nordicsemi.no>
2025-03-14 10:48:26 +0100

    Revert "[nrf fromlist] boards: nrf: Default to nRF Util for flashing"
    This reverts commit a72ee93ab45f0b935be6e408345cd4adfa47ce22.
    
    Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>

--- boards/ruuvi/ruuvitag/board.cmake
+++ boards/ruuvi/ruuvitag/board.cmake
@@ -2,6 +2,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=nRF52832_xxAA" "--speed=4000")
-include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
