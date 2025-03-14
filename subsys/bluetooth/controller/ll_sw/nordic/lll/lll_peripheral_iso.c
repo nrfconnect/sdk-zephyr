e9ee49410ab6d08038649ff2836dec0fd4a3ca2f
Tomasz Moń <tomasz.mon@nordicsemi.no>
2025-03-14 10:48:32 +0100

    Revert "[nrf fromtree] Bluetooth: Controller: Single timer use ISO Peripheral nRF54L support"
    This reverts commit 218b06f3a07582db13d937a04e4ffa4b24ebf156.
    
    Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_peripheral_iso.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_peripheral_iso.c
@@ -462,6 +462,8 @@ static void isr_rx(void *param)
 	struct pdu_cis *pdu_tx;
 	uint64_t payload_count;
 	uint8_t payload_index;
+	uint32_t subevent_us;
+	uint32_t start_us;
 	uint8_t trx_done;
 	uint8_t crc_ok;
 	uint8_t cie;
@@ -805,12 +807,6 @@ static void isr_rx(void *param)
 		se_curr = 0U;
 	}
 
-	radio_isr_set(isr_tx, cis_lll);
-
-#if !defined(CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER)
-	uint32_t subevent_us;
-	uint32_t start_us;
-
 	/* Schedule next subevent reception */
 	subevent_us = radio_tmr_aa_restore();
 	subevent_us += cis_lll->offset - cis_offset_first +
@@ -829,7 +825,8 @@ static void isr_rx(void *param)
 
 	start_us = radio_tmr_start_us(0U, subevent_us);
 	LL_ASSERT(start_us == (subevent_us + 1U));
-#endif /* !CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER */
+
+	radio_isr_set(isr_tx, cis_lll);
 }
 
 static void isr_tx(void *param)
@@ -926,14 +923,8 @@ static void isr_tx(void *param)
 	subevent_us -= radio_rx_chain_delay_get(0U, 0U);
 #endif /* !CONFIG_BT_CTLR_PHY */
 
-#if defined(CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER)
-	start_us = radio_tmr_start_us(0U, subevent_us);
-	LL_ASSERT(start_us == (subevent_us + 1U));
-
-#else /* !CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER */
 	/* Compensate for the 1 us added by radio_tmr_start_us() */
 	start_us = subevent_us + 1U;
-#endif /* !CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER */
 
 	hcto = start_us +
 	       ((EVENT_JITTER_US + EVENT_TICKER_RES_MARGIN_US +
@@ -950,7 +941,7 @@ static void isr_tx(void *param)
 	hcto += radio_rx_chain_delay_get(0U, 0U);
 #endif /* !CONFIG_BT_CTLR_PHY */
 
-	radio_tmr_hcto_configure_abs(hcto);
+	radio_tmr_hcto_configure(hcto);
 
 #if defined(HAL_RADIO_GPIO_HAVE_LNA_PIN)
 	radio_gpio_lna_setup();
@@ -1181,7 +1172,7 @@ static void isr_prepare_subevent_common(void *param)
 	hcto += radio_rx_chain_delay_get(0U, 0U);
 #endif /* !CONFIG_BT_CTLR_PHY */
 
-	radio_tmr_hcto_configure_abs(hcto);
+	radio_tmr_hcto_configure(hcto);
 
 #if defined(HAL_RADIO_GPIO_HAVE_LNA_PIN)
 	radio_gpio_lna_setup();
