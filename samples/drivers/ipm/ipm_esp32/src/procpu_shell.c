zephyrproject-rtos/zephyr

samples: driver: ipm: esp32 sysbuild refactor
Make APP_CPU board name auto generated and remove the related config.
Remove the boards overlay and replace with a socs overlays.

Signed-off-by: Marek Matej <marek.matej@espressif.com>

samples/drivers/ipm/ipm_esp32/src/procpu_shell.c
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#if defined(CONFIG_IPM) || defined(CONIFG_MBOX)
+
 #include <stdlib.h>
 
 #include <zephyr/device.h>
@@ -57,3 +59,4 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_amp,
 );
 
 SHELL_CMD_REGISTER(amp, &sub_amp, "AMP debug commands.", NULL);
+#endif

nrfconnect/sdk-zephyr

samples/drivers/ipm/ipm_esp32/src/procpu_shell.c
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#if defined(CONFIG_IPM) || defined(CONFIG_MBOX)
+
 #include <stdlib.h>
 
 #include <zephyr/device.h>
@@ -57,3 +59,4 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_amp,
 );
 
 SHELL_CMD_REGISTER(amp, &sub_amp, "AMP debug commands.", NULL);
+#endif