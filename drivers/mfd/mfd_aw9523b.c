anilmaguluri/zephyr_ble

drivers: gpio: Add support for Awinic AW9523B GPIO controller
Add support for GPIO controller feature of AW9523B.

Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

drivers/mfd/mfd_aw9523b.c
@@ -1,0 +1,65 @@
+/*
+ * Copyright (c) 2024 TOKITA Hiroshi
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT awinic_aw9523b
+
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/mfd/aw9523b.h>
+#include <zephyr/kernel.h>
+
+#define AW9523B_ID_VALUE 0x23
+
+struct mfd_aw9523b_config {
+	struct i2c_dt_spec i2c;
+};
+
+struct mfd_aw9523b_data {
+	struct k_sem lock;
+};
+
+static int mfd_aw9523b_init(const struct device *dev)
+{
+	const struct mfd_aw9523b_config *config = dev->config;
+	struct mfd_aw9523b_data *data = dev->data;
+	uint8_t reg;
+	int ret;
+
+	if (!i2c_is_ready_dt(&config->i2c)) {
+		return -ENODEV;
+	}
+
+	k_sem_init(&data->lock, 1, 1);
+
+	ret = i2c_reg_read_byte_dt(&config->i2c, AW9523B_REG_ADDR_ID, &reg);
+
+	if (ret) {
+		return ret;
+	};
+
+	if (reg != AW9523B_ID_VALUE) {
+		return -1;
+	}
+
+	return 0;
+}
+
+struct k_sem *aw9523b_get_lock(const struct device *dev)
+{
+	struct mfd_aw9523b_data *data = dev->data;
+
+	return &data->lock;
+}
+
+#define MFD_AW9523B_DEFINE(inst)                                                                   \
+	static const struct mfd_aw9523b_config config##inst = {                                    \
+		.i2c = I2C_DT_SPEC_INST_GET(inst),                                                 \
+	};                                                                                         \
+                                                                                                   \
+	static struct mfd_aw9523b_data data##inst;                                                 \
+                                                                                                   \
+	DEVICE_DT_INST_DEFINE(inst, mfd_aw9523b_init, NULL, &data##inst, &config##inst,            \
+			      POST_KERNEL, CONFIG_MFD_AW9523B_INIT_PRIORITY, NULL);
+
+DT_INST_FOREACH_STATUS_OKAY(MFD_AW9523B_DEFINE)

include/zephyr/drivers/mfd/aw9523b.h
@@ -1,0 +1,45 @@
+/*
+ * Copyright (c) 2024 TOKITA Hiroshi
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_MFD_AW9523B_H_
+#define ZEPHYR_INCLUDE_DRIVERS_MFD_AW9523B_H_
+
+struct k_sem;
+struct device;
+
+#define AW9523B_REG_ADDR_INPUT0  0x00
+#define AW9523B_REG_ADDR_INPUT1  0x01
+#define AW9523B_REG_ADDR_OUTPUT0 0x02
+#define AW9523B_REG_ADDR_OUTPUT1 0x03
+#define AW9523B_REG_ADDR_CONFIG0 0x04
+#define AW9523B_REG_ADDR_CONFIG1 0x05
+#define AW9523B_REG_ADDR_INT0    0x06
+#define AW9523B_REG_ADDR_INT1    0x07
+#define AW9523B_REG_ADDR_ID      0x10
+#define AW9523B_REG_ADDR_CTL     0x11
+#define AW9523B_REG_ADDR_MODE0   0x12
+#define AW9523B_REG_ADDR_MODE1   0x13
+#define AW9523B_REG_ADDR_DIM0    0x20
+#define AW9523B_REG_ADDR_DIM1    0x21
+#define AW9523B_REG_ADDR_DIM2    0x22
+#define AW9523B_REG_ADDR_DIM3    0x23
+#define AW9523B_REG_ADDR_DIM4    0x24
+#define AW9523B_REG_ADDR_DIM5    0x25
+#define AW9523B_REG_ADDR_DIM6    0x26
+#define AW9523B_REG_ADDR_DIM7    0x27
+#define AW9523B_REG_ADDR_DIM8    0x28
+#define AW9523B_REG_ADDR_DIM9    0x29
+#define AW9523B_REG_ADDR_DIM10   0x2A
+#define AW9523B_REG_ADDR_DIM11   0x2B
+#define AW9523B_REG_ADDR_DIM12   0x2C
+#define AW9523B_REG_ADDR_DIM13   0x2D
+#define AW9523B_REG_ADDR_DIM14   0x2E
+#define AW9523B_REG_ADDR_DIM15   0x2F
+#define AW9523B_REG_ADDR_SW_RSTN 0x7F
+
+struct k_sem *aw9523b_get_lock(const struct device *dev);
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_MFD_AW9523B_H_ */

nrfconnect/sdk-zephyr

Co-authored-by: Benjamin Cabé <kartben@gmail.com>

drivers/mfd/mfd_aw9523b.c
@@ -1,0 +1,65 @@
+/*
+ * Copyright (c) 2024 TOKITA Hiroshi
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT awinic_aw9523b
+
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/mfd/aw9523b.h>
+#include <zephyr/kernel.h>
+
+#define AW9523B_ID_VALUE 0x23
+
+struct mfd_aw9523b_config {
+	struct i2c_dt_spec i2c;
+};
+
+struct mfd_aw9523b_data {
+	struct k_sem lock;
+};
+
+static int mfd_aw9523b_init(const struct device *dev)
+{
+	const struct mfd_aw9523b_config *config = dev->config;
+	struct mfd_aw9523b_data *data = dev->data;
+	uint8_t reg;
+	int ret;
+
+	if (!i2c_is_ready_dt(&config->i2c)) {
+		return -ENODEV;
+	}
+
+	k_sem_init(&data->lock, 1, 1);
+
+	ret = i2c_reg_read_byte_dt(&config->i2c, AW9523B_REG_ID, &reg);
+
+	if (ret) {
+		return ret;
+	};
+
+	if (reg != AW9523B_ID_VALUE) {
+		return -1;
+	}
+
+	return 0;
+}
+
+struct k_sem *aw9523b_get_lock(const struct device *dev)
+{
+	struct mfd_aw9523b_data *data = dev->data;
+
+	return &data->lock;
+}
+
+#define MFD_AW9523B_DEFINE(inst)                                                                   \
+	static const struct mfd_aw9523b_config config##inst = {                                    \
+		.i2c = I2C_DT_SPEC_INST_GET(inst),                                                 \
+	};                                                                                         \
+                                                                                                   \
+	static struct mfd_aw9523b_data data##inst;                                                 \
+                                                                                                   \
+	DEVICE_DT_INST_DEFINE(inst, mfd_aw9523b_init, NULL, &data##inst, &config##inst,            \
+			      POST_KERNEL, CONFIG_MFD_INIT_PRIORITY, NULL);
+
+DT_INST_FOREACH_STATUS_OKAY(MFD_AW9523B_DEFINE)

include/zephyr/drivers/mfd/aw9523b.h
@@ -1,0 +1,45 @@
+/*
+ * Copyright (c) 2024 TOKITA Hiroshi
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_MFD_AW9523B_H_
+#define ZEPHYR_INCLUDE_DRIVERS_MFD_AW9523B_H_
+
+struct k_sem;
+struct device;
+
+#define AW9523B_REG_INPUT0  0x00
+#define AW9523B_REG_INPUT1  0x01
+#define AW9523B_REG_OUTPUT0 0x02
+#define AW9523B_REG_OUTPUT1 0x03
+#define AW9523B_REG_CONFIG0 0x04
+#define AW9523B_REG_CONFIG1 0x05
+#define AW9523B_REG_INT0    0x06
+#define AW9523B_REG_INT1    0x07
+#define AW9523B_REG_ID      0x10
+#define AW9523B_REG_CTL     0x11
+#define AW9523B_REG_MODE0   0x12
+#define AW9523B_REG_MODE1   0x13
+#define AW9523B_REG_DIM0    0x20
+#define AW9523B_REG_DIM1    0x21
+#define AW9523B_REG_DIM2    0x22
+#define AW9523B_REG_DIM3    0x23
+#define AW9523B_REG_DIM4    0x24
+#define AW9523B_REG_DIM5    0x25
+#define AW9523B_REG_DIM6    0x26
+#define AW9523B_REG_DIM7    0x27
+#define AW9523B_REG_DIM8    0x28
+#define AW9523B_REG_DIM9    0x29
+#define AW9523B_REG_DIM10   0x2A
+#define AW9523B_REG_DIM11   0x2B
+#define AW9523B_REG_DIM12   0x2C
+#define AW9523B_REG_DIM13   0x2D
+#define AW9523B_REG_DIM14   0x2E
+#define AW9523B_REG_DIM15   0x2F
+#define AW9523B_REG_SW_RSTN 0x7F
+
+struct k_sem *aw9523b_get_lock(const struct device *dev);
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_MFD_AW9523B_H_ */