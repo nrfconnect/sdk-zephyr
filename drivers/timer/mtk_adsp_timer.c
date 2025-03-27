ravi-sharma-adi/zephyr

boards/mediatek: Add mt8196_adsp
Add Zephyr support for the Audio DSP on the MT8196 SOC.  This is a
very similar device to previous designs.  Most of this patch is just
DTS.

The biggest delta is the more complicated second level interrupt
controller, though it is still able to be represented using some
vaguely clever DTS config over the older intc_mtk_adsp driver.

Also the memory layout is slightly different, requiring a little
indirection to set the initial boot stack address and log output
buffer.  And the timer "irq_ack" register bits moved.

Signed-off-by: Andy Ross <andyross@google.com>

drivers/timer/mtk_adsp_timer.c
@@ -56,8 +56,13 @@ struct mtk_ostimer64 {
 #define OSTIMER_CON_CLKSRC_BCLK 0x20 /*  CPU speed, 720 MHz */
 #define OSTIMER_CON_CLKSRC_PCLK 0x30 /*  ~312 MHz experimentally */
 
+#ifndef CONFIG_SOC_MT8196_ADSP
 #define OSTIMER_IRQ_ACK_ENABLE BIT(4) /*  read = status, write = enable */
 #define OSTIMER_IRQ_ACK_CLEAR  BIT(5)
+#else
+#define OSTIMER_IRQ_ACK_ENABLE BIT(0)
+#define OSTIMER_IRQ_ACK_CLEAR  BIT(5)
+#endif
 
 #define OST64_HZ 13000000U
 #define OST_HZ 26000000U

soc/mediatek/mtk_adsp/irq.c
@@ -25,6 +25,16 @@ static const struct device *irq_dev(unsigned int *irq_inout)
 	__ASSERT_NO_MSG((*irq_inout & 0xff) == 23);
 	*irq_inout = (*irq_inout >> 8) - 1;
 	return DEVICE_DT_GET(DT_INST(1, mediatek_adsp_intc));
+#elif defined(CONFIG_SOC_SERIES_MT8196_ADSP)
+	/* Two subcontrollers on core IRQs 1 and 2 */
+	uint32_t lvl1 = *irq_inout & 0xff;
+
+	*irq_inout = (*irq_inout >> 8) - 1;
+	if (lvl1 == 1) {
+		return DEVICE_DT_GET(DT_INST(0, mediatek_adsp_intc));
+	}
+	__ASSERT_NO_MSG(lvl1 == 2);
+	return DEVICE_DT_GET(DT_INST(1, mediatek_adsp_intc));
 #else
 	/* Only one on 818x */
 	return DEVICE_DT_GET(DT_INST(0, mediatek_adsp_intc));

soc/mediatek/mtk_adsp/soc.c
@@ -19,14 +19,23 @@ extern char _mtk_adsp_dram_end[];
 #define DRAM_SIZE  DT_REG_SIZE(DT_NODELABEL(dram0))
 #define DRAM_END   (DRAM_START + DRAM_SIZE)
 
+#ifdef CONFIG_SOC_MT8196_ADSP
+#define INIT_STACK "0x90400000"
+#define LOG_BASE   0x90580000
+#define LOG_LEN    0x80000
+#else
+#define INIT_STACK "0x60e00000"
+#define LOG_BASE   0x60700000
+#define LOG_LEN    0x100000
+#endif
+
 /* This is the true boot vector.  This device allows for direct
  * setting of the alternate reset vector, so we let it link wherever
  * it lands and extract its address in the loader.  This represents
  * the minimum amount of effort required to successfully call a C
  * function (and duplicates a few versions elsewhere in the tree:
- * really this should move to the arch layer).  Note that the stack
- * used is 15MB into the DRAM region and safe/unused by all devices.
- * But really this should be z_interrupt_stacks[0].
+ * really this should move to the arch layer).  The initial stack
+ * really should be the end of _interrupt_stacks[0]
  */
 __asm__(".align 4\n\t"
 	".global mtk_adsp_boot_entry\n\t"
@@ -38,7 +47,7 @@ __asm__(".align 4\n\t"
 	"  movi  a0, 1\n\t"
 	"  wsr   a0, WINDOWSTART\n\t"
 	"  rsync\n\t"
-	"  movi  a1, 0x60e00000\n\t"
+	"  movi  a1, " INIT_STACK "\n\t"
 	"  call4 c_boot\n\t");
 
 /* Initial MPU configuration, needed to enable caching */
@@ -109,8 +118,8 @@ static void enable_mpu(void)
  */
 int arch_printk_char_out(int c)
 {
-	char volatile * const buf = (void *)0x60700000;
-	const size_t max = 0x100000 - 4;
+	char volatile * const buf = (void *)LOG_BASE;
+	const size_t max = LOG_LEN - 4;
 	int volatile * const len = (int *)&buf[max];
 
 	if (*len < max) {

nrfconnect/sdk-zephyr

drivers/timer/mtk_adsp_timer.c
@@ -56,8 +56,13 @@ struct mtk_ostimer64 {
 #define OSTIMER_CON_CLKSRC_BCLK 0x20 /*  CPU speed, 720 MHz */
 #define OSTIMER_CON_CLKSRC_PCLK 0x30 /*  ~312 MHz experimentally */
 
+#ifndef CONFIG_SOC_MT8196
 #define OSTIMER_IRQ_ACK_ENABLE BIT(4) /*  read = status, write = enable */
 #define OSTIMER_IRQ_ACK_CLEAR  BIT(5)
+#else
+#define OSTIMER_IRQ_ACK_ENABLE BIT(0)
+#define OSTIMER_IRQ_ACK_CLEAR  BIT(5)
+#endif
 
 #define OST64_HZ 13000000U
 #define OST_HZ 26000000U

soc/mediatek/mt8xxx/irq.c
@@ -25,6 +25,16 @@ static const struct device *irq_dev(unsigned int *irq_inout)
 	__ASSERT_NO_MSG((*irq_inout & 0xff) == 23);
 	*irq_inout = (*irq_inout >> 8) - 1;
 	return DEVICE_DT_GET(DT_INST(1, mediatek_adsp_intc));
+#elif defined(CONFIG_SOC_SERIES_MT8196)
+	/* Two subcontrollers on core IRQs 1 and 2 */
+	uint32_t lvl1 = *irq_inout & 0xff;
+
+	*irq_inout = (*irq_inout >> 8) - 1;
+	if (lvl1 == 1) {
+		return DEVICE_DT_GET(DT_INST(0, mediatek_adsp_intc));
+	}
+	__ASSERT_NO_MSG(lvl1 == 2);
+	return DEVICE_DT_GET(DT_INST(1, mediatek_adsp_intc));
 #else
 	/* Only one on 818x */
 	return DEVICE_DT_GET(DT_INST(0, mediatek_adsp_intc));

soc/mediatek/mt8xxx/soc.c
@@ -19,14 +19,23 @@ extern char _mtk_adsp_dram_end[];
 #define DRAM_SIZE  DT_REG_SIZE(DT_NODELABEL(dram0))
 #define DRAM_END   (DRAM_START + DRAM_SIZE)
 
+#ifdef CONFIG_SOC_MT8196
+#define INIT_STACK "0x90400000"
+#define LOG_BASE   0x90580000
+#define LOG_LEN    0x80000
+#else
+#define INIT_STACK "0x60e00000"
+#define LOG_BASE   0x60700000
+#define LOG_LEN    0x100000
+#endif
+
 /* This is the true boot vector.  This device allows for direct
  * setting of the alternate reset vector, so we let it link wherever
  * it lands and extract its address in the loader.  This represents
  * the minimum amount of effort required to successfully call a C
  * function (and duplicates a few versions elsewhere in the tree:
- * really this should move to the arch layer).  Note that the stack
- * used is 15MB into the DRAM region and safe/unused by all devices.
- * But really this should be z_interrupt_stacks[0].
+ * really this should move to the arch layer).  The initial stack
+ * really should be the end of _interrupt_stacks[0]
  */
 __asm__(".align 4\n\t"
 	".global mtk_adsp_boot_entry\n\t"
@@ -38,7 +47,7 @@ __asm__(".align 4\n\t"
 	"  movi  a0, 1\n\t"
 	"  wsr   a0, WINDOWSTART\n\t"
 	"  rsync\n\t"
-	"  movi  a1, 0x60e00000\n\t"
+	"  movi  a1, " INIT_STACK "\n\t"
 	"  call4 c_boot\n\t");
 
 /* Initial MPU configuration, needed to enable caching */
@@ -109,8 +118,8 @@ static void enable_mpu(void)
  */
 int arch_printk_char_out(int c)
 {
-	char volatile * const buf = (void *)0x60700000;
-	const size_t max = 0x100000 - 4;
+	char volatile * const buf = (void *)LOG_BASE;
+	const size_t max = LOG_LEN - 4;
 	int volatile * const len = (int *)&buf[max];
 
 	if (*len < max) {