zephyrproject-rtos/zephyr

usb: device_next: support vendor request with recipient device
Allow the user to register a vendor request node identified by the
vendor code (bRequest) and containing two callbacks to handle the vendor
request. The device stack uses the vendor request node to call the
vendor request callbacks when it receives a request of type Vendor,
recipient Device, and bRequest value equal to the vendor code.

Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

subsys/usb/device_next/usbd_core.c
@@ -249,6 +249,8 @@ int usbd_device_shutdown_core(struct usbd_context *const uds_ctx)
 		LOG_ERR("Failed to cleanup descriptors, %d", ret);
 	}
 
+	usbd_device_unregister_all_vreg(uds_ctx);
+
 	return udc_shutdown(uds_ctx->dev);
 }
 

subsys/usb/device_next/usbd_device.c
@@ -310,3 +310,63 @@ bool usbd_can_detect_vbus(struct usbd_context *const uds_ctx)
 
 	return caps.can_detect_vbus;
 }
+
+struct usbd_vreq_node *usbd_device_get_vreq(struct usbd_context *const uds_ctx,
+					    const uint8_t code)
+{
+	struct usbd_vreq_node *vreq_nd;
+
+	SYS_DLIST_FOR_EACH_CONTAINER(&uds_ctx->vreqs, vreq_nd, node) {
+		if (vreq_nd->code == code) {
+			return vreq_nd;
+		}
+	}
+
+	return NULL;
+}
+
+int usbd_device_register_vreq(struct usbd_context *const uds_ctx,
+			      struct usbd_vreq_node *const vreq_nd)
+{
+	int ret = 0;
+
+	usbd_device_lock(uds_ctx);
+
+	if (usbd_is_initialized(uds_ctx)) {
+		ret = -EPERM;
+		goto error;
+	}
+
+	if (vreq_nd->to_dev == NULL && vreq_nd->to_host == NULL) {
+		ret = -EINVAL;
+		goto error;
+	}
+
+	if (!sys_dnode_is_linked(&uds_ctx->vreqs)) {
+		LOG_DBG("Initialize vendor request list");
+		sys_dlist_init(&uds_ctx->vreqs);
+	}
+
+	if (sys_dnode_is_linked(&vreq_nd->node)) {
+		ret = -EALREADY;
+		goto error;
+	}
+
+	sys_dlist_append(&uds_ctx->vreqs, &vreq_nd->node);
+	LOG_DBG("Registered vendor request 0x%02x", vreq_nd->code);
+
+error:
+	usbd_device_unlock(uds_ctx);
+	return ret;
+}
+
+void usbd_device_unregister_all_vreg(struct usbd_context *const uds_ctx)
+{
+	struct usbd_vreq_node *tmp;
+	sys_dnode_t *node;
+
+	while ((node = sys_dlist_get(&uds_ctx->vreqs))) {
+		tmp = CONTAINER_OF(node, struct usbd_vreq_node, node);
+		LOG_DBG("Remove vendor request 0x%02x", tmp->code);
+	}
+}

subsys/usb/device_next/usbd_device.h
@@ -11,6 +11,26 @@
 #include <zephyr/usb/usbd.h>
 
 /**
+ * @brief Get vendor request node
+ *
+ * Get vendor request node from internal vendor request list.
+ *
+ * @param[in] ctx    Pointer to USB device support context
+ * @param[in] code   Vendor request code
+ *
+ * @return pointer to vendor request node or NULL if not found.
+ */
+struct usbd_vreq_node *usbd_device_get_vreq(struct usbd_context *const uds_ctx,
+					    const uint8_t code);
+
+/**
+ * @brief Unregister all registered vendor request
+ *
+ * @param[in] uds_ctx Pointer to a device context
+ */
+void usbd_device_unregister_all_vreg(struct usbd_context *const uds_ctx);
+
+/**
  * @brief Get device descriptor bNumConfigurations value
  *
  * @param[in] uds_ctx Pointer to a device context

nrfconnect/sdk-zephyr

subsys/usb/device_next/usbd_core.c
@@ -251,6 +251,8 @@ int usbd_device_shutdown_core(struct usbd_context *const uds_ctx)
 		LOG_ERR("Failed to cleanup descriptors, %d", ret);
 	}
 
+	usbd_device_unregister_all_vreq(uds_ctx);
+
 	return udc_shutdown(uds_ctx->dev);
 }
 

subsys/usb/device_next/usbd_device.c
@@ -334,3 +334,67 @@ bool usbd_can_detect_vbus(struct usbd_context *const uds_ctx)
 
 	return caps.can_detect_vbus;
 }
+
+struct usbd_vreq_node *usbd_device_get_vreq(struct usbd_context *const uds_ctx,
+					    const uint8_t code)
+{
+	struct usbd_vreq_node *vreq_nd;
+
+	SYS_DLIST_FOR_EACH_CONTAINER(&uds_ctx->vreqs, vreq_nd, node) {
+		if (vreq_nd->code == code) {
+			return vreq_nd;
+		}
+	}
+
+	return NULL;
+}
+
+int usbd_device_register_vreq(struct usbd_context *const uds_ctx,
+			      struct usbd_vreq_node *const vreq_nd)
+{
+	int ret = 0;
+
+	usbd_device_lock(uds_ctx);
+
+	if (usbd_is_initialized(uds_ctx)) {
+		ret = -EPERM;
+		goto error;
+	}
+
+	if (vreq_nd->to_dev == NULL && vreq_nd->to_host == NULL) {
+		ret = -EINVAL;
+		goto error;
+	}
+
+	if (!sys_dnode_is_linked(&uds_ctx->vreqs)) {
+		LOG_DBG("Initialize vendor request list");
+		sys_dlist_init(&uds_ctx->vreqs);
+	}
+
+	if (sys_dnode_is_linked(&vreq_nd->node)) {
+		ret = -EALREADY;
+		goto error;
+	}
+
+	sys_dlist_append(&uds_ctx->vreqs, &vreq_nd->node);
+	LOG_DBG("Registered vendor request 0x%02x", vreq_nd->code);
+
+error:
+	usbd_device_unlock(uds_ctx);
+	return ret;
+}
+
+void usbd_device_unregister_all_vreq(struct usbd_context *const uds_ctx)
+{
+	struct usbd_vreq_node *tmp;
+	sys_dnode_t *node;
+
+	if (!sys_dnode_is_linked(&uds_ctx->vreqs)) {
+		return;
+	}
+
+	while ((node = sys_dlist_get(&uds_ctx->vreqs))) {
+		tmp = CONTAINER_OF(node, struct usbd_vreq_node, node);
+		LOG_DBG("Remove vendor request 0x%02x", tmp->code);
+	}
+}

subsys/usb/device_next/usbd_device.h
@@ -11,6 +11,26 @@
 #include <zephyr/usb/usbd.h>
 
 /**
+ * @brief Get vendor request node
+ *
+ * Get vendor request node from internal vendor request list.
+ *
+ * @param[in] ctx    Pointer to USB device support context
+ * @param[in] code   Vendor request code
+ *
+ * @return pointer to vendor request node or NULL if not found.
+ */
+struct usbd_vreq_node *usbd_device_get_vreq(struct usbd_context *const uds_ctx,
+					    const uint8_t code);
+
+/**
+ * @brief Unregister all registered vendor request
+ *
+ * @param[in] uds_ctx Pointer to a device context
+ */
+void usbd_device_unregister_all_vreq(struct usbd_context *const uds_ctx);
+
+/**
  * @brief Get device descriptor bNumConfigurations value
  *
  * @param[in] uds_ctx Pointer to a device context