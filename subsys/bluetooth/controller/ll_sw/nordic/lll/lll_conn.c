f142097d2fd5a2bfc08a7b1c810654c600ac9c0b
Johann Fischer <johann.fischer@nordicsemi.no>
2025-05-09 12:36:58 +0200

    Revert "[nrf fromtree] Bluetooth: Controller: Fix BT_CTLR_LOW_LAT_ULL conditional code"
    This reverts commit 59ae559950b0ae24d7edd4496944568fcfdbc1ad.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_conn.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_conn.c
@@ -771,9 +771,9 @@ void lll_conn_isr_tx(void *param)
 
 	radio_isr_set(lll_conn_isr_rx, param);
 
-#if defined(CONFIG_BT_CTLR_LOW_LAT)
+#if defined(CONFIG_BT_CTLR_LOW_LAT_ULL)
 	ull_conn_lll_tx_demux_sched(lll);
-#endif /* CONFIG_BT_CTLR_LOW_LAT */
+#endif /* CONFIG_BT_CTLR_LOW_LAT_ULL */
 }
 
 void lll_conn_rx_pkt_set(struct lll_conn *lll)
