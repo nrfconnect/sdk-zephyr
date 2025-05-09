0bfeb572636a411c38c95a48ed5bbc78180edd6b
Johann Fischer <johann.fischer@nordicsemi.no>
2025-05-09 12:36:40 +0200

    Revert "[nrf fromtree] net: mqtt: Add MQTT 5.0 support for DISCONNECT"
    This reverts commit 9f6b55f8f06fff096abc8ea6b01634d43ec16a13.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- samples/net/secure_mqtt_sensor_actuator/src/mqtt_client.c
+++ samples/net/secure_mqtt_sensor_actuator/src/mqtt_client.c
@@ -402,7 +402,7 @@ void app_mqtt_run(struct mqtt_client *client)
 		}
 	}
 	/* Gracefully close connection */
-	mqtt_disconnect(client, NULL);
+	mqtt_disconnect(client);
 }
 
 void app_mqtt_connect(struct mqtt_client *client)
