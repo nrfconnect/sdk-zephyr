ef0deb5a516deea8e6c6d104271ad4b58d98f704
Johann Fischer <johann.fischer@nordicsemi.no>
2025-05-13 10:31:34 +0200

    Revert "[nrf fromlist] logging: frontend_stmesp: Fix string addresses from remote core"
    This reverts commit 6147494082221c0d8e03f0fc2190f0e1c15db233.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- include/zephyr/logging/log_frontend_stmesp_demux.h
+++ include/zephyr/logging/log_frontend_stmesp_demux.h
@@ -290,7 +290,6 @@ void log_frontend_stmesp_demux_free(union log_frontend_stmesp_demux_packet packe
  *         cannot be retrieved.
  */
 const char *log_frontend_stmesp_demux_sname_get(uint32_t m_id, uint16_t s_id);
-const char *log_frontend_stmesp_demux_str_get(uint32_t m_id, uint16_t s_id);
 
 /** @brief Check if there are any started but not completed log messages.
  *
