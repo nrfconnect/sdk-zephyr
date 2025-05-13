884b8a5ed7d0ba635e2ac060786f65c62c9f89fb
Johann Fischer <johann.fischer@nordicsemi.no>
2025-05-13 10:31:38 +0200

    Revert "[nrf fromtree] Bluetooth: Controller: Fix device address in Periodic Adv Sync"
    This reverts commit 02e286678cbcffa9cca70e54fd67f902d4715e85.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_scan_types.h
+++ subsys/bluetooth/controller/ll_sw/ull_scan_types.h
@@ -31,11 +31,6 @@ struct ll_scan_set {
 		 * cancelling sync create, hence the volatile keyword.
 		 */
 		struct ll_sync_set *volatile sync;
-
-		/* Non-NULL when Periodic Advertising Synchronisation address
-		 * matched.
-		 */
-		void *param;
 	} periodic;
 #endif
 };
