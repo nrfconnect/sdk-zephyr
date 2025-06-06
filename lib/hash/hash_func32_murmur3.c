34950032a88bbd96fdcfabb86f007b75151fe2e8
Robert Lubos <robert.lubos@nordicsemi.no>
2025-06-06 10:26:14 +0200

    Revert "[nrf fromtree] lib: hash: murmur3: Account for unaligned 32-bit access"
    This reverts commit 6ca49f6ca44e05ff7c96a06863b4c38aa3fc4fff.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- lib/hash/hash_func32_murmur3.c
+++ lib/hash/hash_func32_murmur3.c
@@ -6,8 +6,6 @@
 
 #include <stddef.h>
 #include <stdint.h>
-#include <zephyr/toolchain.h>
-#include <zephyr/sys/util.h>
 
 static inline uint32_t murmur_32_scramble(uint32_t k)
 {
@@ -18,14 +16,6 @@ static inline uint32_t murmur_32_scramble(uint32_t k)
 	return k;
 }
 
-#define _LOOP(_GET) \
-	for (; n >= sizeof(uint32_t); n -= sizeof(uint32_t), str += sizeof(uint32_t)) { \
-		k =  _GET; \
-		h ^= murmur_32_scramble(k); \
-		h = (h << 13) | (h >> 19); \
-		h = h * 5 + 0xe6546b64; \
-	}
-
 uint32_t sys_hash32_murmur3(const char *str, size_t n)
 {
 	uint32_t k;
@@ -33,10 +23,11 @@ uint32_t sys_hash32_murmur3(const char *str, size_t n)
 	uint32_t h = 0;
 	const size_t len = n;
 
-	if (IS_ALIGNED(str, sizeof(uint32_t))) {
-		_LOOP(*(const uint32_t *)str);
-	} else {
-		_LOOP(UNALIGNED_GET((const uint32_t *)str));
+	for (; n >= sizeof(uint32_t); n -= sizeof(uint32_t), str += sizeof(uint32_t)) {
+		k = *(const uint32_t *)str;
+		h ^= murmur_32_scramble(k);
+		h = (h << 13) | (h >> 19);
+		h = h * 5 + 0xe6546b64;
 	}
 
 	for (k = 0; n != 0; --n, ++str) {
