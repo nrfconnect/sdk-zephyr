6a7c878a2b0ab59959feaa503dfd632465664207
Łukasz Stępnicki <lukasz.stepnicki@nordicsemi.no>
2025-10-07 13:36:38 +0200

    system off sample: nrf54h20 alignement
    Enabled LATCH reading on sw1 this need additiona REATAIN bits
    manipulation. Changed console to be handled automatically,
    otherwise there are some unknown characters when console
    is suspended. Removed obsolete configs.
    
    Signed-off-by: Łukasz Stępnicki <lukasz.stepnicki@nordicsemi.no>

--- samples/boards/nordic/system_off/remote/src/main.c
+++ samples/boards/nordic/system_off/remote/src/main.c
@@ -14,23 +14,11 @@
 int main(void)
 {
 
-
 	if (IS_ENABLED(CONFIG_CONSOLE)) {
 		printf("%s system off demo. Ready for system off.\n", CONFIG_BOARD);
 	}
 
-	z_nrf_grtc_wakeup_prepare(1000);
-
-	if (0) {
-		/*nrf_memconf_ramblock_ret_mask_enable_set(NRF_MEMCONF, 0, RAMBLOCK_RET_MASK, false);*/
-		/*nrf_memconf_ramblock_ret_mask_enable_set(NRF_MEMCONF, 1, RAMBLOCK_RET_MASK, false);*/
-		k_sleep(K_FOREVER);
-	} else {
-		/*nrf_memconf_ramblock_ret_mask_enable_set(NRF_MEMCONF, 0, RAMBLOCK_RET_MASK, false);*/
-		/*nrf_memconf_ramblock_ret_mask_enable_set(NRF_MEMCONF, 1, RAMBLOCK_RET_MASK, false);*/
-
-		sys_poweroff();
-	}
+	sys_poweroff();
 
 	return 0;
 }
