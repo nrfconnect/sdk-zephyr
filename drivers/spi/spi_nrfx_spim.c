be0fb4c008505bc0ea6ac1b3bc56ba3abcf6a03c
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2025-10-28 18:14:07 +0100

    [nrf fromlist] drivers: spi: nrf: align to nrfx_gpiote with extracted control block
    GPIOTE driver instances are no longer defined within nrfx.
    
    Upstream PR #: 98527
    
    Signed-off-by: Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>

--- drivers/spi/spi_nrfx_spim.c
+++ drivers/spi/spi_nrfx_spim.c
@@ -52,8 +52,16 @@ struct spi_nrfx_config {
 	void (*irq_connect)(void);
 	uint16_t max_chunk_len;
 	const struct pinctrl_dev_config *pcfg;
+	nrfx_gpiote_t *wake_gpiote;
 	uint32_t wake_pin;
+<<<<<<< HEAD
 	nrfx_gpiote_t wake_gpiote;
+=======
+#if SPIM_CROSS_DOMAIN_SUPPORTED
+	bool cross_domain;
+	int8_t default_port;
+#endif
+>>>>>>> 83490ed59a5 ([nrf fromlist] drivers: spi: nrf: align to nrfx_gpiote with extracted control block)
 	void *mem_reg;
 };
 
@@ -375,7 +383,7 @@ static int transceive(const struct device *dev,
 		dev_data->busy = true;
 
 		if (dev_config->wake_pin != WAKE_PIN_NOT_USED) {
-			error = spi_nrfx_wake_request(&dev_config->wake_gpiote,
+			error = spi_nrfx_wake_request(dev_config->wake_gpiote,
 						      dev_config->wake_pin);
 			if (error == -ETIMEDOUT) {
 				LOG_WRN("Waiting for WAKE acknowledgment timed out");
@@ -504,13 +512,15 @@ static void spim_suspend(const struct device *dev)
 {
 	const struct spi_nrfx_config *dev_config = dev->config;
 	struct spi_nrfx_data *dev_data = dev->data;
+	int err;
 
 	if (dev_data->initialized) {
 		nrfx_spim_uninit(&dev_data->spim);
 		dev_data->initialized = false;
 	}
 
-	spi_context_cs_put_all(&dev_data->ctx);
+	err = spi_context_cs_put_all(&dev_data->ctx);
+	(void)err;
 
 	(void)pinctrl_apply_state(dev_config->pcfg, PINCTRL_STATE_SLEEP);
 }
@@ -540,7 +550,7 @@ static int spi_nrfx_init(const struct device *dev)
 	(void)pinctrl_apply_state(dev_config->pcfg, PINCTRL_STATE_SLEEP);
 
 	if (dev_config->wake_pin != WAKE_PIN_NOT_USED) {
-		err = spi_nrfx_wake_init(&dev_config->wake_gpiote, dev_config->wake_pin);
+		err = spi_nrfx_wake_init(dev_config->wake_gpiote, dev_config->wake_pin);
 		if (err == -ENODEV) {
 			LOG_ERR("Failed to allocate GPIOTE channel for WAKE");
 			return err;
@@ -631,13 +641,22 @@ static int spi_nrfx_deinit(const struct device *dev)
 			SPI_NRFX_SPIM_EXTENDED_CONFIG(inst)		       \
 		},							       \
 		.irq_connect = irq_connect##inst,			       \
-		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(inst),		       \
 		.max_chunk_len = BIT_MASK(				       \
 			DT_INST_PROP(inst, easydma_maxcnt_bits)),	       \
+		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(inst),		       \
+		.wake_gpiote = WAKE_GPIOTE_NODE(DT_DRV_INST(inst)),	       \
 		.wake_pin = NRF_DT_GPIOS_TO_PSEL_OR(DT_DRV_INST(inst),	       \
 						    wake_gpios,		       \
 						    WAKE_PIN_NOT_USED),	       \
+<<<<<<< HEAD
 		.wake_gpiote = WAKE_GPIOTE_INSTANCE(DT_DRV_INST(inst)),	       \
+=======
+		IF_ENABLED(SPIM_PINS_CROSS_DOMAIN(inst),		       \
+			(.cross_domain = true,				       \
+			 .default_port =				       \
+				DT_PROP_OR(DT_INST_PHANDLE(inst,	       \
+					default_gpio_port), port, -1),))       \
+>>>>>>> 83490ed59a5 ([nrf fromlist] drivers: spi: nrf: align to nrfx_gpiote with extracted control block)
 		.mem_reg = DMM_DEV_TO_REG(DT_DRV_INST(inst)),		       \
 	};								       \
 	BUILD_ASSERT(!DT_INST_NODE_HAS_PROP(inst, wake_gpios) ||	       \
