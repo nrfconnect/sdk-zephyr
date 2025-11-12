5089754bf46c08dc15a958aab41491dcf152260c
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-11-12 14:04:28 +0100

    Revert "[nrf fromtree] Bluetooth: Controller: Fix HCI command buffer allocation failure"
    This reverts commit eabfcff0111f2c74a63900ebfb670239c3a90c90.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/host/hci_raw.c
+++ subsys/bluetooth/host/hci_raw.c
@@ -18,10 +18,7 @@
 
 #include <zephyr/bluetooth/hci.h>
 
-#include "common/hci_common_internal.h"
-
 #include "hci_ecc.h"
-
 #include "monitor.h"
 #include "hci_raw_internal.h"
 
@@ -45,7 +42,7 @@ static uint8_t raw_mode;
 
 NET_BUF_POOL_FIXED_DEFINE(hci_rx_pool, BT_BUF_RX_COUNT,
 			  BT_BUF_RX_SIZE, sizeof(struct bt_buf_data), NULL);
-NET_BUF_POOL_FIXED_DEFINE(hci_cmd_pool, BT_BUF_CMD_TX_COUNT,
+NET_BUF_POOL_FIXED_DEFINE(hci_cmd_pool, CONFIG_BT_BUF_CMD_TX_COUNT,
 			  BT_BUF_CMD_SIZE(CONFIG_BT_BUF_CMD_TX_SIZE),
 			  sizeof(struct bt_buf_data), NULL);
 NET_BUF_POOL_FIXED_DEFINE(hci_acl_pool, CONFIG_BT_BUF_ACL_TX_COUNT,
