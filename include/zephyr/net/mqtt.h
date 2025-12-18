24703c8e7f55f424fe305d89b3e30c8f235d2698
Robert Lubos <robert.lubos@nordicsemi.no>
2025-12-18 12:11:46 +0100

    [nrf fromlist] net: mqtt: Allow to force native TLS on MQTT socket
    Upstream PR #: 101263
    
    Add a parameter to MQTT TLS configuration that allows to force native
    TLS on a socket if offload dispatcher is used. This allows for MQTT to
    use native TLS implementation with an offloaded TCP socket.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- include/zephyr/net/mqtt.h
+++ include/zephyr/net/mqtt.h
@@ -773,6 +773,9 @@ struct mqtt_sec_config {
 
 	/** Indicates the preference for copying certificates to the heap. */
 	int cert_nocopy;
+
+	/** Set socket to use native TLS (used with socket offloading). */
+	bool set_native_tls;
 };
 
 /** @brief MQTT transport type. */
