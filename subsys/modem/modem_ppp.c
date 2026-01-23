687c3c6d57c5a319be3cf11c47810810f6ee9147
Tomasz Moń <tomasz.mon@nordicsemi.no>
2026-01-23 11:01:52 +0100

    Revert "[nrf fromtree] modem: modem_cellular: Allow PPP interface to wake up the device"
    This reverts commit 04e697e8f360f5ba1c1002ce2a05f7e29540238a.
    
    Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>

--- subsys/modem/modem_ppp.c
+++ subsys/modem/modem_ppp.c
@@ -7,7 +7,6 @@
 #include <zephyr/net/ppp.h>
 #include <zephyr/sys/crc.h>
 #include <zephyr/modem/ppp.h>
-#include <zephyr/pm/device_runtime.h>
 #include <string.h>
 
 #include <zephyr/logging/log.h>
@@ -446,24 +445,12 @@ static void modem_ppp_ppp_api_init(struct net_if *iface)
 
 static int modem_ppp_ppp_api_start(const struct device *dev)
 {
-	const struct modem_ppp_config *config = (const struct modem_ppp_config *)dev->config;
-
-	if (config == NULL || config->dev == NULL) {
-		return 0;
-	}
-
-	return pm_device_runtime_get(config->dev);
+	return 0;
 }
 
 static int modem_ppp_ppp_api_stop(const struct device *dev)
 {
-	const struct modem_ppp_config *config = (const struct modem_ppp_config *)dev->config;
-
-	if (config == NULL || config->dev == NULL) {
-		return 0;
-	}
-
-	return pm_device_runtime_put_async(config->dev, K_NO_WAIT);
+	return 0;
 }
 
 static int modem_ppp_ppp_api_send(const struct device *dev, struct net_pkt *pkt)
