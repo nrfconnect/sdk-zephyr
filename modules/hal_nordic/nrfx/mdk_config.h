962a3acb29e119a28104ad4f91c051cfb88b1b6c
Aymen LAOUINI <aymen.laouini@nordicsemi.no>
2026-03-19 14:56:53 +0200

    [nrf noup] Fix missing mdk_config file and fix bicrgen
    - not for merge, only used to advance on integration until
    https://github.com/nrfconnect/sdk-zephyr/pull/3859 is merged.
    
    Signed-off-by: Aymen LAOUINI <aymen.laouini@nordicsemi.no>

---
+++ modules/hal_nordic/nrfx/mdk_config.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2026, Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef NRFX_MDK_CONFIG_H_
+#define NRFX_MDK_CONFIG_H_
+
+#endif /* NRFX_MDK_CONFIG_H_ */
+
