fcec25edb471e4d6c4867bebfbd9d4ffbcb7cfc3
Tomasz Moń <tomasz.mon@nordicsemi.no>
2026-04-28 15:52:01 +0200

    [nrf fromtree] usb: device_next: Return error code directly
    Universal Serial Bus Specification Revision 2.0 does not differentiate
    between "stack errors" and "protocol errors". 8.5.3.1 Reporting Status
    Results lists three control transfer outcomes:
      * Function completes (ZLP/ACK)
      * Function has an error (STALL)
      * Function is busy (NAK)
    
    If the control transfer fails, the device should respond with a STALL
    handshake. In addition to protocol errors, which are reported using
    errno values, the new device stack uses control transfer handler
    return values as indicators for unrecoverable errors. Handlers that
    wish to send a STALL handshake must set an errno value and return 0.
    
    The idea of using both errno and return values was to propagate
    unrecoverable errors to the user application, which would then shut
    down and re-enable the stack. However, this approach is not unambiguous
    for users and causes non-compliant timeout behavior when a handler
    returns a non-zero value.
    
    Make the stack always behave standards-compliant by removing errno
    usage and passing the result directly via the control handler return
    value. For now, keep the errno detection to output a deprecation warning.
    
    Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>
    (cherry picked from commit 8d5d9bb1eee7ae635cf52d375d68cf2ffb005922)

--- subsys/usb/device_next/class/usbd_cdc_acm.c
+++ subsys/usb/device_next/class/usbd_cdc_acm.c
@@ -494,8 +494,7 @@ static int usbd_cdc_acm_cth(struct usbd_class_data *const c_data,
 
 	if (setup->bRequest == GET_LINE_CODING) {
 		if (buf == NULL) {
-			errno = -ENOMEM;
-			return 0;
+			return -ENOMEM;
 		}
 
 		min_len = MIN(sizeof(data->line_coding), setup->wLength);
@@ -506,9 +505,7 @@ static int usbd_cdc_acm_cth(struct usbd_class_data *const c_data,
 
 	LOG_DBG("bmRequestType 0x%02x bRequest 0x%02x unsupported",
 		setup->bmRequestType, setup->bRequest);
-	errno = -ENOTSUP;
-
-	return 0;
+	return -ENOTSUP;
 }
 
 static int usbd_cdc_acm_ctd(struct usbd_class_data *const c_data,
@@ -524,8 +521,7 @@ static int usbd_cdc_acm_ctd(struct usbd_class_data *const c_data,
 	case SET_LINE_CODING:
 		len = sizeof(data->line_coding);
 		if (setup->wLength != len) {
-			errno = -ENOTSUP;
-			return 0;
+			return -ENOTSUP;
 		}
 
 		if (buf == NULL) {
@@ -540,8 +536,7 @@ static int usbd_cdc_acm_ctd(struct usbd_class_data *const c_data,
 
 	case SET_CONTROL_LINE_STATE:
 		if (setup->wLength != 0) {
-			errno = -ENOTSUP;
-			return 0;
+			return -ENOTSUP;
 		}
 
 		data->line_state = setup->wValue;
@@ -555,9 +550,7 @@ static int usbd_cdc_acm_ctd(struct usbd_class_data *const c_data,
 
 	LOG_DBG("bmRequestType 0x%02x bRequest 0x%02x unsupported",
 		setup->bmRequestType, setup->bRequest);
-	errno = -ENOTSUP;
-
-	return 0;
+	return -ENOTSUP;
 }
 
 static int usbd_cdc_acm_init(struct usbd_class_data *const c_data)
