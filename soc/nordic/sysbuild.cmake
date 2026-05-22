7def3f6b63bfc9d806513454ef7546965d6f7e10
Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>
2026-05-22 10:58:31 +0200

    [nrf fromlist] soc: samples: nordic: introcuce coproc launcher sample integration
    Multiple nordic socs have coprocessors of various kinds. riscv based
    "vipers" like flpr and ppr, and ARM based radio cores like cpunet
    and cpurad. These all require cpuapp to launch them, and some socs
    like the nrf54h20 share global peripherals which have to be allocated
    to the coprocessors at build time.
    
    Currently there is one image which works for the cpuflpr and cpuppr
    in cases where no peripherals need to be reallocated from the cpuapp
    to the coprocessors. We need an image which works for any coprocessor
    and which allocates all peripherals to the coprocessor by default.
    
    This commit adds that image, and integrates it into sysbuild for
    nordic socs so the image is included by default if applications are
    built for one of these coprocessors.
    
    All coprocessors for all nordic socs are now supported.
    
    Signed-off-by: Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>
    
    Upstream PR #: 109649

--- soc/nordic/sysbuild.cmake
+++ soc/nordic/sysbuild.cmake
@@ -1,31 +1,31 @@
 # Copyright (c) 2024 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
-if(SB_CONFIG_VPR_LAUNCHER)
+if(SB_CONFIG_NORDIC_COPROC_LAUNCHER)
   set(launcher_core "cpuapp")
   string(REPLACE "/" ";" launcher_quals ${BOARD_QUALIFIERS})
   list(LENGTH launcher_quals launcher_quals_len)
   list(GET launcher_quals 0 launcher_soc)
-  list(GET launcher_quals 1 launcher_vpr)
+  list(GET launcher_quals 1 launcher_coproc)
 
-  string(REPLACE "cpu" "" launcher_vpr ${launcher_vpr})
+  string(REPLACE "cpu" "" launcher_coproc ${launcher_coproc})
 
   if(launcher_quals_len EQUAL 3)
     list(GET launcher_quals 2 launcher_variant)
-    set(launcher_vpr ${launcher_vpr}-${launcher_variant})
+    set(launcher_coproc ${launcher_coproc}-${launcher_variant})
   endif()
 
   string(CONCAT launcher_board ${BOARD} "/" ${launcher_soc} "/" ${launcher_core})
 
-  set(image "vpr_launcher")
+  set(image "coproc_launcher")
 
   ExternalZephyrProject_Add(
     APPLICATION ${image}
-    SOURCE_DIR ${ZEPHYR_BASE}/samples/basic/minimal
+    SOURCE_DIR ${ZEPHYR_BASE}/samples/boards/nordic/coproc_launcher
     BOARD ${launcher_board}
   )
 
-  string(CONCAT launcher_snippet "nordic-" ${launcher_vpr})
+  string(CONCAT launcher_snippet "nordic-" ${launcher_coproc})
 
   sysbuild_cache_set(VAR ${image}_SNIPPET APPEND REMOVE_DUPLICATES ${launcher_snippet})
 endif()
