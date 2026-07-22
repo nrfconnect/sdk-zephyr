b9edd0df0fbc4618f551a03ecc0476050985842d
Robert Lubos <robert.lubos@nordicsemi.no>
2026-07-22 13:41:48 +0200

    Revert "[nrf fromtree] twister: tests: adapt to unified test definition file"
    This reverts commit 53a8f30918d31500dae317d540631b5e8f92fce1.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- scripts/tests/twister_blackbox/conftest.py
+++ scripts/tests/twister_blackbox/conftest.py
@@ -23,7 +23,9 @@ sys.path.insert(0, os.path.join(ZEPHYR_BASE, "scripts/pylib/twister"))
 sys.path.insert(0, os.path.join(ZEPHYR_BASE, "scripts"))
 
 
-test_filename_mock = mock.PropertyMock(return_value=['test_data.yaml'])
+sample_filename_mock = mock.PropertyMock(return_value='test_sample.yaml')
+suite_filename_mock = mock.PropertyMock(return_value='test_data.yaml')
+sample_filename_mock = mock.PropertyMock(return_value='test_sample.yaml')
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "noclearlog: disable the clear_log autouse fixture")
