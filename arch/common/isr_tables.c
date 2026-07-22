0eb7b3b6b171a306c4cb48b328a56871482f7b9e
Robert Lubos <robert.lubos@nordicsemi.no>
2026-07-22 13:42:16 +0200

    Revert "[nrf fromtree] arch: ISR table size optimization"
    This reverts commit c624957b19dd822efe00c453a16c596d8d6ebf9a.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- arch/common/isr_tables.c
+++ arch/common/isr_tables.c
@@ -87,20 +87,15 @@ const uintptr_t __irq_vector_table _irq_vector_table[IRQ_TABLE_SIZE] = {
 /* If there are no interrupts at all, or all interrupts are of the 'direct'
  * type and bypass the _sw_isr_table, then do not generate one.
  */
-#if defined(CONFIG_GEN_SW_ISR_TABLE_ARRAY)
+#ifdef CONFIG_GEN_SW_ISR_TABLE
 #ifndef CONFIG_DYNAMIC_INTERRUPTS
 const
-#endif /* CONFIG_DYNAMIC_INTERRUPTS */
-	struct _isr_table_entry __sw_isr_table _sw_isr_table[IRQ_TABLE_SIZE] = {
-		[0 ...(IRQ_TABLE_SIZE - 1)] = {(const void *)0x42, &z_irq_spurious},
+#endif
+struct _isr_table_entry __sw_isr_table _sw_isr_table[IRQ_TABLE_SIZE] = {
+	[0 ...(IRQ_TABLE_SIZE - 1)] = {(const void *)0x42,
+				       &z_irq_spurious},
 };
-#elif defined(CONFIG_GEN_SW_ISR_TABLE_SWITCH)
-void __sw_isr_table get_isr_entry(int irq_number, struct _isr_table_entry *entry)
-{
-	entry->arg = (const void *)0x0;
-	entry->isr = z_irq_spurious;
-}
-#endif /* CONFIG_GEN_SW_ISR_TABLE_ARRAY */
+#endif
 
 #ifdef CONFIG_SHARED_INTERRUPTS
 #ifndef CONFIG_DYNAMIC_INTERRUPTS
