b3145a1fbe95277bfc0d52b4842f10d4061293c4
Robert Lubos <robert.lubos@nordicsemi.no>
2026-08-07 15:50:20 +0200

    Revert "[nrf fromtree] samples: boards: nordic: coresight_stm: more logs from nrfutil"
    This reverts commit 8f3d234c0920d4263511ac9f22263ce6845436b5.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- samples/boards/nordic/coresight_stm/pytest/test_stm.py
+++ samples/boards/nordic/coresight_stm/pytest/test_stm.py
@@ -203,12 +203,7 @@ def nrfutil_dictionary_from_serial(
     try:
         # run nrfutil trace in background non-blocking
         logger.info(f"Executing:\n{cmd}")
-        proc = subprocess.Popen(cmd.split(),
-                                stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT,
-                                encoding='UTF-8',
-                                errors='replace')
+        proc = subprocess.Popen(cmd.split(), stdout=subprocess.DEVNULL)
     except OSError as exc:
         logger.error(f"Unable to start nrfutil trace:\n{cmd}\n{exc}")
     try:
@@ -217,8 +212,6 @@ def nrfutil_dictionary_from_serial(
         pass
     finally:
         _kill(proc)
-        outs, errs = proc.communicate()
-        logger.info(f"{outs=}\n{errs=}")
 
 
 def test_sample_STM_decoded(dut: DeviceAdapter):
