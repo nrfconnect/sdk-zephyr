a44d81384e9bf6710303e79670b454e9bdcfd983
Antoni Duda <antoni.duda@nordicsemi.no>
2026-08-07 07:31:38 +0200

    Revert "[nrf fromtree] Bluetooth: tests: Take advantage of the new inline string helpers"
    This reverts commit 390685d633830ec576ff0b45aaee1de8656a0574.
    
    Signed-off-by: Antoni Duda <antoni.duda@nordicsemi.no>

--- tests/bsim/bluetooth/audio/src/cap_handover_central_test.c
+++ tests/bsim/bluetooth/audio/src/cap_handover_central_test.c
@@ -312,6 +312,7 @@ static struct bt_gatt_cb gatt_callbacks = {
 
 static bool check_audio_support_and_connect_cb(struct bt_data *data, void *user_data)
 {
+	char addr_str[BT_ADDR_LE_STR_LEN];
 	bt_addr_le_t *addr = user_data;
 	const struct bt_uuid *uuid;
 	uint16_t uuid_val;
@@ -333,7 +334,8 @@ static bool check_audio_support_and_connect_cb(struct bt_data *data, void *user_
 		return true; /* Continue parsing to next AD data type */
 	}
 
-	LOG_DBG("Device found: %s", bt_addr_le_str(addr));
+	bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
+	LOG_DBG("Device found: %s", addr_str);
 
 	bt_addr_le_copy(&remote_dev_addr, addr);
 	SET_FLAG(flag_dev_found);
