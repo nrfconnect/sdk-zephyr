da5f46874c186555a5e81a959db5c5634503c02c
Robert Lubos <robert.lubos@nordicsemi.no>
2026-08-07 15:50:10 +0200

    Revert "[nrf fromtree] modem: uart_isr: support mdm-dtr-gpios"
    This reverts commit 2e8a666f0e5056f55711dd05c1c077b4ff78e89f.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/modem/backends/modem_backend_uart_isr.c
+++ subsys/modem/backends/modem_backend_uart_isr.c
@@ -12,7 +12,6 @@
 LOG_MODULE_REGISTER(modem_backend_uart_isr, CONFIG_MODEM_MODULES_LOG_LEVEL);
 
 #include <string.h>
-#include <zephyr/drivers/gpio.h>
 
 static void modem_backend_uart_isr_flush(struct modem_backend_uart *backend)
 {
@@ -126,9 +125,6 @@ static int modem_backend_uart_isr_open(void *data)
 	}
 
 	modem_backend_uart_isr_flush(backend);
-	if (backend->dtr_gpio) {
-		gpio_pin_set_dt(backend->dtr_gpio, 1);
-	}
 	uart_irq_rx_enable(backend->uart);
 	uart_irq_tx_enable(backend->uart);
 	modem_pipe_notify_opened(&backend->pipe);
@@ -251,9 +247,6 @@ static int modem_backend_uart_isr_close(void *data)
 	int ret;
 	struct modem_backend_uart *backend = (struct modem_backend_uart *)data;
 
-	if (backend->dtr_gpio) {
-		gpio_pin_set_dt(backend->dtr_gpio, 0);
-	}
 	uart_irq_rx_disable(backend->uart);
 	uart_irq_tx_disable(backend->uart);
 	if (backend->dtr_gpio) {
