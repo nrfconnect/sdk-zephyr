38b4c12f911943f88e2429679b694c02582017cc
Robert Lubos <robert.lubos@nordicsemi.no>
2026-08-07 15:50:02 +0200

    Revert "[nrf fromtree] Bluetooth: tests: Take advantage of the new inline string helpers"
    This reverts commit a80c4ee4c4f263accaf924a2ed073a97347d4be3.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- tests/bsim/bluetooth/host/l2cap/multilink_peripheral/src/dut.c
+++ tests/bsim/bluetooth/host/l2cap/multilink_peripheral/src/dut.c
@@ -43,7 +43,11 @@ static struct test_ctx contexts[CONFIG_BT_MAX_CONN];
 
 static int send_data_over_l2cap(struct bt_l2cap_chan *chan, uint8_t *data, size_t len)
 {
-	LOG_DBG("[%s] chan %p data %p len %d", bt_conn_dst_str(chan->conn), chan, data, len);
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(chan->conn), addr, sizeof(addr));
+
+	LOG_DBG("[%s] chan %p data %p len %d", addr, chan, data, len);
 
 	struct net_buf *buf = net_buf_alloc(&sdu_tx_pool, K_NO_WAIT);
 
@@ -75,7 +79,11 @@ static void resume_sending_until_done(struct test_ctx *ctx)
 	if (ctx->sdu_count < SDU_NUM) {
 		send_data_over_l2cap(chan, tx_data, sizeof(tx_data));
 	} else {
-		LOG_DBG("[%s] Done sending", bt_conn_dst_str(chan->conn));
+		char addr[BT_ADDR_LE_STR_LEN];
+
+		bt_addr_le_to_str(bt_conn_get_dst(chan->conn), addr, sizeof(addr));
+
+		LOG_DBG("[%s] Done sending", addr);
 	}
 }
 
@@ -181,18 +189,21 @@ static struct test_ctx *get_ctx_from_address(const bt_addr_le_t *address)
 static void acl_connected(struct bt_conn *conn, uint8_t err)
 {
 	const bt_addr_le_t *central = bt_conn_get_dst(conn);
+	char addr[BT_ADDR_LE_STR_LEN];
 	struct test_ctx *ctx;
 	int ret;
 
-	TEST_ASSERT(err == 0, "Failed to connect to %s (0x%02x)", bt_addr_le_str(central), err);
+	bt_addr_le_to_str(central, addr, sizeof(addr));
+
+	TEST_ASSERT(err == 0, "Failed to connect to %s (0x%02x)", addr, err);
 
 	UNSET_FLAG(ADVERTISING);
 
-	LOG_DBG("[%s] Connected (conn %p)", bt_addr_le_str(central), conn);
+	LOG_DBG("[%s] Connected (conn %p)", addr, conn);
 
 	ctx = get_ctx_from_address(central);
 	if (ctx == NULL) {
-		LOG_DBG("no initialized context for %s, allocating..", bt_addr_le_str(central));
+		LOG_DBG("no initialized context for %s, allocating..", addr);
 		ctx = alloc_ctx();
 		TEST_ASSERT(ctx, "Couldn't allocate ctx for conn %p", conn);
 
@@ -206,7 +217,11 @@ static void acl_connected(struct bt_conn *conn, uint8_t err)
 
 static void acl_disconnected(struct bt_conn *conn, uint8_t reason)
 {
-	LOG_DBG("Disconnected from %s (reason 0x%02x)", bt_conn_dst_str(conn), reason);
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_DBG("Disconnected from %s (reason 0x%02x)", addr, reason);
 }
 
 static void increment(struct bt_conn *conn, void *user_data)
--- tests/bsim/bluetooth/tester/src/host/gap_central.c
+++ tests/bsim/bluetooth/tester/src/host/gap_central.c
@@ -22,6 +22,7 @@ LOG_MODULE_REGISTER(bsim_gap_central, CONFIG_BSIM_BTTESTER_LOG_LEVEL);
 
 static void test_gap_central(void)
 {
+	char addr_str[BT_ADDR_LE_STR_LEN];
 	bt_addr_le_t remote_addr;
 	bt_addr_le_t ev_addr;
 
@@ -32,13 +33,14 @@ static void test_gap_central(void)
 	bsim_btp_core_register(BTP_SERVICE_ID_GAP);
 	bsim_btp_gap_start_discovery(BTP_GAP_DISCOVERY_FLAG_LE);
 	bsim_btp_wait_for_gap_device_found(&remote_addr);
-	LOG_INF("Found remote device %s", bt_addr_le_str(&remote_addr));
+	bt_addr_le_to_str(&remote_addr, addr_str, sizeof(addr_str));
+	LOG_INF("Found remote device %s", addr_str);
 
 	bsim_btp_gap_stop_discovery();
 	bsim_btp_gap_connect(&remote_addr, BTP_GAP_ADDR_TYPE_IDENTITY);
 	bsim_btp_wait_for_gap_device_connected(&ev_addr);
 	TEST_ASSERT(bt_addr_le_eq(&remote_addr, &ev_addr));
-	LOG_INF("Device %s connected", bt_addr_le_str(&remote_addr));
+	LOG_INF("Device %s connected", addr_str);
 
 	/* Keep alive for a little while */
 	k_sleep(K_SECONDS(10));
@@ -46,7 +48,7 @@ static void test_gap_central(void)
 	bsim_btp_gap_disconnect(&remote_addr);
 	bsim_btp_wait_for_gap_device_disconnected(&ev_addr);
 	TEST_ASSERT(bt_addr_le_eq(&remote_addr, &ev_addr));
-	LOG_INF("Device %s disconnected", bt_addr_le_str(&remote_addr));
+	LOG_INF("Device %s disconnected", addr_str);
 
 	TEST_PASS("PASSED\n");
 }
