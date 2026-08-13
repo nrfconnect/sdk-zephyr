cc82af62a22b5b3b196dcd200b6b67b81d33a11c
Robert Lubos <robert.lubos@nordicsemi.no>
2026-08-13 15:57:24 +0200

    Revert "[nrf fromtree] Bluetooth: samples: Take advantage of the new inline string helpers"
    This reverts commit b59598211cb560c5ccbde50b05ea7c66028b54fd.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- samples/bluetooth/iso_peripheral/src/main.c
+++ samples/bluetooth/iso_peripheral/src/main.c
@@ -29,18 +29,25 @@ static const struct bt_data sd[] = {
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
 	if (err) {
-		printk("Failed to connect to %s %u %s\n", bt_conn_dst_str(conn),
-		       err, bt_hci_err_to_str(err));
+		printk("Failed to connect to %s %u %s\n", addr, err, bt_hci_err_to_str(err));
 		return;
 	}
 
-	printk("Connected %s\n", bt_conn_dst_str(conn));
+	printk("Connected %s\n", addr);
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
-	printk("Disconnected from %s, reason 0x%02x %s\n", bt_conn_dst_str(conn),
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Disconnected from %s, reason 0x%02x %s\n", addr,
 	       reason, bt_hci_err_to_str(reason));
 }
 
