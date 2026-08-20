8797f2c42f862d6f5e7d0b4b529f1af71e2ade9e
Antoni Duda <antoni.duda@nordicsemi.no>
2026-08-20 10:20:38 +0200

    Revert "[nrf fromtree] bluetooth: host: consistent notification of subscription termination"
    This reverts commit b26a328a15b976ef409edc24485b69aa10739c9a.
    
    Signed-off-by: Antoni Duda <antoni.duda@nordicsemi.no>

--- subsys/bluetooth/host/gatt.c
+++ subsys/bluetooth/host/gatt.c
@@ -5230,12 +5230,6 @@ static void gatt_write_ccc_rsp(struct bt_conn *conn, int err,
 			return;
 		}
 
-		att_err = att_err_from_int(err);
-
-		if (params->subscribe) {
-			params->subscribe(conn, att_err, params);
-		}
-
 		prev = NULL;
 
 		SYS_SLIST_FOR_EACH_NODE_SAFE(&sub->list, node, tmp) {
@@ -5245,15 +5239,15 @@ static void gatt_write_ccc_rsp(struct bt_conn *conn, int err,
 			}
 			prev = node;
 		}
-	} else {
-		if (params->subscribe) {
-			params->subscribe(conn, BT_ATT_ERR_SUCCESS, params);
-		}
+	} else if (!params->value) {
+		/* Notify with NULL data to complete unsubscribe */
+		params->notify(conn, params, NULL, 0);
+	}
 
-		if (!params->value) {
-			/* Notify with NULL data to complete unsubscribe */
-			params->notify(conn, params, NULL, 0);
-		}
+	att_err = att_err_from_int(err);
+
+	if (params->subscribe) {
+		params->subscribe(conn, att_err, params);
 	}
 }
 
