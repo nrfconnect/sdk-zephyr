e3fa27e9763e39aa78a6f1b9b8c1cc46f036371a
Antoni Duda <antoni.duda@nordicsemi.no>
2026-08-20 10:21:00 +0200

    Revert "[nrf fromtree] Bluetooth: tests: Take advantage of the new inline string helpers"
    This reverts commit 390685d633830ec576ff0b45aaee1de8656a0574.
    
    Signed-off-by: Antoni Duda <antoni.duda@nordicsemi.no>

--- tests/bsim/bluetooth/host/adv/periodic/src/per_adv_advertiser.c
+++ tests/bsim/bluetooth/host/adv/periodic/src/per_adv_advertiser.c
@@ -28,19 +28,27 @@ DEFINE_FLAG_STATIC(flag_bonded);
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
 	if (err != BT_HCI_ERR_SUCCESS) {
-		TEST_FAIL("Failed to connect to %s: %u", bt_conn_dst_str(conn), err);
+		TEST_FAIL("Failed to connect to %s: %u", addr, err);
 		return;
 	}
 
-	printk("Connected to %s\n", bt_conn_dst_str(conn));
+	printk("Connected to %s\n", addr);
 	g_conn = bt_conn_ref(conn);
 	SET_FLAG(flag_connected);
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
-	printk("Disconnected: %s (reason %u)\n", bt_conn_dst_str(conn), reason);
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Disconnected: %s (reason %u)\n", addr, reason);
 
 	bt_conn_unref(g_conn);
 	g_conn = NULL;
