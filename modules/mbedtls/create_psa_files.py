51a3834d12d837b5033fcc9b9ba527a4bc7101eb
Antoni Duda <antoni.duda@nordicsemi.no>
2026-08-26 09:32:16 +0200

    Revert "[nrf fromtree] modules: mbedtls: remove CONFIG_PSA_CRYPTO_CLIENT"
    This reverts commit b5c8226dd3be0584d58d0bad933efe83842f6360.
    
    Signed-off-by: Antoni Duda <antoni.duda@nordicsemi.no>

--- modules/mbedtls/create_psa_files.py
+++ modules/mbedtls/create_psa_files.py
@@ -29,13 +29,19 @@ KCONFIG_HEADER="""\
 # from {}.
 # Some manual additions are made, but no deletion.
 
-if PSA_CRYPTO
+config PSA_CRYPTO_CLIENT
+	bool
+	help
+	  Promptless symbol to state that there is a PSA crypto API provider
+	  enabled in the system. This allows to select desired PSA_WANT features.
+
+if PSA_CRYPTO_CLIENT
 
 config PSA_CRYPTO_ENABLE_ALL
 	bool "All PSA crypto features"
 """.format(os.path.basename(__file__), INPUT_REL_PATH)
 
-KCONFIG_FOOTER="\nendif # PSA_CRYPTO\n"
+KCONFIG_FOOTER="\nendif # PSA_CRYPTO_CLIENT\n"
 
 H_HEADER="""\
 /*
