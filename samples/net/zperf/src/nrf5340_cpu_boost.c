f7385a32bada5f66dd9d4ac4159068e8e323c0dc
Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>
2026-09-10 15:48:42 +0200

    [nrf fromlist] samples: Replace divider set API with set rate API in clock users
    Replaced nrfx_clock_hfclk_divider_set with clock_control_set_rate API.
    
    Signed-off-by: Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>
    
    Upstream PR #: 118610

--- samples/net/zperf/src/nrf5340_cpu_boost.c
+++ samples/net/zperf/src/nrf5340_cpu_boost.c
@@ -11,12 +11,9 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/clock_control/nrf_clock_control.h>
 
-#if defined(CONFIG_CLOCK_CONTROL_NRF)
-#include <nrfx_clock.h>
-#else
-#include <nrfx_clock_hfclk.h>
-#endif
+static const uint32_t hfclk_requested_frequency = MHZ(128);
 
 LOG_MODULE_DECLARE(zperf, CONFIG_NET_ZPERF_LOG_LEVEL);
 
@@ -27,13 +24,13 @@ static int nrf53_cpu_boost(void)
 	/* For optimal performance, the CPU frequency should be set to 128 MHz */
 #if defined(CONFIG_CLOCK_CONTROL_NRF)
 	err = nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK, NRF_CLOCK_HFCLK_DIV_1);
-
+#else
+	err = clock_control_set_rate(DEVICE_DT_GET_ONE(nordic_nrf_clock_hfclk), NULL,
+				     &hfclk_requested_frequency);
+#endif
 	if (err != 0) {
 		LOG_WRN("Failed to set 128 MHz: %d", err);
 	}
-#else
-	nrfx_clock_hfclk_divider_set(NRF_CLOCK_HFCLK_DIV_1);
-#endif
 
 	LOG_INF("Starting %s with CPU frequency: %d MHz", CONFIG_BOARD, SystemCoreClock/MHZ(1));
 
